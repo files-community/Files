fb6d2ed6eca2d68a19a560e0d6e197f3896b66ff
Luke Blevins <lukeb@DESKTOP-D3IK38E>
2018-12-27 14:59:29 -0500

    Remove Heading Cont'd

--- PhotoAlbum.xaml.cs
+++ PhotoAlbum.xaml.cs
@@ -1,17 +1,4 @@
-﻿//  ---- PhotoAlbum.xaml.cs ----
-//
-//   Copyright 2018 Luke Blevins
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//  ---- This file contains various behind-the-scenes code for the image-optimized layout ---- 
-//
-
-using Interacts;
+﻿using Interacts;
 using ItemListPresenter;
 using System;
 using Windows.UI.Xaml;
