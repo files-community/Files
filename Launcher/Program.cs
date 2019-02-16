fa8265aecc80cf1e7497dde0d6ce83dff48f3ff0
Ashik Salim <ashikns@gmail.com>
2019-02-16 09:35:00 +0530

    add launcher project and packaging project

---
+++ Launcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Storage;
+
+namespace Launcher
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var executable = (string) ApplicationData.Current.LocalSettings.Values["executable"];
+                Process.Start(executable);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
