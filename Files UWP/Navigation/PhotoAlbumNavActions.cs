bcfbe740c5610668b59d5a1c26b6252cfed2b6eb
Luke Blevins <lukeblevins15@gmail.com>
2019-02-28 21:32:31 -0500

    Fix for Item Selection in PhotoView navigation

--- Files UWP/Navigation/PhotoAlbumNavActions.cs
+++ Files UWP/Navigation/PhotoAlbumNavActions.cs
@@ -129,15 +129,23 @@ namespace Files.Navigation
                 else
                 {
                     App.PathText.Text = (History.HistoryList[History.HistoryList.Count - 1]);
-                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    if((History.HistoryList[History.HistoryList.Count - 1]).Split('\\')[0].Contains("C:\\"))
                     {
-                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "LocD_IC")
+                        foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
                         {
-                            MainPage.Select.itemSelected = NavItemChoice;
-                            break;
+                            if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Content.ToString() == ("Removable Drive (" + History.ForwardList[History.ForwardList.Count() - 1].Split('\\')[0] + "\\)"))
+                            {
+
+                                MainPage.Select.itemSelected = NavItemChoice;
+                                break;
+                            }
                         }
                     }
-                    App.ViewModel.AddItemsToCollectionAsync(History.HistoryList[History.HistoryList.Count - 1], GenericFileBrowser.GFBPageName); // To take into account the correct index without interference from the folder being navigated to
+                    else
+                    {
+
+                    }
+                    App.ViewModel.AddItemsToCollectionAsync(History.HistoryList[History.HistoryList.Count - 1], PhotoAlbum.PAPageName); // To take into account the correct index without interference from the folder being navigated to
                 }
 
                 if (History.ForwardList.Count == 0)
@@ -163,10 +171,129 @@ namespace Files.Navigation
             {
                 App.ViewModel.TextState.isVisible = Visibility.Collapsed;
                 App.ViewModel.FilesAndFolders.Clear();
-                App.ViewModel.AddItemsToCollectionAsync(History.ForwardList[History.ForwardList.Count() - 1], PhotoAlbum.PAPageName);     // To take into account the correct index without interference from the folder being navigated to
-                App.PathText.Text = History.ForwardList[History.ForwardList.Count() - 1];
+
+
+                if ((History.ForwardList[History.ForwardList.Count() - 1]) == Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory))
+                {
+                    App.PathText.Text = "Desktop";
+                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    {
+                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "DesktopIC")
+                        {
+                            MainPage.Select.itemSelected = NavItemChoice;
+                            break;
+                        }
+                    }
+                    MainPage.accessibleContentFrame.Navigate(typeof(GenericFileBrowser), YourHome.DesktopPath, new SuppressNavigationTransitionInfo());
+                    MainPage.accessibleAutoSuggestBox.PlaceholderText = "Search Desktop";
+                }
+                else if ((History.ForwardList[History.ForwardList.Count() - 1]) == Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments))
+                {
+                    App.PathText.Text = "Documents";
+                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    {
+                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "DocumentsIC")
+                        {
+                            MainPage.Select.itemSelected = NavItemChoice;
+                            break;
+                        }
+                    }
+                    MainPage.accessibleContentFrame.Navigate(typeof(GenericFileBrowser), YourHome.DocumentsPath, new SuppressNavigationTransitionInfo());
+                    MainPage.accessibleAutoSuggestBox.PlaceholderText = "Search Documents";
+                }
+                else if ((History.ForwardList[History.ForwardList.Count() - 1]) == (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads"))
+                {
+                    App.PathText.Text = "Downloads";
+                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    {
+                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "DownloadsIC")
+                        {
+                            MainPage.Select.itemSelected = NavItemChoice;
+                            break;
+                        }
+                    }
+                    MainPage.accessibleContentFrame.Navigate(typeof(GenericFileBrowser), YourHome.DownloadsPath, new SuppressNavigationTransitionInfo());
+                    MainPage.accessibleAutoSuggestBox.PlaceholderText = "Search Downloads";
+                }
+                else if ((History.ForwardList[History.ForwardList.Count() - 1]) == Environment.GetFolderPath(Environment.SpecialFolder.MyPictures))
+                {
+                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    {
+                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "PicturesIC")
+                        {
+                            MainPage.Select.itemSelected = NavItemChoice;
+                            break;
+                        }
+                    }
+                    MainPage.accessibleContentFrame.Navigate(typeof(PhotoAlbum), YourHome.PicturesPath, new SuppressNavigationTransitionInfo());
+                    MainPage.accessibleAutoSuggestBox.PlaceholderText = "Search Pictures";
+                    App.PathText.Text = "Pictures";
+                }
+                else if ((History.ForwardList[History.ForwardList.Count() - 1]) == Environment.GetFolderPath(Environment.SpecialFolder.MyMusic))
+                {
+                    App.PathText.Text = "Music";
+                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    {
+                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "MusicIC")
+                        {
+                            MainPage.Select.itemSelected = NavItemChoice;
+                            break;
+                        }
+                    }
+                    MainPage.accessibleContentFrame.Navigate(typeof(GenericFileBrowser), YourHome.MusicPath, new SuppressNavigationTransitionInfo());
+                    MainPage.accessibleAutoSuggestBox.PlaceholderText = "Search Music";
+                }
+                else if ((History.ForwardList[History.ForwardList.Count() - 1]) == (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\OneDrive"))
+                {
+                    App.PathText.Text = "OneDrive";
+                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    {
+                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "OneD_IC")
+                        {
+                            MainPage.Select.itemSelected = NavItemChoice;
+                            break;
+                        }
+                    }
+                    MainPage.accessibleContentFrame.Navigate(typeof(GenericFileBrowser), YourHome.OneDrivePath, new SuppressNavigationTransitionInfo());
+                    MainPage.accessibleAutoSuggestBox.PlaceholderText = "Search OneDrive";
+                }
+                else if ((History.ForwardList[History.ForwardList.Count() - 1]) == Environment.GetFolderPath(Environment.SpecialFolder.MyVideos))
+                {
+                    App.PathText.Text = "Videos";
+                    foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                    {
+                        if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Name.ToString() == "VideosIC")
+                        {
+                            MainPage.Select.itemSelected = NavItemChoice;
+                            break;
+                        }
+                    }
+                    MainPage.accessibleContentFrame.Navigate(typeof(GenericFileBrowser), YourHome.VideosPath, new SuppressNavigationTransitionInfo());
+                    MainPage.accessibleAutoSuggestBox.PlaceholderText = "Search Videos";
+                }
+                else
+                {
+                    Debug.WriteLine("Debug: " + ("Removable Drive (" + History.ForwardList[History.ForwardList.Count() - 1].Split('\\')[0] + "\\)"));
+                    if (!History.ForwardList[History.ForwardList.Count() - 1].Split('\\')[0].Contains("C:\\"))
+                    {
+                        foreach (Microsoft.UI.Xaml.Controls.NavigationViewItemBase NavItemChoice in MainPage.nv.MenuItems)
+                        {
+                            if (NavItemChoice is Microsoft.UI.Xaml.Controls.NavigationViewItem && NavItemChoice.Content.ToString() == ("Removable Drive (" + History.ForwardList[History.ForwardList.Count() - 1].Split('\\')[0] + "\\)"))
+                            {
+
+                                MainPage.Select.itemSelected = NavItemChoice;
+                                break;
+                            }
+                        }
+                    }
+                    App.PathText.Text = (History.ForwardList[History.ForwardList.Count() - 1]);
+                    App.ViewModel.AddItemsToCollectionAsync(History.ForwardList[History.ForwardList.Count() - 1], PhotoAlbum.PAPageName); // To take into account the correct index without interference from the folder being navigated to
+                
+                }
+
+
+
                 History.ForwardList.RemoveAt(History.ForwardList.Count() - 1);
-                ArrayDiag.DumpForwardArray();
 
                 if (History.ForwardList.Count == 0)
                 {
