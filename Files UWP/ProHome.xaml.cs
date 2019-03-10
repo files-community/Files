c1b32578e5fe1ef3fbe9cf906026c0df5671f1e9
Luke Blevins <lukeblevins15@gmail.com>
2019-03-10 17:58:57 -0400

    Tweaked Settings Layout

---
+++ Files UWP/ProHome.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Files
+{
+    /// <summary>
+    /// This is not finished yet. This is the work that was started on having multiple Tabs
+    /// </summary>
+    public sealed partial class ProHome : Page
+    {
+        ObservableCollection<Tab> tabList = new ObservableCollection<Tab>();
+        public static ObservableCollection<Tab> TabList { get; set; } = new ObservableCollection<Tab>();
+        public ProHome()
+        {
+            this.InitializeComponent();
+            TabList.Clear();
+            TabList.Add(new Tab() { TabName = "Home", TabContent = "local:MainPage" });
+        }
+
+        private void TabView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedTab = e.AddedItems as TabViewItem;
+            
+        }
+    }
+
+    public class Tab
+    {
+        public string TabName { get; set; }
+        public string TabContent { get; set; }
+    }
+}
--- Files UWP/SettingsPages/Preferences.xaml.cs
+++ Files UWP/SettingsPages/Preferences.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Windows.Storage;
 using Windows.UI.Xaml.Controls;
-
+using System;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+using System.IO;
 
 namespace Files.SettingsPages
 {
@@ -28,6 +31,40 @@ namespace Files.SettingsPages
                 AutoRefreshSwitch.IsOn = false;
             }
 
+            if (localSettings.Values["customLocationsSetting"] != null)
+            {
+                if (localSettings.Values["customLocationsSetting"].Equals(true))
+                {
+                    CustomLocationToggle.IsOn = true;
+                    DownloadsL.IsEnabled = true;
+                    DocumentsL.IsEnabled = true;
+                    PictureL.IsEnabled = true;
+                    MusicL.IsEnabled = true;
+                    VideosL.IsEnabled = true;
+                    SaveCustomL.IsEnabled = true;
+                }
+                else
+                {
+                    CustomLocationToggle.IsOn = false;
+                    DownloadsL.IsEnabled = false;
+                    DocumentsL.IsEnabled = false;
+                    PictureL.IsEnabled = false;
+                    MusicL.IsEnabled = false;
+                    VideosL.IsEnabled = false;
+                    SaveCustomL.IsEnabled = false;
+                }
+            }
+            else
+            {
+                CustomLocationToggle.IsOn = false;
+                DownloadsL.IsEnabled = false;
+                DocumentsL.IsEnabled = false;
+                PictureL.IsEnabled = false;
+                MusicL.IsEnabled = false;
+                VideosL.IsEnabled = false;
+                SaveCustomL.IsEnabled = false;
+            }
+            SuccessMark.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
         }
 
         private void AutoRefreshSwitch_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
@@ -41,6 +78,146 @@ namespace Files.SettingsPages
             {
                 localSettings.Values["autoRefreshEnabledSetting"] = false;
             }
+        }
+
+        private void ToggleSwitch_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if ((sender as ToggleSwitch).IsOn)
+            {
+                localSettings.Values["customLocationsSetting"] = true;
+                DownloadsL.IsEnabled = true;
+                DocumentsL.IsEnabled = true;
+                PictureL.IsEnabled = true;
+                MusicL.IsEnabled = true;
+                VideosL.IsEnabled = true;
+                SaveCustomL.IsEnabled = true;
+            }
+            else
+            {
+                localSettings.Values["customLocationsSetting"] = false;
+                DownloadsL.IsEnabled = false;
+                DocumentsL.IsEnabled = false;
+                PictureL.IsEnabled = false;
+                MusicL.IsEnabled = false;
+                VideosL.IsEnabled = false;
+                SaveCustomL.IsEnabled = false;
+            }
+        }
+
+        private async void SaveCustomL_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            StorageFolder newLocationSetting;
+            if(DownloadsL.Text != null)
+            {
+                try
+                {
+                    newLocationSetting = await StorageFolder.GetFolderFromPathAsync(DownloadsL.Text);
+                    localSettings.Values["DownloadsLocation"] = DownloadsL.Text;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    await MainPage.permissionBox.ShowAsync();
+                    return;
+                }
+                catch (ArgumentException)
+                {
+                    DownloadsL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+                catch (FileNotFoundException)
+                {
+                    DownloadsL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+            }
+
+            if (DocumentsL.Text != null)
+            {
+                try
+                {
+                    newLocationSetting = await StorageFolder.GetFolderFromPathAsync(DocumentsL.Text);
+                    localSettings.Values["DocumentsLocation"] = DocumentsL.Text;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    await MainPage.permissionBox.ShowAsync();
+                    return;
+                }
+                catch (ArgumentException)
+                {
+                    DocumentsL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+                catch (FileNotFoundException)
+                {
+                    DocumentsL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+            }
+
+            if (PictureL.Text != null)
+            {
+                try
+                {
+                    newLocationSetting = await StorageFolder.GetFolderFromPathAsync(PictureL.Text);
+                    localSettings.Values["PicturesLocation"] = PictureL.Text;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    await MainPage.permissionBox.ShowAsync();
+                    return;
+                }
+                catch (ArgumentException)
+                {
+                    PictureL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+                catch (FileNotFoundException)
+                {
+                    PictureL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+            }
+
+            if (MusicL.Text != null)
+            {
+                try
+                {
+                    newLocationSetting = await StorageFolder.GetFolderFromPathAsync(MusicL.Text);
+                    localSettings.Values["MusicLocation"] = MusicL.Text;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    await MainPage.permissionBox.ShowAsync();
+                    return;
+                }
+                catch (ArgumentException)
+                {
+                    MusicL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+                catch (FileNotFoundException)
+                {
+                    MusicL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+            }
+
+            if (VideosL.Text != null)
+            {
+                try
+                {
+                    newLocationSetting = await StorageFolder.GetFolderFromPathAsync(VideosL.Text);
+                    localSettings.Values["VideosLocation"] = VideosL.Text;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    await MainPage.permissionBox.ShowAsync();
+                    return;
+                }
+                catch (ArgumentException)
+                {
+                    VideosL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+                catch (FileNotFoundException)
+                {
+                    VideosL.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
+                }
+            }
+
+            SuccessMark.Visibility = Windows.UI.Xaml.Visibility.Visible;
         }
     }
 }
