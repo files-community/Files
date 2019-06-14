6cefe25bf6a815bd1c4dc063295aa652eb50f9ef
duke7553 <lukeblevins15@gmail.com>
2019-06-13 21:44:17 -0400

    Tweaks to Properties Dialog Design

--- Files UWP/Properties.xaml.cs
+++ Files UWP/Properties.xaml.cs
@@ -15,19 +15,14 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Files
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class Properties : Page
     {
         public Properties()
         {
             this.InitializeComponent();
-            CancelButton.Click += ItemViewModel<GenericFileBrowser>.GetCurrentSelectedTabInstance<ProHome>().PropertiesWindow_CancelButton_Click;
         }
 
         private void OK_Button_Click(object sender, RoutedEventArgs e)
