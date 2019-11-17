e2e52ec33b070449150ebf799fb6e3610f990c89
Luke Blevins <lukeblevins15@gmail.com>
2019-11-17 16:02:05 -0500

    Change Name tags to x:Name

--- ProcessLauncher/Program.cs
+++ ProcessLauncher/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Windows.Storage;
 
 namespace ProcessLauncher
