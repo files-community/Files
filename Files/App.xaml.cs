fe34d08f87c2de82e7d91f65e8b1fe11de23245e
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2019-12-17 09:27:25 -0500

    OneDrive now uses the environment variable path

--- Files/App.xaml.cs
+++ Files/App.xaml.cs
@@ -37,7 +37,7 @@ namespace Files
         public static string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
         public static string DocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public static string DownloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads";
-        public static string OneDrivePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\OneDrive";
+        public static string OneDrivePath = Environment.GetEnvironmentVariable("OneDrive");
         public static string PicturesPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
         public static string MusicPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
         public static string VideosPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
