d7c6bf99894d5c0a7a12ddd280bb2d6cadb4351d
Yair Aichenbaum <explortec@outlook.com>
2020-02-09 14:15:26 -0500

    Added flags page

---
+++ Files/SettingsPages/Flags.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using System;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+using System.IO;
+using Files.Filesystem;
+using Newtonsoft.Json;
+using Files.DataModels;
+using System.Collections.ObjectModel;
+using Windows.System;
+using Windows.UI.Xaml.Navigation;
+using System.Linq;
+
+namespace Files.SettingsPages
+{
+    
+    public sealed partial class Flags : Page
+    {
+        StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+        ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+
+        public Flags()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
