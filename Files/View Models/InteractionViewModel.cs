3256482e7f6dcc1964109283c4477db63950ff47
Yair Aichenbaum <explortec@outlook.com>
2020-03-11 13:46:06 -0400

    Added InteractionViewModel

---
+++ Files/View Models/InteractionViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Windows.UI.Xaml.Controls;
+using System;
+using Files.Interacts;
+
+namespace Files.Controls
+{
+    public class InteractionViewModel : ViewModelBase
+    {
+        private string _ToggleRibbonIcon = "";
+
+        public InteractionViewModel()
+        {
+
+        }
+
+        public string ToggleRibbonIcon
+        {
+            get => _ToggleRibbonIcon;
+            set => Set(ref _ToggleRibbonIcon, value);
+        }
+
+       
+    }
+}
