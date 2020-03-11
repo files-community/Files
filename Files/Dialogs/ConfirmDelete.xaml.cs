2c2f54d9d66889041fd7b4ecb8e1016f3d9ffc21
Yair Aichenbaum <explortec@outlook.com>
2020-03-11 13:38:32 -0400

    Added confirm delete ui

---
+++ Files/Dialogs/ConfirmDelete.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Files.Dialogs
+{
+    public sealed partial class ConfirmDialog : ContentDialog
+    {
+        public ConfirmDialog()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
