358a734e1613cb42e6b958f7adc7bc4922d6fc01
Yair Aichenbaum <explortec@outlook.com>
2020-03-19 19:13:17 -0400

    Temporary fix for settings

--- Files/Views/Pages/Settings.xaml.cs
+++ Files/Views/Pages/Settings.xaml.cs
@@ -51,6 +51,22 @@ namespace Files
 
         private void NavigationView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
         {
+            //ToDo waiting for WinUI bug to be fixed before using item invoked
+
+            //_ = SettingsPane.MenuItems.IndexOf(SettingsPane.SelectedItem) switch
+            //{
+            //    0 => SettingsContentFrame.Navigate(typeof(Appearance)),
+            //    1 => SettingsContentFrame.Navigate(typeof(OnStartup)),
+            //    //2 => SettingsContentFrame.Navigate(typeof(StartPageWidgets)),
+            //    3 => SettingsContentFrame.Navigate(typeof(Preferences)),
+            //    4 => SettingsContentFrame.Navigate(typeof(FilesAndFolders)),
+            //    5 => SettingsContentFrame.Navigate(typeof(About)),
+            //    _ => SettingsContentFrame.Navigate(typeof(Appearance))
+            //};
+        }
+
+        private void SettingsPane_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
             _ = SettingsPane.MenuItems.IndexOf(SettingsPane.SelectedItem) switch
             {
                 0 => SettingsContentFrame.Navigate(typeof(Appearance)),
