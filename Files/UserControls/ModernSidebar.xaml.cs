09da9bfe2da29dacfd1f87a5b116888c48652c88
Luke Blevins <lukeblevins15@gmail.com>
2020-03-27 15:59:47 -0400

    Modern UI Changes

---
+++ Files/UserControls/ModernSidebar.xaml.cs
@@ -0,0 +1,129 @@
+﻿using Files.Filesystem;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+
+
+namespace Files.Controls
+{
+    public sealed partial class ModernSidebar : UserControl, INotifyPropertyChanged
+    {
+        private INavigationControlItem _SelectedSidebarItem;
+        public INavigationControlItem SelectedSidebarItem
+        {
+            get
+            {
+                return _SelectedSidebarItem;
+            }
+            set
+            {
+                if (value != _SelectedSidebarItem)
+                {
+                    _SelectedSidebarItem = value;
+                    NotifyPropertyChanged("SelectedSidebarItem");
+                }
+            }
+        }
+
+        public ModernSidebar()
+        {
+            this.InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Sidebar_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.HomeItems.isEnabled = false;
+            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.ShareItems.isEnabled = false;
+
+            if (args.InvokedItem == null)
+            {
+                return;
+            }
+
+            switch ((args.InvokedItemContainer.DataContext as INavigationControlItem).ItemType)
+            {
+                case NavigationControlItemType.Location:
+                    {
+                        var ItemPath = (args.InvokedItemContainer.DataContext as INavigationControlItem).Path; // Get the path of the invoked item
+
+                        if (ItemPath.Equals("Home", StringComparison.OrdinalIgnoreCase)) // Home item
+                        {
+                            App.CurrentInstance.ContentFrame.Navigate(typeof(YourHome), "New tab", new SuppressNavigationTransitionInfo());
+
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.HomeItems.isEnabled = false;
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.ShareItems.isEnabled = false;
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.LayoutItems.isEnabled = false;
+                        }
+                        else if (ItemPath.Equals(App.AppSettings.PicturesPath, StringComparison.OrdinalIgnoreCase)) // Photo item
+                        {
+                            App.CurrentInstance.ContentFrame.Navigate(typeof(PhotoAlbum), args.InvokedItemContainer.Tag.ToString(), new SuppressNavigationTransitionInfo());
+
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.HomeItems.isEnabled = false;
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.ShareItems.isEnabled = false;
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.LayoutItems.isEnabled = true;
+
+                        }
+                        else // Any other item
+                        {
+                            App.CurrentInstance.ContentFrame.Navigate(typeof(GenericFileBrowser), args.InvokedItemContainer.Tag.ToString(), new SuppressNavigationTransitionInfo());
+
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.HomeItems.isEnabled = false;
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.ShareItems.isEnabled = false;
+                            (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.LayoutItems.isEnabled = true;
+                        }
+
+                        break;
+                    }
+                case NavigationControlItemType.OneDrive:
+                    {
+                        App.CurrentInstance.ContentFrame.Navigate(typeof(GenericFileBrowser), App.AppSettings.OneDrivePath, new SuppressNavigationTransitionInfo());
+                        break;
+                    }
+                default:
+                    {
+                        var clickedItem = args.InvokedItemContainer;
+
+                        App.CurrentInstance.ContentFrame.Navigate(typeof(GenericFileBrowser), clickedItem.Tag.ToString(), new SuppressNavigationTransitionInfo());
+                        App.CurrentInstance.NavigationToolbar.PathControlDisplayText = clickedItem.Tag.ToString();
+                        (App.CurrentInstance.OperationsControl as RibbonArea).RibbonViewModel.LayoutItems.isEnabled = true;
+
+                        break;
+                    }
+            }
+
+            App.CurrentInstance.NavigationToolbar.PathControlDisplayText = App.CurrentInstance.ViewModel.Universal.WorkingDirectory;
+        }
+
+        private void NavigationViewItem_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            Microsoft.UI.Xaml.Controls.NavigationViewItem sidebarItem = (Microsoft.UI.Xaml.Controls.NavigationViewItem)sender;
+            var item = sidebarItem.DataContext as LocationItem;
+            if (!item.IsDefaultLocation)
+            {
+                SideBarItemContextFlyout.ShowAt(sidebarItem, e.GetPosition(sidebarItem));
+                App.rightClickedItem = item;
+            }
+        }
+    }
+}
---
+++ Files/Views/Pages/ModernShellPage.xaml.cs
@@ -0,0 +1,298 @@
+﻿using Files.Controls;
+using Files.Filesystem;
+using Files.Interacts;
+using Files.UserControls;
+using Files.View_Models;
+using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Files.Views.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ModernShellPage : Page, IShellPage
+    {
+        public ModernShellPage()
+        {
+            this.InitializeComponent();
+        }
+
+        Frame IShellPage.ContentFrame => throw new NotImplementedException();
+
+        Interaction IShellPage.InteractionOperations => throw new NotImplementedException();
+
+        ItemViewModel IShellPage.ViewModel => throw new NotImplementedException();
+
+        BaseLayout IShellPage.ContentPage => throw new NotImplementedException();
+
+        Control IShellPage.OperationsControl => throw new NotImplementedException();
+
+        Type IShellPage.CurrentPageType => throw new NotImplementedException();
+
+        INavigationControlItem IShellPage.SidebarSelectedItem { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        INavigationToolbar IShellPage.NavigationToolbar => throw new NotImplementedException();
+
+        private bool _isSwiped;
+        private void SwipeablePage_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            if (e.IsInertial && !_isSwiped)
+            {
+                var swipedDistance = e.Cumulative.Translation.X;
+
+                if (Math.Abs(swipedDistance) <= 2) return;
+
+                if (swipedDistance > 0)
+                {
+                    NavigationActions.Back_Click(null, null);
+                }
+                else
+                {
+                    NavigationActions.Forward_Click(null, null);
+                }
+                _isSwiped = true;
+            }
+        }
+
+        private void SwipeablePage_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            _isSwiped = false;
+        }
+        private async void DisplayFilesystemConsentDialog()
+        {
+            await App.consentDialog.ShowAsync(ContentDialogPlacement.Popup);
+        }
+
+        string NavParams = null;
+
+        protected override void OnNavigatedTo(NavigationEventArgs eventArgs)
+        {
+            base.OnNavigatedTo(eventArgs);
+            NavParams = eventArgs.Parameter.ToString();
+        }
+
+        private ItemViewModel viewModel = null;
+        private Interaction interactionOperation = null;
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel = new ItemViewModel();
+            interactionOperation = new Interaction();
+
+            switch (NavParams)
+            {
+                case "Start":
+                    ItemDisplayFrame.Navigate(typeof(YourHome), NavParams, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems[0];
+                    break;
+                case "New tab":
+                    ItemDisplayFrame.Navigate(typeof(YourHome), NavParams, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems[0];
+                    break;
+                case "Desktop":
+                    ItemDisplayFrame.Navigate(typeof(GenericFileBrowser), App.AppSettings.DesktopPath, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems.First(x => x.Path.Equals(App.AppSettings.DesktopPath, StringComparison.OrdinalIgnoreCase));
+                    break;
+                case "Downloads":
+                    ItemDisplayFrame.Navigate(typeof(GenericFileBrowser), App.AppSettings.DownloadsPath, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems.First(x => x.Path.Equals(App.AppSettings.DownloadsPath, StringComparison.OrdinalIgnoreCase));
+                    break;
+                case "Documents":
+                    ItemDisplayFrame.Navigate(typeof(GenericFileBrowser), App.AppSettings.DocumentsPath, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems.First(x => x.Path.Equals(App.AppSettings.DocumentsPath, StringComparison.OrdinalIgnoreCase));
+                    break;
+                case "Pictures":
+                    ItemDisplayFrame.Navigate(typeof(PhotoAlbum), App.AppSettings.PicturesPath, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems.First(x => x.Path.Equals(App.AppSettings.PicturesPath, StringComparison.OrdinalIgnoreCase));
+                    break;
+                case "Music":
+                    ItemDisplayFrame.Navigate(typeof(GenericFileBrowser), App.AppSettings.MusicPath, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems.First(x => x.Path.Equals(App.AppSettings.MusicPath, StringComparison.OrdinalIgnoreCase));
+                    break;
+                case "Videos":
+                    ItemDisplayFrame.Navigate(typeof(GenericFileBrowser), App.AppSettings.VideosPath, new SuppressNavigationTransitionInfo());
+                    SidebarControl.SelectedSidebarItem = App.sideBarItems.First(x => x.Path.Equals(App.AppSettings.VideosPath, StringComparison.OrdinalIgnoreCase));
+                    break;
+
+                default:
+                    if (NavParams[0] >= 'A' && NavParams[0] <= 'Z' && NavParams[1] == ':')
+                    {
+                        ItemDisplayFrame.Navigate(typeof(GenericFileBrowser), NavParams, new SuppressNavigationTransitionInfo());
+                        SidebarControl.SelectedSidebarItem = SettingsViewModel.foundDrives.First(x => x.tag.ToString().Equals($"{NavParams[0]}:\\", StringComparison.OrdinalIgnoreCase));
+                    }
+                    else
+                    {
+                        SidebarControl.SelectedSidebarItem = null;
+                    }
+                    break;
+            }
+
+            this.Loaded -= Page_Loaded;
+        }
+
+
+        private void ItemDisplayFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (ItemDisplayFrame.CurrentSourcePageType == typeof(GenericFileBrowser))
+            {
+                // Reset DataGrid Rows that may be in "cut" command mode
+                App.CurrentInstance.InteractionOperations.dataGridRows.Clear();
+                Interaction.FindChildren<DataGridRow>(App.CurrentInstance.InteractionOperations.dataGridRows, (ItemDisplayFrame.Content as GenericFileBrowser).AllView);
+                foreach (DataGridRow dataGridRow in App.CurrentInstance.InteractionOperations.dataGridRows)
+                {
+                    if ((ItemDisplayFrame.Content as GenericFileBrowser).AllView.Columns[0].GetCellContent(dataGridRow).Opacity < 1)
+                    {
+                        (ItemDisplayFrame.Content as GenericFileBrowser).AllView.Columns[0].GetCellContent(dataGridRow).Opacity = 1;
+                    }
+                }
+            }
+            RibbonArea.Focus(FocusState.Programmatic);
+        }
+
+        public void UpdateProgressFlyout(InteractionOperationType operationType, int amountComplete, int amountTotal)
+        {
+            this.FindName("ProgressFlyout");
+
+            string operationText = null;
+            switch (operationType)
+            {
+                case InteractionOperationType.PasteItems:
+                    operationText = "Completing Paste";
+                    break;
+                case InteractionOperationType.DeleteItems:
+                    operationText = "Deleting Items";
+                    break;
+            }
+            ProgressFlyoutTextBlock.Text = operationText + " (" + amountComplete + "/" + amountTotal + ")" + "...";
+            ProgressFlyoutProgressBar.Value = amountComplete;
+            ProgressFlyoutProgressBar.Maximum = amountTotal;
+
+            if (amountComplete == amountTotal)
+            {
+                UnloadObject(ProgressFlyout);
+            }
+        }
+
+        private async void ProHomeInstance_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            var ctrl = Window.Current.CoreWindow.GetKeyState(VirtualKey.Control).HasFlag(CoreVirtualKeyStates.Down);
+            var alt = Window.Current.CoreWindow.GetKeyState(VirtualKey.Menu).HasFlag(CoreVirtualKeyStates.Down);
+            var shift = Window.Current.CoreWindow.GetKeyState(VirtualKey.Shift).HasFlag(CoreVirtualKeyStates.Down);
+            var tabInstance = App.CurrentInstance != null;
+
+            //switch (c: ctrl, s: shift, a: alt, t: tabInstance, k: e.Key)
+            //{
+            //    case (true, true, false, true, VirtualKey.N): //ctrl + shift + n, new item
+            //        await App.addItemDialog.ShowAsync();
+            //        break;
+            //    case (false, true, false, true, VirtualKey.Delete): //shift + delete, PermanentDelete
+            //        if (!App.CurrentInstance.NavigationToolbar.IsEditModeEnabled)
+            //            App.InteractionViewModel.PermanentlyDelete = true;
+            //        App.CurrentInstance.InteractionOperations.DeleteItem_Click(null, null);
+            //        break;
+            //    case (true, false, false, true, VirtualKey.C): //ctrl + c, copy
+            //        if (!App.CurrentInstance.NavigationToolbar.IsEditModeEnabled)
+            //            App.CurrentInstance.InteractionOperations.CopyItem_ClickAsync(null, null);
+            //        break;
+            //    case (true, false, false, true, VirtualKey.V): //ctrl + v, paste
+            //        if (!App.CurrentInstance.NavigationToolbar.IsEditModeEnabled)
+            //            App.CurrentInstance.InteractionOperations.PasteItem_ClickAsync(null, null);
+            //        break;
+            //    case (true, false, false, true, VirtualKey.X): //ctrl + x, cut
+            //        if (!App.CurrentInstance.NavigationToolbar.IsEditModeEnabled)
+            //            App.CurrentInstance.InteractionOperations.CutItem_Click(null, null);
+            //        break;
+            //    case (true, false, false, true, VirtualKey.A): //ctrl + a, select all
+            //        if (!App.CurrentInstance.NavigationToolbar.IsEditModeEnabled)
+            //            App.CurrentInstance.InteractionOperations.SelectAllItems();
+            //        break;
+            //    case (true, false, false, true, VirtualKey.N): //ctrl + n, new window
+            //        App.CurrentInstance.InteractionOperations.LaunchNewWindow();
+            //        break;
+            //    case (true, false, false, true, VirtualKey.W): //ctrl + w, close tab
+            //        App.CurrentInstance.InteractionOperations.CloseTab();
+            //        break;
+            //    case (true, false, false, true, VirtualKey.F4): //ctrl + F4, close tab
+            //        App.CurrentInstance.InteractionOperations.CloseTab();
+            //        break;
+            //    case (false, false, false, true, VirtualKey.Delete): //delete, delete item
+            //        if (!App.CurrentInstance.NavigationToolbar.IsEditModeEnabled)
+            //            App.CurrentInstance.InteractionOperations.DeleteItem_Click(null, null);
+            //        break;
+            //    case (false, false, false, true, VirtualKey.Space): //space, quick look
+            //        if (!App.CurrentInstance.NavigationToolbar.IsEditModeEnabled)
+            //        {
+            //            if ((App.CurrentInstance.ContentPage).IsQuickLookEnabled)
+            //            {
+            //                App.CurrentInstance.InteractionOperations.ToggleQuickLook();
+            //            }
+            //        }
+            //        break;
+            //    case (false, false, true, true, VirtualKey.Left): //alt + back arrow, backward
+            //        NavigationActions.Back_Click(null, null);
+            //        break;
+            //    case (false, false, true, true, VirtualKey.Right): //alt + right arrow, forward
+            //        NavigationActions.Forward_Click(null, null);
+            //        break;
+            //    case (true, false, false, true, VirtualKey.R): //ctrl + r, refresh
+            //        NavigationActions.Refresh_Click(null, null);
+            //        break;
+            //    case (true, false, false, true, VirtualKey.F): //ctrl + f, search box
+            //        (App.CurrentInstance.OperationsControl as RibbonArea).RibbonTabView.SelectedIndex = 0;
+            //        break;
+            //    case (true, false, false, true, VirtualKey.E): //ctrl + e, search box
+            //        (App.CurrentInstance.OperationsControl as RibbonArea).RibbonTabView.SelectedIndex = 0;
+            //        break;
+            //    case (false, false, true, true, VirtualKey.H):
+            //        (App.CurrentInstance.OperationsControl as RibbonArea).RibbonTabView.SelectedIndex = 1;
+            //        break;
+            //    case (false, false, true, true, VirtualKey.S):
+            //        (App.CurrentInstance.OperationsControl as RibbonArea).RibbonTabView.SelectedIndex = 2;
+            //        break;
+            //    case (false, false, true, true, VirtualKey.V):
+            //        (App.CurrentInstance.OperationsControl as RibbonArea).RibbonTabView.SelectedIndex = 3;
+            //        break;
+            //};
+
+
+            if (App.CurrentInstance.CurrentPageType == typeof(PhotoAlbum))
+            {
+                switch (e.Key)
+                {
+                    case VirtualKey.F2: //F2, rename
+                        if ((App.CurrentInstance.ContentPage).SelectedItems.Count > 0)
+                        {
+                            App.CurrentInstance.InteractionOperations.RenameItem_Click(null, null);
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    public enum InteractionOperationType
+    {
+        PasteItems = 0,
+        DeleteItems = 1,
+    }
+}
+
