4e9f054d80ae7889e2b395f75966ccb9fc8cf5a5
Yair Aichenbaum <explortec@outlook.com>
2020-03-29 13:39:52 -0400

    Cleaned up some unused references

--- Files/UserControls/ModernSidebar.xaml.cs
+++ Files/UserControls/ModernSidebar.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using Files.Filesystem;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
-using Windows.UI.Xaml.Navigation;
-
-
 
 namespace Files.Controls
 {
