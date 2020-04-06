1f81ae15e61d4395e933282d4daed3de6f059333
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2020-04-06 11:01:13 -0400

    v0.7.4 (#510)

--- Files/Properties.xaml.cs
+++ Files/Properties.xaml.cs
@@ -1,25 +1,67 @@
-﻿using Files.Filesystem;
-using System.Collections.Generic;
+﻿using Files.Interacts;
+using System;
+using Windows.Foundation.Metadata;
+using Windows.Storage;
+using Windows.UI.WindowManagement;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace Files
 {
 
     public sealed partial class Properties : Page
     {
-        ListedItem Item { get; set; }
-        ContentDialog PropertiesDialog { get; set; }
+        public AppWindow propWindow;
+
         public Properties()
         {
             this.InitializeComponent();
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
+            {
+                Loaded += Properties_Loaded;
+            }
+            else
+            {
+                this.OKButton.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private void Properties_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            base.OnNavigatedTo(e);
-            Item = e.Parameter as ListedItem;
-            PropertiesDialog = Frame.Tag as ContentDialog;
+            // Collect AppWindow-specific info
+            propWindow = Interaction.AppWindows[this.UIContext];
+        }
+
+        private async void itemIcon_Loading(Windows.UI.Xaml.FrameworkElement sender, object args)
+        {
+            if (App.CurrentInstance.ContentPage.SelectedItem != null)
+            {
+                if (App.CurrentInstance.ContentPage.SelectedItem.FolderImg != Windows.UI.Xaml.Visibility.Visible)
+                {
+                    var thumbnail = await (await StorageFile.GetFileFromPathAsync(App.CurrentInstance.ContentPage.SelectedItem.FilePath)).GetThumbnailAsync(Windows.Storage.FileProperties.ThumbnailMode.SingleItem, 40, Windows.Storage.FileProperties.ThumbnailOptions.ResizeThumbnail);
+                    var bitmap = new BitmapImage();
+                    await bitmap.SetSourceAsync(thumbnail);
+                    itemIcon.Source = bitmap;
+                }
+                else
+                {
+                    EmptyImageIcon.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                EmptyImageIcon.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+
+
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
+            {
+                await propWindow.CloseAsync();
+            }
         }
     }
 }
--- Files/Views/SettingsPages/Preferences.xaml.cs
+++ Files/Views/SettingsPages/Preferences.xaml.cs
@@ -25,23 +25,6 @@ namespace Files.SettingsPages
         {
             this.InitializeComponent();
 
-            if (App.AppSettings != null && localSettings.Values["customLocationsSetting"] != null)
-            {
-                if (localSettings.Values["customLocationsSetting"].Equals(true))
-                {
-                    CustomLocationToggle.IsOn = true;                
-                }
-                else
-                {
-                    aaaa.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-                }
-            }
-            else
-            {
-                CustomLocationToggle.IsOn = false;
-                aaaa.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-            }
-
             try
             {
                 StorageFolder.GetFolderFromPathAsync(App.AppSettings.OneDrivePath);
@@ -63,45 +46,14 @@ namespace Files.SettingsPages
             TerminalApplicationsComboBox.SelectedItem = App.AppSettings.Terminals.Single(p => p.Id == terminalId);
         }
 
-        private void CustomLocationToggle_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            if ((sender as ToggleSwitch).IsOn)
-            {
-                localSettings.Values["customLocationsSetting"] = true;
-
-                localSettings.Values["DesktopLocation"] = App.AppSettings.DesktopPath;
-
-                localSettings.Values["DownloadsLocation"] = App.AppSettings.DownloadsPath;
-
-                localSettings.Values["DocumentsLocation"] = App.AppSettings.DocumentsPath;
-
-                localSettings.Values["PicturesLocation"] = App.AppSettings.PicturesPath;
-
-                localSettings.Values["MusicLocation"] = App.AppSettings.MusicPath;
-
-                localSettings.Values["VideosLocation"] = App.AppSettings.VideosPath;
-
-                localSettings.Values["OneDriveLocation"] = App.AppSettings.OneDrivePath;
-
-                aaaa.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            }
-            else
-            {
-                localSettings.Values["customLocationsSetting"] = false;
-
-                aaaa.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
-
-            }
-        }
-
-        private async void EditTerminalApplications_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void EditTerminalApplications_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             LaunchTerminalsConfigFile();
         }
 
         private async void LaunchTerminalsConfigFile()
         {
-            Launcher.LaunchFileAsync(await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appdata:///local/settings/terminal.json")));
+            await Launcher.LaunchFileAsync(await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appdata:///local/settings/terminal.json")));
         }
 
         private void TerminalApp_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -118,69 +70,6 @@ namespace Files.SettingsPages
             OneDrivePin.IsEnabled = false;
             App.AppSettings.PinOneDriveToSideBar = OneDrivePin.IsOn;
             OneDrivePin.IsEnabled = true;
-        }
-
-        private async void btnBrowse_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
-            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
-            folderPicker.FileTypeFilter.Add("*");
-
-            Windows.Storage.StorageFolder folder = await folderPicker.PickSingleFolderAsync();
-            if (folder != null)
-            {
-                // Application now has read/write access to all contents in the picked folder
-                // (including other sub-folder contents)
-                Windows.Storage.AccessCache.StorageApplicationPermissions.
-                FutureAccessList.AddOrReplace("PickedFolderToken", folder);
-
-                var item = ((Button)sender).Tag;
-                switch (item)
-                {
-                    case "1":
-                        {
-                            localSettings.Values["DesktopLocation"] = folder.Name;
-                            App.AppSettings.DesktopPath = folder.Name;
-                            break;
-                        }
-                    case "2":
-                        {
-                            localSettings.Values["DownloadsLocation"] = folder.Name;
-                            App.AppSettings.DownloadsPath = folder.Name;
-                            break;
-                        }
-                    case "3":
-                        {
-                            localSettings.Values["DocumentsLocation"] = folder.Name;
-                            App.AppSettings.DocumentsPath = folder.Name;
-                            break;
-                        }
-                    case "4":
-                        {
-                            localSettings.Values["PicturesLocation"] = folder.Name;
-                            App.AppSettings.PicturesPath = folder.Name;
-                            break;
-                        }
-                    case "5":
-                        {
-                            localSettings.Values["MusicLocation"] = folder.Name;
-                            App.AppSettings.MusicPath = folder.Name;
-                            break;
-                        }
-                    case "6":
-                        {
-                            localSettings.Values["VideosLocation"] = folder.Name;
-                            App.AppSettings.VideosPath = folder.Name;
-                            break;
-                        }
-                    case "7":
-                        {
-                            localSettings.Values["OneDriveLocation"] = folder.Name;
-                            App.AppSettings.OneDrivePath = folder.Name;
-                            break;
-                        }
-                }
-            }
         }
     }
 }
