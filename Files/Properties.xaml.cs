716cd9a3dce8d8254c275c60817aa72dc6959a84
Luke Blevins <lukeblevins15@gmail.com>
2020-04-06 02:17:39 -0400

    Improve Properties Dialog Information Density

--- Files/Properties.xaml.cs
+++ Files/Properties.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using Files.Filesystem;
+using System;
 using System.Collections.Generic;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 namespace Files
@@ -8,18 +12,33 @@ namespace Files
 
     public sealed partial class Properties : Page
     {
-        ListedItem Item { get; set; }
-        ContentDialog PropertiesDialog { get; set; }
         public Properties()
         {
             this.InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private async void itemIcon_Loading(Windows.UI.Xaml.FrameworkElement sender, object args)
         {
-            base.OnNavigatedTo(e);
-            Item = e.Parameter as ListedItem;
-            PropertiesDialog = Frame.Tag as ContentDialog;
+            if (App.CurrentInstance.ContentPage.SelectedItem != null)
+            {
+                if (App.CurrentInstance.ContentPage.SelectedItem.FolderImg != Windows.UI.Xaml.Visibility.Visible)
+                {
+                    var thumbnail = await (await StorageFile.GetFileFromPathAsync(App.CurrentInstance.ContentPage.SelectedItem.FilePath)).GetThumbnailAsync(Windows.Storage.FileProperties.ThumbnailMode.SingleItem, 40, Windows.Storage.FileProperties.ThumbnailOptions.ResizeThumbnail);
+                    var bitmap = new BitmapImage();
+                    await bitmap.SetSourceAsync(thumbnail);
+                    itemIcon.Source = bitmap;
+                }
+                else
+                {
+                    EmptyImageIcon.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                EmptyImageIcon.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+
+
         }
     }
 }
