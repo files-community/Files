0ba5ae0c93e06821a03b8324ed0aab49084f7449
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2020-04-08 15:54:05 -0400

    Navigation support for terminal (#542)
    * Added support for launching any terminal profile from the navigation bar

--- Files/UserControls/NavigationToolbar/ModernNavigationToolbar.xaml.cs
+++ Files/UserControls/NavigationToolbar/ModernNavigationToolbar.xaml.cs
@@ -4,7 +4,9 @@ using Files.Views.Pages;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using Windows.ApplicationModel;
 using Windows.Storage;
 using Windows.System;
 using Windows.UI.Xaml;
@@ -240,6 +242,23 @@ namespace Files.UserControls
                         }
                         catch (Exception ex) // Not a file or not accessible
                         {
+
+                            // Launch terminal application if possible
+                            var localSettings = ApplicationData.Current.LocalSettings;
+
+                            foreach (var item in App.AppSettings.Terminals)
+                            {
+                                if (item.Path.Equals(CurrentInput, StringComparison.OrdinalIgnoreCase) || item.Path.Equals(CurrentInput + ".exe", StringComparison.OrdinalIgnoreCase))
+                                {
+                                    localSettings.Values["Application"] = item.Path;
+                                    localSettings.Values["Arguments"] = String.Format(item.arguments, App.CurrentInstance.ViewModel.WorkingDirectory);
+
+                                    await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+
+                                    return;
+                                }
+                            }
+
                             var dialog = new ContentDialog()
                             {
                                 Title = "Invalid item",
