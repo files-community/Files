d2bad8eb87f626563eda67260f9c34bef4901b77
Yair Aichenbaum <explortec@outlook.com>
2020-04-13 20:45:05 -0400

    Added drag area to settings page

--- Files/Views/Pages/Settings.xaml.cs
+++ Files/Views/Pages/Settings.xaml.cs
@@ -18,35 +18,8 @@ namespace Files
 
             var CoreTitleBar = CoreApplication.GetCurrentView().TitleBar;
             CoreTitleBar.ExtendViewIntoTitleBar = true;
-            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
-            titleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0, 255, 255, 255);
-            titleBar.ButtonHoverBackgroundColor = Color.FromArgb(75, 10, 10, 10);
-            if (App.Current.RequestedTheme == ApplicationTheme.Dark)
-            {
-                titleBar.ButtonBackgroundColor = Color.FromArgb(0, 0, 0, 0);
-                titleBar.ButtonForegroundColor = Colors.White;
-                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(75, 240, 240, 240);
-                titleBar.BackgroundColor = Color.FromArgb(255, 25, 25, 25);
-            }
-            else if (App.Current.RequestedTheme == ApplicationTheme.Light)
-            {
-                titleBar.ButtonBackgroundColor = Color.FromArgb(0, 255, 255, 255);
-                titleBar.ButtonForegroundColor = Colors.Black;
-                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(75, 155, 155, 155);
-            }
+            Window.Current.SetTitleBar(DragArea);
 
-            if (this.RequestedTheme == ElementTheme.Dark)
-            {
-                titleBar.ButtonForegroundColor = Colors.White;
-                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(75, 240, 240, 240);
-                titleBar.BackgroundColor = Color.FromArgb(255, 25, 25, 25);
-            }
-            else if (this.RequestedTheme == ElementTheme.Light)
-            {
-                titleBar.ButtonForegroundColor = Colors.Black;
-                titleBar.ButtonHoverBackgroundColor = Color.FromArgb(75, 155, 155, 155);
-                titleBar.BackgroundColor = Colors.Transparent;
-            }
             SettingsContentFrame.Navigate(typeof(Appearance));
         }
 
