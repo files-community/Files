1a3664ef2964a2af499a35ff02ef9b515efa688b
Yair Aichenbaum <explortec@outlook.com>
2020-04-14 12:32:32 -0400

    Removed update dialog

--- Files/App.xaml.cs
+++ Files/App.xaml.cs
@@ -165,18 +165,6 @@ namespace Files
             //start tracking app usage
             SystemInformation.TrackAppUse(e);
 
-            if (SystemInformation.IsAppUpdated)
-            {
-                var dialog = new ContentDialog()
-                {
-                    Title = "What's new in v0.7.5",
-                    Content = "• Fixed a crash that would sometimes occur when right clicking on a file or folder.\n• Fixed an issue where the status bar wouldn't hide on the new tab page.\n• Fixed an issue where clicking on the quick access item for home wouldn't navigate to the home page.\n• Fixed a bug that prevented users from right clicking on the navigation bar.",
-                    PrimaryButtonText = "Lets go!"
-                };
-
-                dialog.ShowAsync();
-            }
-
             Logger.Info("App launched");
 
             bool canEnablePrelaunch = Windows.Foundation.Metadata.ApiInformation.IsMethodPresent("Windows.ApplicationModel.Core.CoreApplication", "EnablePrelaunch");
