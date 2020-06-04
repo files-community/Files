51b33ab53cdb26dcfc46bf8bf35726fca9857255
Vladyslav <tsvetkov_vlad@ukr.net>
2020-06-05 00:20:28 +0300

    Remove terminal from json if terminal app has been removed
    Add support for Fluent Terminal

--- Files/DataModels/TerminalFileModel.cs
+++ Files/DataModels/TerminalFileModel.cs
@@ -1,6 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using Files.Helpers;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Files.DataModels
 {
@@ -22,6 +25,30 @@ namespace Files.DataModels
                 return Terminals.Single(x => x.Id == DefaultTerminalId);
             }
             return Terminals.First();
+        }
+
+        public void ResetToDefaultTerminal()
+        {
+            DefaultTerminalId = 1;
+        }
+
+        public async Task<bool> AddTerminal(TerminalModel terminal, string packageName)
+        {
+            bool isChanged = false;
+            bool isInstalled = await PackageHelper.IsAppInstalledAsync(packageName);
+            //Ensure terminal is not already in List
+            if (Terminals.FirstOrDefault(x => x.Path.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase)) == null && isInstalled)
+            {
+                Terminals.Add(terminal);
+                isChanged = true;
+            }
+            else if (!isInstalled)
+            {
+                Terminals.Remove(Terminals.FirstOrDefault(x => x.Path.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase)));
+                ResetToDefaultTerminal();
+                isChanged = true;
+            }
+            return isChanged;
         }
     }
 }
