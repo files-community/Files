40ea7bd3b9805422adaaf812102d561b4055ac07
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2020-06-03 21:25:15 -0400

    Improved open a specific page option

--- Files/Views/SettingsPages/OnStartup.xaml.cs
+++ Files/Views/SettingsPages/OnStartup.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Controls;
 
 namespace Files.SettingsPages
 {
@@ -7,6 +10,21 @@ namespace Files.SettingsPages
         public OnStartup()
         {
             InitializeComponent();
+        }
+
+        private async void btnOpenASpecificPageOnStartupPath_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+            var folderPicker = new FolderPicker();
+            folderPicker.FileTypeFilter.Add("*");
+
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+
+            if (folder != null)
+            {
+                // Application now has read/write access to the picked file
+                App.AppSettings.OpenASpecificPageOnStartupPath = folder.Path;
+            }
         }
     }
 }
