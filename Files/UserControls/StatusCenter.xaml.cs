4bebb8bf052c4125c045a8f906c9abf8a05753f6
Luke Blevins <lukeblevins15@gmail.com>
2020-06-20 23:39:28 -0400

    StatusCenter: Add more StatusBanner logic

--- Files/UserControls/StatusCenter.xaml.cs
+++ Files/UserControls/StatusCenter.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -26,16 +27,29 @@ namespace Files.UserControls
         }
     }
 
-    public class StatusBanner
+    public class StatusBanner : ViewModelBase
     {
         public string Title { get; set; }
-        public StatusBannerSeverity Severity { get; set; }
+        public StatusBannerSeverity Severity { get; set; } = StatusBannerSeverity.Ongoing;
         public StatusBannerOperation Operation { get; set; }
         public string Message { get; set; }
-        public uint Progress { get; set; } = 0;
+        private uint _Progress = 0;
+        public uint Progress
+        {
+            get => _Progress;
+            set => Set(ref _Progress, value);
+        }
 
-        private Color StrokeColor { get; set; }
+        private Color StrokeColor { get; set; } = Colors.DeepSkyBlue;
         private IconSource Glyph { get; set; }
+        private bool IsProgressing = false;
+
+        private string _FullTitle;
+        public string FullTitle
+        {
+            get => _FullTitle;
+            set => Set(ref _FullTitle, value);
+        }
 
         public enum StatusBannerSeverity
         {
@@ -46,6 +60,7 @@ namespace Files.UserControls
 
         public enum StatusBannerOperation
         {
+            Recycle,
             Delete,
             Paste,
             Extract
@@ -53,7 +68,77 @@ namespace Files.UserControls
 
         public StatusBanner()
         {
-
+            switch (Severity)
+            {
+                case StatusBannerSeverity.Ongoing:
+                    IsProgressing = true;
+                    if (string.IsNullOrWhiteSpace(Title))
+                    {
+                        switch (Operation)
+                        {
+                            case StatusBannerOperation.Extract:
+                                Title = ResourceController.GetTranslation("ExtractInProgress/Title");
+                                Glyph = new FontIconSource()
+                                {
+                                    Glyph = "\uED25"    // OpenFolder to substitute for extract glyph
+                                };
+                                break;
+                            case StatusBannerOperation.Paste:
+                                Title = ResourceController.GetTranslation("PasteInProgress/Title");
+                                Glyph = new FontIconSource()
+                                {
+                                    Glyph = "\uE77F"    // Paste glyph
+                                };
+                                break;
+                            case StatusBannerOperation.Delete:
+                                Title = ResourceController.GetTranslation("DeleteInProgress/Title");
+                                Glyph = new FontIconSource()
+                                {
+                                    Glyph = "\uE74D"    // Delete glyph
+                                };
+                                break;
+                            case StatusBannerOperation.Recycle:
+                                Title = ResourceController.GetTranslation("RecycleInProgress/Title");
+                                Glyph = new FontIconSource()
+                                {
+                                    //Glyph = "\uED25"    // OpenFolder to substitute for extract glyph
+                                };
+                                break;
+                        }
+                    }
+                    
+                    break;
+                case StatusBannerSeverity.Success:
+                    if (string.IsNullOrWhiteSpace(Title) || string.IsNullOrWhiteSpace(Message))
+                    {
+                        throw new NotImplementedException();
+                    }
+                    else
+                    {
+                        FullTitle = Title;
+                        StrokeColor = Colors.Green;
+                        Glyph = new FontIconSource()
+                        {
+                            Glyph = "\uE73E"    // CheckMark glyph
+                        };
+                    }
+                    break;
+                case StatusBannerSeverity.Error:
+                    if (string.IsNullOrWhiteSpace(Title) || string.IsNullOrWhiteSpace(Message))
+                    {
+                        throw new NotImplementedException();
+                    }
+                    else
+                    {
+                        FullTitle = Title;
+                        StrokeColor = Colors.Red;
+                        Glyph = new FontIconSource()
+                        {
+                            Glyph = "\uE783"    // Error glyph
+                        };
+                    }
+                    break;
+            }
         }
     }
 }
