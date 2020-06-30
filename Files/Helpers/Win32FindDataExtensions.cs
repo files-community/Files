c9530ea56cabb91d0a1bbdc4c63f3c2829499e7c
Vladyslav <tsvetkov_vlad@ukr.net>
2020-06-30 21:36:34 +0300

    Use constant when calculating file size

--- Files/Helpers/Win32FindDataExtensions.cs
+++ Files/Helpers/Win32FindDataExtensions.cs
@@ -1,27 +1,28 @@
-﻿using System;
-using static Files.Helpers.NativeFindStorageItemHelper;
+﻿using static Files.Helpers.NativeFindStorageItemHelper;
 
 namespace Files.Helpers
 {
     public static class Win32FindDataExtensions
     {
+        private const long MAXDWORD = 4294967295;
+
         public static long GetSize(this WIN32_FIND_DATA findData)
         {
             long fDataFSize = findData.nFileSizeLow;
             long fileSize;
             if (fDataFSize < 0 && findData.nFileSizeHigh > 0)
             {
-                fileSize = fDataFSize + 4294967296 + (findData.nFileSizeHigh * 4294967296);
+                fileSize = fDataFSize + (MAXDWORD + 1) + (findData.nFileSizeHigh * (MAXDWORD + 1));
             }
             else
             {
                 if (findData.nFileSizeHigh > 0)
                 {
-                    fileSize = fDataFSize + (findData.nFileSizeHigh * 4294967296);
+                    fileSize = fDataFSize + (findData.nFileSizeHigh * (MAXDWORD + 1));
                 }
                 else if (fDataFSize < 0)
                 {
-                    fileSize = fDataFSize + 4294967296;
+                    fileSize = fDataFSize + (MAXDWORD + 1);
                 }
                 else
                 {
