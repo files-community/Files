169925b448f19196285bf5042d6dbf131563a160
Jaiganesh Kumaran <jaiganesh.kumaran@outlook.com>
2020-07-17 08:02:26 +0530

    Add LayoutMode Enum

---
+++ Files/Enums/LayoutMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files.Enums
+{
+    public enum LayoutMode : byte
+    {
+        ListView,
+        TilesView,
+        GridView,
+        DetailsView // Reserved for future use
+    }
+}
