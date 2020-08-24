d38e7728c388b67367e8ff4522aba1a71e90e880
Marco Gavelli <marcogavelli2@gmail.com>
2020-08-24 21:34:38 +0200

    Use UserControl for flyout item

--- Files/UserControls/MenuFlyoutItemWithImage.xaml.cs
+++ Files/UserControls/MenuFlyoutItemWithImage.xaml.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// Il modello di elemento Controllo utente è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace Files.UserControls
 {
-    public class MenuFlyoutItemWithImage : MenuFlyoutItem
+    public sealed partial class MenuFlyoutItemWithImage : MenuFlyoutItem
     {
         public BitmapImage BitmapIcon
         {
@@ -21,7 +30,7 @@ namespace Files.UserControls
 
         public MenuFlyoutItemWithImage()
         {
-            
+            this.InitializeComponent();
         }
     }
 }
