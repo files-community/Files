4096e540c5f2e860a779a5dbf0aceb3215aa2d38
Marco Gavelli <marcogavelli2@gmail.com>
2020-08-24 21:48:29 +0200

    Added explanantion for commented lines

--- Files.Launcher/Program.cs
+++ Files.Launcher/Program.cs
@@ -255,6 +255,10 @@ namespace FilesFullTrust
                     {
                         await execThreadWithMessageQueue.PostMessage(args.Request.Message);
                     }
+                    // The following line is needed to cleanup resources when menu is closed. 
+                    // Unfortunately if you uncomment it some menu items will randomly stop working.
+                    // Resource cleanup is currently done on app closing, 
+                    // if we find a solution for the issue above, we should cleanup as soon as a menu is closed.
                     //handleTable.RemoveValue(menuKey);
                     break;
 
@@ -315,6 +319,10 @@ namespace FilesFullTrust
                 case "ExecAndCloseContextMenu":
                     var cMenuExec = table.GetValue<Win32API.ContextMenu>("MENU");
                     cMenuExec?.InvokeItem(message.Get("ItemID", -1));
+                    // The following line is needed to cleanup resources when menu is closed. 
+                    // Unfortunately if you uncomment it some menu items will randomly stop working.
+                    // Resource cleanup is currently done on app closing, 
+                    // if we find a solution for the issue above, we should cleanup as soon as a menu is closed.
                     //table.RemoveValue("MENU");
                     return null;
 
