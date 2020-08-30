b59f4382d9d7f33a6436102e9d258bc8c9d7b242
Thomas Heinrichs <thheinrichs@outlook.de>
2020-08-30 12:53:23 +0200

    Implement ItemIcon UserControl

---
+++ Files/UserControls/ItemIcon.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Files.View_Models;
+using Windows.UI.Xaml.Controls;
+
+
+namespace Files.UserControls
+{
+    public sealed partial class ItemIcon : UserControl
+    {
+        private SelectedItemsPropertiesViewModel viewModel;
+        private double size;
+
+        public SelectedItemsPropertiesViewModel ViewModel { get => viewModel; set => viewModel = value; }
+
+        public double Size
+        {
+            get => size;
+            set
+            {
+                size = value;
+                LargerItemSize = size + 2.0;
+            }
+        }
+        private double LargerItemSize { get; set; }
+
+        public ItemIcon()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
