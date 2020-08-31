21a25c67b26bbc1387369ec0beba43e6e43df8e7
pixl-8 <68357832+pixl-8@users.noreply.github.com>
2020-08-31 11:01:06 +1000

    Update copyright in AssemblyInfo.cs

--- Files.Launcher/Properties/AssemblyInfo.cs
+++ Files.Launcher/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FilesFullTrust")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
