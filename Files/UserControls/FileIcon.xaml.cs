2b3fbcc1b779359c6f2ce31712fe22509181ee60
Thomas Heinrichs <thheinrichs@outlook.de>
2020-09-01 16:51:29 +0200

    Implement user control FileIcon

---
+++ Files/UserControls/FileIcon.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Files.View_Models;
+using Windows.UI.Xaml.Controls;
+
+namespace Files.UserControls
+{
+    public sealed partial class FileIcon : UserControl
+    {
+        private double itemSize;
+
+        public SelectedItemsPropertiesViewModel ViewModel { get; set; }
+        public double ItemSize { get; set; }
+
+        public double Size
+        {
+            get => itemSize;
+            set
+            {
+                itemSize = value;
+                LargerItemSize = itemSize + 2.0;
+            }
+        }
+        private double LargerItemSize { get; set; }
+        
+        public FileIcon()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
