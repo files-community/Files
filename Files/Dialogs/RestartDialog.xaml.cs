7d5b764f7139fc8be9653abe3405f48010f4cc68
Thomas Heinrichs <thheinrichs@outlook.de>
2020-09-08 22:50:39 +0200

    Implement InAppNotification

---
+++ Files/Dialogs/RestartDialog.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Files.Dialogs
+{
+    public sealed partial class RestartDialog : UserControl
+    {
+        public RestartDialog()
+        {
+            this.InitializeComponent();
+            RestartNotification.Show();
+        }
+    }
+}
