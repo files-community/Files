fa84cf78943f0734d2d3e2191b9d1c05a7b6da6a
Thomas Heinrichs <thheinrichs@outlook.de>
2020-09-10 00:23:30 +0200

    Revert Merge branch 'master' of git://github.com/files-community/files-uwp into localization

---
+++ Files/UserControls/FileIcon.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Files.View_Models;
+using Windows.UI.Xaml.Controls;
+
+namespace Files.UserControls
+{
+    public sealed partial class FileIcon : UserControl
+    {
+        public SelectedItemsPropertiesViewModel ViewModel { get; set; }
+
+        private double itemSize;
+        public double ItemSize
+        {
+            get => itemSize;
+            set
+            {
+                itemSize = value;
+                LargerItemSize = itemSize + 2.0;
+            }
+        }
+        private double LargerItemSize { get; set; }
+
+        public FileIcon()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
