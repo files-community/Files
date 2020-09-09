4a9d51b50eef23ad168054bd050da2b041a68b18
Thomas Heinrichs <thheinrichs@outlook.de>
2020-09-09 16:38:59 +0200

    Add event handlers

--- Files/Dialogs/RestartDialog.xaml.cs
+++ Files/Dialogs/RestartDialog.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Files.Dialogs
 {
@@ -7,7 +10,31 @@ namespace Files.Dialogs
         public RestartDialog()
         {
             this.InitializeComponent();
+        }
+
+        public void Show()
+        {
             RestartNotification.Show();
+        }
+
+        public void Dismiss()
+        {
+            RestartNotification.Dismiss();
+        }
+
+        private async void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Restart app");
+            AppRestartFailureReason failureReason = await CoreApplication.RequestRestartAsync("");
+            if (failureReason == AppRestartFailureReason.NotInForeground)
+            {
+                System.Diagnostics.Debug.WriteLine("App not in foreground");
+            }
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            RestartNotification.Dismiss();
         }
     }
 }
