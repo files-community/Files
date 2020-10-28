ad3b63c4bb0390dffad5c2fe939714b79fc2e5f4
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-10-28 17:31:48 +0100

    upd

--- Files.Extensions/pch.h
+++
@@ -1,2 +0,0 @@
-﻿#pragma once
-#include <unknwn.h>
--- Files/CommandLine/ParsedCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Files.CommandLine
-{
-    internal class ParsedCommand
-    {
-        public ParsedCommandType Type { get; set; }
-
-        public string Payload { get; set; }
-    }
-}
