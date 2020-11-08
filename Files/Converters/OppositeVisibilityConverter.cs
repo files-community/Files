58030fa9ec257403a44789c6fc9fc58df4754b3b
Luke Blevins <lukeblevins15@outlook.com>
2020-11-08 01:03:21 -0500

    Fix context flyout not showing in layout modes

---
+++ Files/Converters/OppositeVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Files.Converters
+{
+    class OppositeVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+            {
+                return ((bool)value)? Visibility.Collapsed : Visibility.Visible;
+                
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
