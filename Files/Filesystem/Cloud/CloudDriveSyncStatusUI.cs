5302e0be5913d79ecd2f43271c34e619917732ff
Vladyslav <tsvetkov_vlad@ukr.net>
2020-11-13 17:39:57 +0200

    Update

--- Files/Filesystem/Cloud/CloudDriveSyncStatusUI.cs
+++ Files/Filesystem/Cloud/CloudDriveSyncStatusUI.cs
@@ -1,24 +1,9 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+﻿using Files.Enums;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Windows.UI.Xaml.Media;
 
 namespace Files.Filesystem.Cloud
 {
-    public enum CloudDriveSyncStatus
-    {
-        Unknown = -1,
-        FolderOnline = 0,
-        FolderOfflinePartial = 1,
-        FolderOfflineFull = 2,
-        FolderOfflinePinned = 3,
-        FolderExcluded = 4,
-        FolderEmpty = 5,
-        NotSynced = 6,
-        FileOnline = 8,
-        FileSync = 9,
-        FileOffline = 14,
-        FileOfflinePinned = 15,
-    }
-
     public class CloudDriveSyncStatusUI : ObservableObject
     {
         private bool loadSyncStatus;
