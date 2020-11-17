749f27286d1efa53bc5932917486c2c3f08c57c1
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2020-11-17 09:01:49 -0500

    Update GenericFileBrowser.xaml.cs

--- Files/Views/LayoutModes/GenericFileBrowser.xaml.cs
+++ Files/Views/LayoutModes/GenericFileBrowser.xaml.cs
@@ -298,7 +298,7 @@ namespace Files
                     {
                         tapDebounceTimer.Stop();
                         AllView.BeginEdit(); // EditingEventArgs will be null
-                    }, TimeSpan.FromMilliseconds(1000), false);
+                    }, TimeSpan.FromMilliseconds(700), false);
                 }
                 else
                 {
