5c2e573ded0206e37a9be1b40a049de11d8b4c4c
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2020-11-23 12:53:46 -0500

    Small tweak

--- Files/View Models/ItemViewModel.cs
+++ Files/View Models/ItemViewModel.cs
@@ -824,7 +824,7 @@ namespace Files.Filesystem
                     {
                         var item = folderContentsList[count];
                         AddFileOrFolderFromShellFile(item, returnformat);
-                        if (count % 64 == 0)
+                        if (count % 300 == 0)
                         {
                             OrderFiles();
                         }
@@ -1030,7 +1030,7 @@ namespace Files.Filesystem
                             break;
                         }
 
-                        if (count % 64 == 0)
+                        if (count % 300 == 0)
                         {
                             OrderFiles();
                         }
@@ -1084,7 +1084,7 @@ namespace Files.Filesystem
                 {
                     break;
                 }
-                if (count % 64 == 0)
+                if (count % 300 == 0)
                 {
                     OrderFiles();
                 }
