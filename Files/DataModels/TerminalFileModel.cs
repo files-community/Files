993815e3c7ee48e0a4d2c2f433152080cc5f7862
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2020-12-30 10:41:33 -0500

    Fixed an issue where Files would soemtime remove the Windows Terminal profile

--- Files/DataModels/TerminalFileModel.cs
+++ Files/DataModels/TerminalFileModel.cs
@@ -1,9 +1,7 @@
-﻿using Files.Helpers;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Files.DataModels
 {
@@ -38,33 +36,28 @@ namespace Files.DataModels
             DefaultTerminalPath = "cmd.exe";
         }
 
-        public async Task<bool> AddOrRemoveTerminalAsync(Terminal terminal, string packageName)
+        public void AddTerminal(Terminal terminal)
         {
-            bool isChanged = false;
-            bool isInstalled = await PackageHelper.IsAppInstalledAsync(packageName);
             //Ensure terminal is not already in List
-            if (Terminals.FirstOrDefault(x => x.Path.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase)) == null && isInstalled)
+            if (Terminals.FirstOrDefault(x => x.Path.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase)) == null)
             {
                 Terminals.Add(terminal);
-                isChanged = true;
             }
-            else if (!isInstalled)
-            {
-                if (Terminals.Remove(Terminals.FirstOrDefault(x => x.Path.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase))))
-                {
-                    if (string.IsNullOrWhiteSpace(DefaultTerminalPath))
-                    {
-                        ResetToDefaultTerminal();
-                    }
-                    else if (DefaultTerminalPath.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase))
-                    {
-                        ResetToDefaultTerminal();
-                    }
+        }
 
-                    isChanged = true;
+        public void RemoveTerminal(Terminal terminal)
+        {
+            if (Terminals.Remove(Terminals.FirstOrDefault(x => x.Path.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase))))
+            {
+                if (string.IsNullOrWhiteSpace(DefaultTerminalPath))
+                {
+                    ResetToDefaultTerminal();
+                }
+                else if (DefaultTerminalPath.Equals(terminal.Path, StringComparison.OrdinalIgnoreCase))
+                {
+                    ResetToDefaultTerminal();
                 }
             }
-            return isChanged;
         }
     }
 }
