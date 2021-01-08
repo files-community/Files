6a382b379a7089b9d259438752b8c9fc2b1bbf81
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2021-01-08 12:40:28 +0100

    Fixed item text wrapping

--- Files/UserControls/Widgets/Bundles.xaml.cs
+++ Files/UserControls/Widgets/Bundles.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
