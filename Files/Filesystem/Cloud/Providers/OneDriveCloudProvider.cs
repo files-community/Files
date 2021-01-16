775dd761e1c65d88a0b8382a5d5e6e0eecfe4e19
Andy Doyle <me@andydoyle.org>
2021-01-16 22:23:33 +0000

    Improve detection of OneDrive accounts with no display name

--- Files/Filesystem/Cloud/Providers/OneDriveCloudProvider.cs
+++ Files/Filesystem/Cloud/Providers/OneDriveCloudProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Files.Enums;
-using System;
+using Microsoft.Win32;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Files.Filesystem.Cloud.Providers
@@ -11,19 +12,32 @@ namespace Files.Filesystem.Cloud.Providers
         {
             try
             {
-                await Task.Run(() =>
+                using var oneDriveAccountsKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\OneDrive\Accounts", false);
+                var results = new List<CloudProvider>();
+
+                foreach (var account in oneDriveAccountsKey.GetSubKeyNames())
                 {
-                    var onedrivePersonal = Environment.GetEnvironmentVariable("OneDriveConsumer");
-                    if (!string.IsNullOrEmpty(onedrivePersonal))
+                    var accountKeyName = @$"{oneDriveAccountsKey.Name}\{account}";
+                    var displayName = (string)Registry.GetValue(accountKeyName, "DisplayName", null);
+                    var userFolder = (string)Registry.GetValue(accountKeyName, "UserFolder", null);
+
+                    if (!string.IsNullOrWhiteSpace(userFolder))
                     {
-                        cloudProviders.Add(new CloudProvider()
+                        results.Add(new CloudProvider()
                         {
                             ID = CloudProviders.OneDrive,
-                            Name = "OneDrive",
-                            SyncFolder = onedrivePersonal
+                            Name = string.IsNullOrWhiteSpace(displayName) ? "OneDrive - Personal" : $"OneDrive - {displayName}",
+                            SyncFolder = userFolder
                         });
                     }
-                });
+                }
+
+                if (results.Count > 0)
+                {
+                    cloudProviders.AddRange(results
+                        .OrderByDescending(o => string.Equals(o.Name, "OneDrive - Personal", System.StringComparison.OrdinalIgnoreCase))
+                        .ThenBy(o => o.Name));
+                }
             }
             catch
             {
