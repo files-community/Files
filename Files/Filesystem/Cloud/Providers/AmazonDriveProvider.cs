c85e48637959653bf2bb86da842afb0a0a08f5ec
Steven He <hez2010@outlook.com>
2021-01-18 20:29:23 +0800

    Avoid blocking wait on an async operation

--- Files/Filesystem/Cloud/Providers/AmazonDriveProvider.cs
+++ Files/Filesystem/Cloud/Providers/AmazonDriveProvider.cs
@@ -12,12 +12,14 @@ namespace Files.Filesystem.Cloud.Providers
     {
         public async Task DetectAsync(List<CloudProvider> cloudProviders)
         {
-            try
+            await Task.Run(() =>
             {
-                await Task.Run(() =>
+                try
                 {
                     using var key = Registry.ClassesRoot.OpenSubKey(@"CLSID\{9B57F475-CCB0-4C85-88A9-2AA9A6C0809A}\Instance\InitPropertyBag");
-                    var syncedFolder = (string)key.GetValue("TargetFolderPath");
+                    var syncedFolder = (string)key?.GetValue("TargetFolderPath");
+
+                    if (syncedFolder == null) return;
 
                     cloudProviders.Add(new CloudProvider()
                     {
@@ -25,12 +27,12 @@ namespace Files.Filesystem.Cloud.Providers
                         Name = "Amazon Drive",
                         SyncFolder = syncedFolder
                     });
-                });
-            }
-            catch
-            {
-                // Not detected
-            }
+                }
+                catch
+                {
+                    // Not detected
+                }
+            });
         }
     }
 }
