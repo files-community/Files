5b0afd59fb23626e8d5c78bbe25b89b85b626220
Winston de Jong <59544401+winston-de@users.noreply.github.com>
2021-01-27 10:00:05 -0800

    Remove string

--- Files/Filesystem/ListedItem.cs
+++ Files/Filesystem/ListedItem.cs
@@ -138,7 +138,6 @@ namespace Files.Filesystem
 
         public string FileExtension { get; set; }
         public string FileSize { get; set; }
-        public string FileSizeDisplay => FileSize ?? "ItemSizeNotCalcluated".GetLocalized();
         public long FileSizeBytes { get; set; }
 
         public string ItemDateModified { get; private set; }
