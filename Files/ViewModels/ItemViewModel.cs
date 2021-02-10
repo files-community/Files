ff292a295b35cfdee6e48537529dc2794c359b61
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2021-02-10 21:27:03 +0100

    Small Tweaks 2

--- Files/ViewModels/ItemViewModel.cs
+++ Files/ViewModels/ItemViewModel.cs
@@ -1023,7 +1023,7 @@ namespace Files.ViewModels
                     if (foldersCount > 0)
                     { // There are folders in current directory
 
-                        if ((FilesAndFolders.Count - imagesAndVideosCount) < (FilesAndFolders.Count < 20 ? 0 : FilesAndFolders.Count - 20) && imagesAndVideosCount > 10)
+                        if ((FilesAndFolders.Count - imagesAndVideosCount) > (FilesAndFolders.Count < 20 ? 0 : FilesAndFolders.Count - 20))
                         { // Most of items are images/videos
                             AssociatedInstance.InstanceViewModel.FolderSettings.ToggleLayoutModeTilesAction(
                                AssociatedInstance.InstanceViewModel.FolderSettings);
