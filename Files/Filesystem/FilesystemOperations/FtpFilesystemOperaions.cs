004a61d9c166626292c1834d0344d1ea5ce29052
Steven He <hez2010@outlook.com>
2021-02-19 15:57:06 +0800

    FtpStorageItem

---
+++ Files/Filesystem/FilesystemOperations/FtpFilesystemOperaions.cs
@@ -0,0 +1,55 @@
+﻿using Files.Enums;
+using Files.Filesystem.FilesystemHistory;
+using Files.Filesystem.StorageFileHelpers;
+using FluentFTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Files.Filesystem.FilesystemOperations
+{
+    public class FtpFilesystemOperaions : IFilesystemOperations
+    {
+        public IFtpClient FtpClient { get; set; }
+
+        private async Task<bool> EnsureConnected()
+        {
+            if (FtpClient is null)
+            {
+                return false;
+            }
+
+            await FtpClient.AutoConnectAsync();
+            return true;
+        }
+
+        public async Task<IStorageHistory> CopyAsync(IStorageItem source, string destination, IProgress<float> progress, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken)
+        {
+            if (!(source is FtpStorageItem item) || !await EnsureConnected())
+            {
+                return null;
+            }
+
+            throw new NotImplementedException();
+        }
+        public async Task<IStorageHistory> CopyAsync(IStorageItemWithPath source, string destination, IProgress<float> progress, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public async Task<IStorageHistory> CreateAsync(IStorageItemWithPath source, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public async Task<IStorageHistory> DeleteAsync(IStorageItem source, IProgress<float> progress, IProgress<FileSystemStatusCode> errorCode, bool permanently, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public async Task<IStorageHistory> DeleteAsync(IStorageItemWithPath source, IProgress<float> progress, IProgress<FileSystemStatusCode> errorCode, bool permanently, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+
+        }
+
+        public async Task<IStorageHistory> MoveAsync(IStorageItem source, string destination, IProgress<float> progress, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public async Task<IStorageHistory> MoveAsync(IStorageItemWithPath source, string destination, IProgress<float> progress, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public async Task<IStorageHistory> RenameAsync(IStorageItem source, string newName, NameCollisionOption collision, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public async Task<IStorageHistory> RenameAsync(IStorageItemWithPath source, string newName, NameCollisionOption collision, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken) => throw new NotImplementedException();
+        public async Task<IStorageHistory> RestoreFromTrashAsync(IStorageItemWithPath source, string destination, IProgress<float> progress, IProgress<FileSystemStatusCode> errorCode, CancellationToken cancellationToken) => throw new NotImplementedException();
+    }
+}
---
+++ Files/Filesystem/StorageFileHelpers/FtpStorageItem.cs
@@ -0,0 +1,91 @@
+﻿using FluentFTP;
+using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.System.Threading;
+
+namespace Files.Filesystem.StorageFileHelpers
+{
+    public class FtpStorageItem : IStorageItem
+    {
+        public FtpStorageItem(IFtpClient ftpClient, string name, string path, FileAttributes attributes, DateTimeOffset dateCreated)
+        {
+            FtpClient = ftpClient;
+            Name = name;
+            Path = path;
+            Attributes = attributes;
+            DateCreated = dateCreated;
+        }
+
+        public IFtpClient FtpClient { get; set; }
+
+        private async Task<bool> EnsureConnected()
+        {
+            if (FtpClient is null)
+            {
+                return false;
+            }
+
+            await FtpClient.AutoConnectAsync();
+            return true;
+        }
+
+        public IAsyncAction RenameAsync(string desiredName)
+        {
+            return ThreadPool.RunAsync(async (x) =>
+            {
+                if (!await EnsureConnected())
+                {
+                    return;
+                }
+
+                await FtpClient.RenameAsync(Path, string.Join("/", Path.GetFtpDirectoryName(), desiredName));
+            });
+        }
+
+        public IAsyncAction RenameAsync(string desiredName, NameCollisionOption option) => throw new NotImplementedException();
+        public IAsyncAction DeleteAsync()
+        {
+            return ThreadPool.RunAsync(async (x) =>
+            {
+                if (!await EnsureConnected())
+                {
+                    return;
+                }
+                if ((Attributes & FileAttributes.Directory) != 0)
+                {
+                    await FtpClient.DeleteDirectoryAsync(Path);
+                }
+                else
+                {
+                    await FtpClient.DeleteFileAsync(Path);
+                }
+            });
+        }
+
+        public IAsyncAction DeleteAsync(StorageDeleteOption option) => throw new NotImplementedException();
+        public IAsyncOperation<BasicProperties> GetBasicPropertiesAsync() => throw new NotImplementedException();
+        public bool IsOfType(StorageItemTypes type)
+        {
+            switch (type)
+            {
+                case StorageItemTypes.File:
+                    return (Attributes & FileAttributes.Directory) == 0;
+                case StorageItemTypes.Folder:
+                    return (Attributes & FileAttributes.Directory) != 0;
+                default:
+                    return false;
+            }
+        }
+
+        public FileAttributes Attributes { get; private set; }
+
+        public DateTimeOffset DateCreated { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Path { get; private set; }
+    }
+}
