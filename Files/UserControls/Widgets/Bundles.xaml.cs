71434300013d64ea434ed4f183b63681a6d5976f
d2dyno1 <53011783+d2dyno1@users.noreply.github.com>
2021-03-09 16:33:21 +0100

    Design Tweaks to Bundles (#3894)

--- Files/UserControls/Widgets/Bundles.xaml.cs
+++ Files/UserControls/Widgets/Bundles.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Files.ViewModels.Bundles;
 using System;
-using System.Diagnostics;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
--- Files/ViewModels/Bundles/BundleContainerViewModel.cs
+++ Files/ViewModels/Bundles/BundleContainerViewModel.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows.Input;
 using Windows.ApplicationModel.DataTransfer;
