6af12ff1d04cae5839893b5650fd106f3dd350bb
Winston de Jong <59544401+winston-de@users.noreply.github.com>
2021-03-13 16:41:30 -0800

    Revert "Use single instance of appserviceconnection for preview pane thumbnails (#4014)" (#4023)

--- Files/Helpers/FileThumbnailHelper.cs
+++ Files/Helpers/FileThumbnailHelper.cs
@@ -8,11 +8,10 @@ namespace Files.Helpers
 {
     public static class FileThumbnailHelper
     {
-        private static AppServiceConnection AppServiceConnection { get; set; }
         public static async Task<(byte[] IconData, byte[] OverlayData, bool IsCustom)> LoadIconOverlayAsync(string filePath, uint thumbnailSize)
         {
-            AppServiceConnection ??= await AppServiceConnectionHelper.Instance;
-            if (AppServiceConnection != null)
+            var connection = await AppServiceConnectionHelper.Instance;
+            if (connection != null)
             {
                 var value = new ValueSet
                 {
@@ -20,7 +19,7 @@ namespace Files.Helpers
                     { "filePath", filePath },
                     { "thumbnailSize", (int)thumbnailSize }
                 };
-                var response = await AppServiceConnection.SendMessageAsync(value);
+                var response = await connection.SendMessageAsync(value);
                 var hasCustomIcon = (response.Status == AppServiceResponseStatus.Success)
                     && response.Message.Get("HasCustomIcon", false);
                 var icon = response.Message.Get("Icon", (string)null);
