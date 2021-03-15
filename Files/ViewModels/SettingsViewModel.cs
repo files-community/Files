c21459fcad24c235a8a4aa968dbde836cc979df6
nvi9 <42123156+nvi9@users.noreply.github.com>
2021-03-16 00:59:11 +0100

    Added support for collapsing the sidebar (#3895)

--- Files/ViewModels/SettingsViewModel.cs
+++ Files/ViewModels/SettingsViewModel.cs
@@ -95,10 +95,22 @@ namespace Files.ViewModels
             await Launcher.LaunchUriAsync(new Uri(@"https://github.com/files-community/Files/issues/new/choose"));
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating the width of the the sidebar pane when open.
+        /// </summary>
         public GridLength SidebarWidth
         {
             get => new GridLength(Math.Min(Math.Max(Get(250d), 250d), 500d), GridUnitType.Pixel);
             set => Set(value.Value);
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating if the sidebar pane should be open or closed.
+        /// </summary>
+        public bool IsSidebarOpen
+        {
+            get => Get(true);
+            set => Set(value);
         }
 
         /// <summary>
