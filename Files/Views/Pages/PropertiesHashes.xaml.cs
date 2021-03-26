d1414296bed9a6a3c3df801db5a5c83f22796a60
oleitao <olivleitao@gmail.com>
2021-03-26 23:13:18 +0000

    Compare option little lower down on the page

--- Files/Views/Pages/PropertiesHashes.xaml.cs
+++ Files/Views/Pages/PropertiesHashes.xaml.cs
@@ -60,5 +60,49 @@ namespace Files.Views
                 }
             }
         }
+
+        private async void btnCompareHash_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+
+            openPicker.FileTypeFilter.Clear();
+            openPicker.FileTypeFilter.Add("*");
+
+            StorageFile file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                file = await Filesystem.FilesystemTasks.Wrap(() => StorageFile.GetFileFromPathAsync(file.Path).AsTask());
+
+                if ((BaseProperties as FileProperties) != null)
+                {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+
+                    ComboBoxItem ComboItem = (ComboBoxItem)ddlCompareHash.SelectedItem;
+                    switch (ComboItem.Name)
+                    {
+                        case "MD5":
+                            txtCompareHash.Text = await (BaseProperties as FileProperties).GetSystemFileHashes(HashAlgorithmNames.Md5, file);
+                            break;
+                        case "SHA1":
+                            txtCompareHash.Text = await (BaseProperties as FileProperties).GetSystemFileHashes(HashAlgorithmNames.Sha1, file);
+                            break;
+                        default:
+                            txtCompareHash.Text = await (BaseProperties as FileProperties).GetSystemFileHashes(string.Empty, file);
+                            break;
+                    }
+
+                    stopwatch.Stop();
+                    Debug.WriteLine(string.Format("System file properties were obtained in {0} milliseconds", stopwatch.ElapsedMilliseconds));
+                }
+            }
+        }
+
+        private void ddlCompareHash_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtCompareHash.Text = string.Empty;
+        }
     }
 }
