b0cbf64e68b8d177b15736e0d91389fc444dbda7
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2021-03-29 10:24:32 +0200

    Improve navigation animation

--- Files/Views/ModernShellPage.xaml.cs
+++ Files/Views/ModernShellPage.xaml.cs
@@ -1376,33 +1376,39 @@ namespace Files.Views
 
         public void NavigateToPath(string navigationPath, Type sourcePageType, NavigationArguments navArgs = null)
         {
+            if (sourcePageType == null)
+            {
+                sourcePageType = InstanceViewModel.FolderSettings.GetLayoutType(navigationPath);
+            }
+
             if (navArgs != null && navArgs.AssociatedTabInstance != null)
             {
                 ItemDisplayFrame.Navigate(
-                sourcePageType == null ? InstanceViewModel.FolderSettings.GetLayoutType(navigationPath) : sourcePageType,
+                sourcePageType,
                 navArgs,
                 new SuppressNavigationTransitionInfo());
             }
             else
             {
-                if (string.IsNullOrEmpty(navigationPath) ||
-                string.IsNullOrEmpty(FilesystemViewModel?.WorkingDirectory) ||
-                navigationPath.TrimEnd(Path.DirectorySeparatorChar).Equals(
-                    FilesystemViewModel.WorkingDirectory.TrimEnd(Path.DirectorySeparatorChar),
-                    StringComparison.OrdinalIgnoreCase)) // return if already selected
+                if (string.IsNullOrEmpty(navigationPath) 
+                    || string.IsNullOrEmpty(FilesystemViewModel?.WorkingDirectory)
+                    || navigationPath.TrimEnd(Path.DirectorySeparatorChar).Equals(
+                        FilesystemViewModel.WorkingDirectory.TrimEnd(Path.DirectorySeparatorChar),
+                        StringComparison.OrdinalIgnoreCase)) // return if already selected
                 {
                     return;
                 }
 
                 NavigationTransitionInfo transition = new SuppressNavigationTransitionInfo();
 
-                if (sourcePageType == typeof(YourHome))
+                if (sourcePageType == typeof(YourHome) 
+                    || ItemDisplayFrame.Content.GetType() == typeof(YourHome) && (sourcePageType == typeof(GenericFileBrowser) || sourcePageType == typeof(GridViewBrowser)))
                 {
                     transition = new DrillInNavigationTransitionInfo();
                 }
 
                 ItemDisplayFrame.Navigate(
-                sourcePageType == null ? InstanceViewModel.FolderSettings.GetLayoutType(navigationPath) : sourcePageType,
+                sourcePageType,
                 new NavigationArguments()
                 {
                     NavPathParam = navigationPath,
