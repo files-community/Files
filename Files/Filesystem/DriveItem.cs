87e29e932334b6349065bf184b602bc0b7943e35
Winston de Jong <59544401+winston-de@users.noreply.github.com>
2021-04-17 19:53:20 -0700

    Fixed crashes when opening properties dialog (#4658)

--- Files/Filesystem/DriveItem.cs
+++ Files/Filesystem/DriveItem.cs
@@ -16,6 +16,7 @@ namespace Files.Filesystem
     public class DriveItem : ObservableObject, INavigationControlItem
     {
         public SvgImageSource Icon { get; set; }
+        public Uri IconSource { get; set; }
 
         private string path;
 
@@ -194,47 +195,48 @@ namespace Files.Filesystem
                 switch (type)
                 {
                     case DriveType.Fixed:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Drive.svg"));
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Drive.svg");
                         break;
 
                     case DriveType.Removable:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Folder.svg")); // TODO
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Folder.svg");
                         break;
 
                     case DriveType.Network:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Drive_Network.svg"));
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Drive_Network.svg");
                         break;
 
                     case DriveType.Ram:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Folder.svg")); // TODO
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Folder.svg");
                         break;
 
                     case DriveType.CDRom:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Folder.svg")); // TODO
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Folder.svg");
                         break;
 
                     case DriveType.Unknown:
                         break;
 
                     case DriveType.NoRootDirectory:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Folder.svg")); // TODO
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Folder.svg"); // TODO
                         break;
 
                     case DriveType.VirtualDrive:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Folder.svg")); // TODO
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Folder.svg"); // TODO
                         break;
 
                     case DriveType.CloudDrive:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Folder.svg")); // TODO
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Folder.svg"); // TODO
                         break;
 
                     case DriveType.FloppyDisk:
-                        Icon = new SvgImageSource(new Uri("ms-appx:///Assets/FluentIcons/Folder.svg")); // TODO
+                        IconSource = new Uri("ms-appx:///Assets/FluentIcons/Folder.svg");
                         break;
 
                     default:
                         throw new ArgumentOutOfRangeException(nameof(type), type, null);
                 }
+                Icon = new SvgImageSource(IconSource);
             });
         }
 
