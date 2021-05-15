5415537889df7ef5e25d0352df3eb3dae0bd6414
Marco Gavelli <marcogavelli2@gmail.com>
2021-05-15 21:20:33 +0200

    Fix Box Drive enum

--- Files/ViewModels/ItemViewModel.cs
+++ Files/ViewModels/ItemViewModel.cs
@@ -1143,7 +1143,8 @@ namespace Files.ViewModels
         public async Task<bool> EnumerateItemsFromStandardFolderAsync(string path, StorageFolderWithPath storageFolderForGivenPath, Type sourcePageType, CancellationToken cancellationToken, List<string> skipItems, bool cacheOnly = false, LibraryItem library = null)
         {
             // Flag to use FindFirstFileExFromApp or StorageFolder enumeration
-            bool enumFromStorageFolder = false;
+            bool enumFromStorageFolder =
+                path == App.CloudDrivesManager.Drives.FirstOrDefault(x => x.Text == "Box")?.Path?.TrimEnd('\\'); // Use storage folder for Box Drive (#4629)
 
             StorageFolder rootFolder = null;
             var res = await FilesystemTasks.Wrap(() => StorageFolder.GetFolderFromPathAsync(path).AsTask());
