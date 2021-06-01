637a166fe1806c0012534e7caaa76be6c9e7ff89
Winston de Jong <winiston2212@outlook.com>
2021-05-31 17:33:50 -0700

    - implement try-catch properly - wait, then try to write again on IO exception - implement semaphore slim for writing to logs

--- Common/Logger.cs
+++ Common/Logger.cs
@@ -13,6 +13,7 @@ namespace Files.Common
     public class Logger
     {       
         ILogWriter LogWriter { get; }
+        SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1);
 
         public Logger(ILogWriter logWriter, [System.Runtime.CompilerServices.CallerMemberName] string caller = "")
         {
@@ -64,15 +65,31 @@ namespace Files.Common
             Log(type: "WARN", caller: caller, message: $"{ex.Message}\n\t{ex}");
         }
 
-        private void Log(string type, string caller, string message)
+        private async void Log(string type, string caller, string message, int attemptNumber = 0)
         {
             try
             {
-                LogWriter.WriteLineToLog($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffff}|{type}|{caller}|{message}");
+                await semaphoreSlim.WaitAsync();
+                await LogWriter.WriteLineToLog($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffff}|{type}|{caller}|{message}");
+            }
+            catch (IOException e) when (!(e is FileNotFoundException))
+            {
+                if(attemptNumber < 5) // check the attempt count to prevent a stack overflow exception
+                {
+                    // Log is likely in use by another process instance, so wait then try again
+                    await Task.Delay(50);
+                    Log(type, caller, message, attemptNumber + 1);
+                } else
+                {
+                    Debug.WriteLine($"Writing to log file failed after 5 attempts with the following exception:\n{e}");
+                }
             }
             catch (Exception e)
             {
                 Debug.WriteLine($"Writing to log file failed with the following exception:\n{e}");
+            } finally
+            {
+                semaphoreSlim.Release();
             }
         }
     }
