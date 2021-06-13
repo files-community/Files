3453949d3bf3ba1e9221b9293aad33d8f58ad0f1
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2021-06-13 16:56:21 +0200

    Fix codestyle

--- Files/ViewModels/Dialogs/DecompressArchiveDialogViewModel.cs
+++ Files/ViewModels/Dialogs/DecompressArchiveDialogViewModel.cs
@@ -11,30 +11,30 @@ namespace Files.ViewModels.Dialogs
 {
     public class DecompressArchiveDialogViewModel : ObservableObject
     {
-        private readonly IStorageFile _archive;
+        private readonly IStorageFile archive;
 
         public StorageFolder DestinationFolder { get; private set; }
 
-        private string _DestinationFolderPath;
+        private string destinationFolderPath;
         public string DestinationFolderPath
         {
-            get => _DestinationFolderPath;
-            private set => SetProperty(ref _DestinationFolderPath, value);
+            get => destinationFolderPath;
+            private set => SetProperty(ref destinationFolderPath, value);
         }
 
-        private bool _OpenDestinationFolderOnCompletion;
+        private bool openDestinationFolderOnCompletion;
         public bool OpenDestinationFolderOnCompletion
         {
-            get => _OpenDestinationFolderOnCompletion;
-            set => SetProperty(ref _OpenDestinationFolderOnCompletion, value);
+            get => openDestinationFolderOnCompletion;
+            set => SetProperty(ref openDestinationFolderOnCompletion, value);
         }
 
         public ICommand SelectDestinationCommand { get; private set; }
 
         public DecompressArchiveDialogViewModel(IStorageFile archive)
         {
-            this._archive = archive;
-            this.DestinationFolderPath = DefaultDestinationFolderPath();
+            this.archive = archive;
+            this.destinationFolderPath = DefaultDestinationFolderPath();
 
             // Create commands
             SelectDestinationCommand = new AsyncRelayCommand(SelectDestination);
@@ -59,7 +59,7 @@ namespace Files.ViewModels.Dialogs
 
         private string DefaultDestinationFolderPath()
         {
-            return Path.Combine(Path.GetDirectoryName(_archive.Path), Path.GetFileNameWithoutExtension(_archive.Path));
+            return Path.Combine(Path.GetDirectoryName(archive.Path), Path.GetFileNameWithoutExtension(archive.Path));
         }
     }
 }
