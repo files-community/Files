0741c5b12d3e902a355535da0b61b3078711ef28
Winston de Jong <59544401+winston-de@users.noreply.github.com>
2021-06-26 20:10:55 -0700

    Switch to muxc navigation view for properties dialog (#5329)

--- Files/Views/Pages/Properties.xaml.cs
+++ Files/Views/Pages/Properties.xaml.cs
@@ -192,7 +192,7 @@ namespace Files.Views
             }
         }
 
-        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        private void NavigationView_SelectionChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewSelectionChangedEventArgs args)
         {
             var navParam = new PropertyNavParam()
             {
