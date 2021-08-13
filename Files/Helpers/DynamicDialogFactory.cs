456f2e0e0300af7679d76f9ceb950fad8e6c9115
Winston de Jong <59544401+winston-de@users.noreply.github.com>
2021-08-13 14:36:10 -0700

    Dialog improvements (#5782)

--- Files/Helpers/DynamicDialogFactory.cs
+++ Files/Helpers/DynamicDialogFactory.cs
@@ -4,6 +4,8 @@ using Files.Filesystem;
 using Files.ViewModels.Dialogs;
 using Microsoft.Toolkit.Uwp;
 using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.System;
 using Windows.UI.Xaml.Controls;
 
@@ -76,6 +78,12 @@ namespace Files.Helpers
                 }
 
                 tipText.Opacity = 0.0d;
+            };
+
+            inputText.Loaded += (s, e) =>
+            {
+                // dispatching to the ui thread fixes an issue where the primary dialog button would steal focus
+                _ = CoreApplication.MainView.DispatcherQueue.EnqueueAsync(() => inputText.Focus(Windows.UI.Xaml.FocusState.Programmatic));
             };
 
             dialog = new DynamicDialog(new DynamicDialogViewModel()
