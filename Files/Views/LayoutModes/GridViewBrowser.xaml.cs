fec4c360f4e7e426fe85d06fee14bf677ef08ad1
Marco Gavelli <marcogavelli2@gmail.com>
2021-08-13 22:44:12 +0200

    Fix rename starting on Space when Quicklook is enabled

--- Files/Views/LayoutModes/GridViewBrowser.xaml.cs
+++ Files/Views/LayoutModes/GridViewBrowser.xaml.cs
@@ -385,11 +385,11 @@ namespace Files.Views.LayoutModes
             {
                 if (!IsRenamingItem && !ParentShellPageInstance.NavToolbarViewModel.IsEditModeEnabled)
                 {
+                    e.Handled = true;
                     if (MainViewModel.IsQuickLookEnabled)
                     {
                         await QuickLookHelpers.ToggleQuickLook(ParentShellPageInstance);
                     }
-                    e.Handled = true;
                 }
             }
             else if (e.KeyStatus.IsMenuKeyDown && (e.Key == VirtualKey.Left || e.Key == VirtualKey.Right || e.Key == VirtualKey.Up))
