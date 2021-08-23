1490694debc94261a699fc581b647bbd20234cfa
Marco Gavelli <marcogavelli2@gmail.com>
2021-08-23 22:11:07 +0200

    Open empty folders in ColumnView Open zip files in ColumnView

--- Files/Views/LayoutModes/ColumnViewBase.xaml.cs
+++ Files/Views/LayoutModes/ColumnViewBase.xaml.cs
@@ -422,12 +422,8 @@ namespace Files.Views.LayoutModes
                 if (item.PrimaryItemAttribute == Windows.Storage.StorageItemTypes.Folder)
                 {
                     DismissColumn?.Invoke(sender as ListView, EventArgs.Empty);
-                    if (item.ContainsFilesOrFolders)
-                    {
-                        listViewItem = (FileList.ContainerFromItem(item) as ListViewItem);
-
-                        ItemInvoked?.Invoke(new ColumnParam { Path = item.ItemPath, ListView = FileList }, EventArgs.Empty);
-                    }
+                    listViewItem = (FileList.ContainerFromItem(item) as ListViewItem);
+                    ItemInvoked?.Invoke(new ColumnParam { Path = item.ItemPath, ListView = FileList }, EventArgs.Empty);
                 }
                 else
                 {
@@ -486,11 +482,8 @@ namespace Files.Views.LayoutModes
                 if (item.PrimaryItemAttribute == Windows.Storage.StorageItemTypes.Folder)
                 {
                     DismissColumn?.Invoke(sender as ListView, EventArgs.Empty);
-                    if (item.ContainsFilesOrFolders)
-                    {
-                        listViewItem = (FileList.ContainerFromItem(item) as ListViewItem);
-                        ItemInvoked?.Invoke(new ColumnParam { Path = item.ItemPath, ListView = FileList }, EventArgs.Empty);
-                    }
+                    listViewItem = (FileList.ContainerFromItem(item) as ListViewItem);
+                    ItemInvoked?.Invoke(new ColumnParam { Path = item.ItemPath, ListView = FileList }, EventArgs.Empty);
                 }
                 else
                 {
