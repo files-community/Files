54950a324b3fcd0448e47136789533eb69c7a629
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-08-26 09:10:56 -0400

    Fixed color combo box

---
+++ Files/Helpers/ResourceHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Markup;
+
+namespace Files.Helpers
+{
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public sealed class ResourceString : MarkupExtension
+    {
+        private static ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView();
+
+        public string Name
+        {
+            get; set;
+        }
+
+        protected override object ProvideValue()
+        {
+            return resourceLoader.GetString(this.Name);
+        }
+    }
+}
