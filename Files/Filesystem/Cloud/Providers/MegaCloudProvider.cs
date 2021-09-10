e55b9fe16d16e25664e8dc4ab7e49b1ce76a71d7
Marco Gavelli <marcogavelli2@gmail.com>
2021-09-10 16:08:11 +0200

    Re-add support for MEGA cloud drive (#6076)

--- Files/Filesystem/Cloud/Providers/MegaCloudProvider.cs
+++ Files/Filesystem/Cloud/Providers/MegaCloudProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Files.Enums;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,8 +10,64 @@ namespace Files.Filesystem.Cloud.Providers
     {
         public async Task<IList<CloudProvider>> DetectAsync()
         {
-            // MEGA not supported anymore
-            return await Task.FromResult(Array.Empty<CloudProvider>());
+            try
+            {
+                return await Task.Run(() => DetectFromRegistry());
+            }
+            catch
+            {
+                // Not detected
+                return Array.Empty<CloudProvider>();
+            }
+        }
+
+        private IList<CloudProvider> DetectFromRegistry()
+        {
+            var results = new List<CloudProvider>();
+            using var clsidKey = Registry.ClassesRoot.OpenSubKey(@"CLSID");
+            foreach (var subKeyName in clsidKey.GetSubKeyNames())
+            {
+                using var subKey = clsidKey.OpenSubKey(subKeyName);
+                if ((int?)subKey.GetValue("System.IsPinnedToNameSpaceTree") == 1)
+                {
+                    using var namespaceKey = Registry.CurrentUser.OpenSubKey($@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{subKeyName}");
+                    var driveType = (string)namespaceKey?.GetValue("");
+                    if (driveType == null)
+                    {
+                        continue;
+                    }
+
+                    using var bagKey = subKey.OpenSubKey(@"Instance\InitPropertyBag");
+                    var syncedFolder = (string)bagKey?.GetValue("TargetFolderPath");
+                    if (syncedFolder == null)
+                    {
+                        continue;
+                    }
+
+                    // Also works for OneDrive, Box, Amazon Drive, iCloudDrive, Dropbox
+                    CloudProviders? driveID = driveType switch
+                    {
+                        "MEGA" => CloudProviders.Mega,
+                        _ => null
+                    };
+                    if (driveID == null)
+                    {
+                        continue;
+                    }
+
+                    results.Add(new CloudProvider()
+                    {
+                        ID = driveID.Value,
+                        Name = driveID switch
+                        {
+                            CloudProviders.Mega => $"MEGA ({System.IO.Path.GetFileName(syncedFolder.TrimEnd('\\'))})",
+                            _ => null
+                        },
+                        SyncFolder = syncedFolder
+                    });
+                }
+            }
+            return results;
         }
     }
 }
