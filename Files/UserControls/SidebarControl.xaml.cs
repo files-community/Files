2b33c6a9e2027abd3f17cab99dec167329049df9
Marco Gavelli <marcogavelli2@gmail.com>
2021-10-02 20:48:59 +0200

    Use GetDraggedStorageItems instead of GetStorageItemsAsync (2547497699u)

--- Files/UserControls/SidebarControl.xaml.cs
+++ Files/UserControls/SidebarControl.xaml.cs
@@ -709,7 +709,7 @@ namespace Files.UserControls
             }
 
             // If the dropped item is a folder or file from a file system
-            if (Filesystem.FilesystemHelpers.HasDraggedStorageItems(e.DataView))
+            if (FilesystemHelpers.HasDraggedStorageItems(e.DataView))
             {
                 VisualStateManager.GoToState(sender as Microsoft.UI.Xaml.Controls.NavigationViewItem, "Drop", false);
 
@@ -717,10 +717,10 @@ namespace Files.UserControls
 
                 if (string.IsNullOrEmpty(locationItem.Path) && SectionType.Favorites.Equals(locationItem.Section) && isDropOnProcess) // Pin to Favorites section
                 {
-                    var storageItems = await e.DataView.GetStorageItemsAsync();
+                    var (_, storageItems) = await FilesystemHelpers.GetDraggedStorageItems(e.DataView);
                     foreach (var item in storageItems)
                     {
-                        if (item.IsOfType(StorageItemTypes.Folder) && !SidebarPinnedModel.FavoriteItems.Contains(item.Path))
+                        if (item.ItemType == FilesystemItemType.Directory && !SidebarPinnedModel.FavoriteItems.Contains(item.Path))
                         {
                             SidebarPinnedModel.AddItem(item.Path);
                         }
