595cb5c8bb026cf84b6c34605b0f819b5d9783ff
Marco Gavelli <marcogavelli2@gmail.com>
2021-10-04 00:12:38 +0200

    Fix tab loading indicator with continue left off

--- Files/Views/ModernShellPage.xaml.cs
+++ Files/Views/ModernShellPage.xaml.cs
@@ -1017,11 +1017,10 @@ namespace Files.Views
         private void SetLoadingIndicatorForTabs(bool isLoading)
         {
             var multitaskingControls = ((Window.Current.Content as Frame).Content as MainPage).ViewModel.MultitaskingControls;
-            var tabItemControl = this.FindAscendant<TabItemControl>();
 
             foreach (var x in multitaskingControls)
             {
-                x.SetLoadingIndicatorStatus(x.Items.FirstOrDefault(x => x.Control == tabItemControl), isLoading);
+                x.SetLoadingIndicatorStatus(x.Items.FirstOrDefault(x => x.Control.TabItemContent == PaneHolder), isLoading);
             }
         }
 
