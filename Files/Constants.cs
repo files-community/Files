fb80bb35300bc6b679770951e07d8147d7609307
Marco Gavelli <marcogavelli2@gmail.com>
2021-10-13 15:56:52 +0200

    Do not constrain context menu height unless necessary (#6483)

--- Files/Constants.cs
+++ Files/Constants.cs
@@ -60,6 +60,12 @@
             public const double MinimumSidebarWidth = 250;
 
             public const double MaximumSidebarWidth = 500;
+
+            public const double ContextMenuMaxHeight = 480; // For contextmenu hacks, must match WinUI style
+            public const double ContextMenuSecondaryItemsHeight = 32; // For contextmenu hacks, must match WinUI style
+            public const double ContextMenuPrimaryItemsHeight = 48; // For contextmenu hacks, must match WinUI style
+            public const double ContextMenuLabelMargin = 10; // For contextmenu hacks
+            public const double ContextMenuItemsMaxWidth = 250; // For contextmenu hacks
         }
 
         public static class Browser
