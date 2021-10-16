89b8ea5d3db7cc4a25e5154537304a452f2abdbe
Steve <hez2010@outlook.com>
2021-10-16 18:27:47 +0800

    Remove redundant BOM and DataContext assignment

--- Files/UserControls/NavigationToolbar.xaml.cs
+++ Files/UserControls/NavigationToolbar.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Files.Helpers.XamlHelpers;
+using Files.Helpers.XamlHelpers;
 using Files.ViewModels;
 using System.Windows.Input;
 using Windows.System;
@@ -50,7 +50,6 @@ namespace Files.UserControls
         {
             InitializeComponent();
             Loading += NavigationToolbar_Loading;
-            DataContext = OngoingTasksViewModel;
         }
 
         private void NavigationToolbar_Loading(FrameworkElement sender, object args)
