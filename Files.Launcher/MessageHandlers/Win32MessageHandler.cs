5d97527ee40a68101cadccf0d24858003cc3efdf
Marco Gavelli <marcogavelli2@gmail.com>
2021-10-23 22:07:34 +0200

    Package.InstalledPath is not available on 1809

--- Files.Launcher/MessageHandlers/Win32MessageHandler.cs
+++ Files.Launcher/MessageHandlers/Win32MessageHandler.cs
@@ -117,7 +117,7 @@ namespace FilesFullTrust.MessageHandlers
                         Directory.CreateDirectory(destFolder);
                         if (enable)
                         {
-                            foreach (var file in Directory.GetFiles(Path.Combine(Package.Current.InstalledPath, "Files.Launcher", "Assets", "FilesOpenDialog")))
+                            foreach (var file in Directory.GetFiles(Path.Combine(Package.Current.InstalledLocation.Path, "Files.Launcher", "Assets", "FilesOpenDialog")))
                             {
                                 File.Copy(file, Path.Combine(destFolder, Path.GetFileName(file)), true);
                             }
@@ -144,7 +144,7 @@ namespace FilesFullTrust.MessageHandlers
                         Directory.CreateDirectory(destFolder);
                         if (enable)
                         {
-                            foreach (var file in Directory.GetFiles(Path.Combine(Package.Current.InstalledPath, "Files.Launcher", "Assets", "FilesOpenDialog")))
+                            foreach (var file in Directory.GetFiles(Path.Combine(Package.Current.InstalledLocation.Path, "Files.Launcher", "Assets", "FilesOpenDialog")))
                             {
                                 File.Copy(file, Path.Combine(destFolder, Path.GetFileName(file)), true);
                             }
