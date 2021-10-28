d52bd11004313439adbda78834be708a4334ee19
Steve <hez2010@outlook.com>
2021-10-28 20:53:52 +0800

    Fix return type

--- Files/ViewModels/ItemViewModel.cs
+++ Files/ViewModels/ItemViewModel.cs
@@ -1378,7 +1378,7 @@ namespace Files.ViewModels
                 client.Port = FtpHelpers.GetFtpPort(path);
                 client.Credentials = FtpManager.Credentials.Get(client.Host, FtpManager.Anonymous);
                 
-                static FtpProfile WrappedAutoConnectFtpAsync(FtpClient client)
+                static Task<FtpProfile> WrappedAutoConnectFtpAsync(FtpClient client)
                 {
                     try
                     {
