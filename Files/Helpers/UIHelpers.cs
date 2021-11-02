910c5c3442c31cf8d2ee84d675e94a1defb02de8
Marco Gavelli <marcogavelli2@gmail.com>
2021-11-02 18:27:16 +0100

    Do not use unused using

--- Files/Helpers/UIHelpers.cs
+++ Files/Helpers/UIHelpers.cs
@@ -1,9 +1,7 @@
 ﻿using Files.Common;
-using Files.Filesystem.StorageItems;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.AppService;
