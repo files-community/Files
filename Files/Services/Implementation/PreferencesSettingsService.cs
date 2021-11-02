632c4f656c873f2dcc17c5ddf83710ccae48c37c
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-11-02 13:42:09 -0400

    Merged setting pages

--- Files/Services/Implementation/PreferencesSettingsService.cs
+++ Files/Services/Implementation/PreferencesSettingsService.cs
@@ -16,6 +16,16 @@ namespace Files.Services.Implementation
             {
                 case nameof(ShowConfirmDeleteDialog):
                 case nameof(OpenFoldersInNewTab):
+                case nameof(ShowFileExtensions):
+                case nameof(AreHiddenItemsVisible):
+                case nameof(AreSystemItemsHidden):
+                case nameof(ListAndSortDirectoriesAlongsideFiles):
+                case nameof(OpenFilesWithOneClick):
+                case nameof(OpenFoldersWithOneClick):
+                case nameof(SearchUnindexedItems):
+                case nameof(AreLayoutPreferencesPerFolder):
+                case nameof(AdaptiveLayoutEnabled):
+                case nameof(AreFileTagsEnabled):
                     Microsoft.AppCenter.Analytics.Analytics.TrackEvent($"{nameof(e.settingName)} {e.newValue}");
                     break;
             }
@@ -30,6 +40,66 @@ namespace Files.Services.Implementation
         }
 
         public bool OpenFoldersInNewTab
+        {
+            get => Get(false);
+            set => Set(value);
+        }
+
+        public bool ShowFileExtensions
+        {
+            get => Get(true);
+            set => Set(value);
+        }
+
+        public bool AreHiddenItemsVisible
+        {
+            get => Get(false);
+            set => Set(value);
+        }
+
+        public bool AreSystemItemsHidden
+        {
+            get => Get(true);
+            set => Set(value);
+        }
+
+        public bool ListAndSortDirectoriesAlongsideFiles
+        {
+            get => Get(false);
+            set => Set(value);
+        }
+
+        public bool OpenFilesWithOneClick
+        {
+            get => Get(false);
+            set => Set(value);
+        }
+
+        public bool OpenFoldersWithOneClick
+        {
+            get => Get(false);
+            set => Set(value);
+        }
+
+        public bool SearchUnindexedItems
+        {
+            get => Get(false);
+            set => Set(value);
+        }
+
+        public bool AreLayoutPreferencesPerFolder
+        {
+            get => Get(true);
+            set => Set(value);
+        }
+
+        public bool AdaptiveLayoutEnabled
+        {
+            get => Get(true);
+            set => Set(value);
+        }
+
+        public bool AreFileTagsEnabled
         {
             get => Get(false);
             set => Set(value);
