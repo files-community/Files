2c17404fe933ac178b4ec7e2f8aad12344a83461
Marco Gavelli <marcogavelli2@gmail.com>
2021-11-28 14:02:17 +0100

    Restore changes from main

--- Files/Filesystem/StorageItems/ZipStorageFile.cs
+++ Files/Filesystem/StorageItems/ZipStorageFile.cs
@@ -350,7 +350,7 @@ namespace Files.Filesystem.StorageItems
             get
             {
                 var itemType = "ItemTypeFile".GetLocalized();
-                if (Name.Contains("."))
+                if (Name.Contains(".", StringComparison.Ordinal))
                 {
                     itemType = System.IO.Path.GetExtension(Name).Trim('.') + " " + itemType;
                 }
