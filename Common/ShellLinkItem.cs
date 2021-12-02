e6083c3f1b3489d7deb97c9ad7a17ecfc649165f
Marco Gavelli <marcogavelli2@gmail.com>
2021-12-02 03:16:18 +0100

    Show shortcuts icon

---
+++ Common/ShellLinkItem.cs
@@ -0,0 +1,27 @@
+﻿namespace Files.Common
+{
+    public class ShellLinkItem : ShellFileItem
+    {
+        public string TargetPath;
+        public string Arguments;
+        public string WorkingDirectory;
+        public bool RunAsAdmin;
+
+        public ShellLinkItem()
+        {
+        }
+
+        public ShellLinkItem(ShellFileItem baseItem)
+        {
+            this.RecyclePath = baseItem.RecyclePath;
+            this.FileName = baseItem.FileName;
+            this.FilePath = baseItem.FilePath;
+            this.RecycleDate = baseItem.RecycleDate;
+            this.ModifiedDate = baseItem.ModifiedDate;
+            this.CreatedDate = baseItem.CreatedDate;
+            this.FileSize = baseItem.FileSize;
+            this.FileSizeBytes = baseItem.FileSizeBytes;
+            this.FileType = baseItem.FileType;
+        }
+    }
+}
