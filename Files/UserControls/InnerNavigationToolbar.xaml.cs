28abc2d1e2008b23f5f6f08a5f5b61cb98984f8d
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-12-08 11:59:49 -0500

    Tweaked button style

--- Files/UserControls/InnerNavigationToolbar.xaml.cs
+++ Files/UserControls/InnerNavigationToolbar.xaml.cs
@@ -193,18 +193,5 @@ namespace Files.UserControls
                 }
             }
         }
-
-        private void NavToolbarDetailsHeader_PointerReleased(object sender, PointerRoutedEventArgs e)
-            => ViewModel.InstanceViewModel.FolderSettings.ToggleLayoutModeDetailsView.Execute(true);
-        private void NavToolbarTilesHeader_PointerReleased(object sender, PointerRoutedEventArgs e)
-            => ViewModel.InstanceViewModel.FolderSettings.ToggleLayoutModeTiles.Execute(true);
-        private void NavToolbarSmallIconsHeader_PointerReleased(object sender, PointerRoutedEventArgs e)
-            => ViewModel.InstanceViewModel.FolderSettings.ToggleLayoutModeGridViewSmall.Execute(true);
-        private void NavToolbarMediumIconsHeader_PointerReleased(object sender, PointerRoutedEventArgs e)
-            => ViewModel.InstanceViewModel.FolderSettings.ToggleLayoutModeGridViewMedium.Execute(true);
-        private void NavToolbarLargeIconsHeader_PointerReleased(object sender, PointerRoutedEventArgs e)
-            => ViewModel.InstanceViewModel.FolderSettings.ToggleLayoutModeGridViewLarge.Execute(true);
-        private void NavToolbarColumnsHeader_PointerReleased(object sender, PointerRoutedEventArgs e)
-            => ViewModel.InstanceViewModel.FolderSettings.ToggleLayoutModeColumnView.Execute(true);
     }
 }
