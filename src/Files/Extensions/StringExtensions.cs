fd846044372eeb53d83d4320bda5d11911da6684
cinqmilleans <cinq.mille.ans@gmail.com>
2021-12-17 13:56:10 +0100

    merge

--- src/Files/Extensions/StringExtensions.cs
+++ src/Files/Extensions/StringExtensions.cs
@@ -91,12 +91,12 @@ namespace Files.Extensions
             return value;
         }
 
-        public static string ToSizeString(this long size) => ToSizeString(ByteSize.FromBytes(size));
-        public static string ToSizeString(this ulong size) => ToSizeString(ByteSize.FromBytes(size));
+        public static string ToSizeString(this long size) => ByteSize.FromBytes(size).ToSizeString();
+        public static string ToSizeString(this ulong size) => ByteSize.FromBytes(size).ToSizeString();
         public static string ToSizeString(this ByteSize size) => size.ToBinaryString().ConvertSizeAbbreviation();
 
-        public static string ToLongSizeString(this long size) => ToLongSizeString(ByteSize.FromBytes(size));
-        public static string ToLongSizeString(this ulong size) => ToLongSizeString(ByteSize.FromBytes(size));
+        public static string ToLongSizeString(this long size) => ByteSize.FromBytes(size).ToLongSizeString();
+        public static string ToLongSizeString(this ulong size) => ByteSize.FromBytes(size).ToLongSizeString();
         public static string ToLongSizeString(this ByteSize size) => $"{size.ToSizeString()} ({size.Bytes:#,##0} {"ItemSizeBytes".GetLocalized()})";
     }
 }
