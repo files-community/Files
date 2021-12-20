2f08b887b8bc662f76c9e10971ababd1c87297c2
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2021-12-20 22:11:56 +0100

    Extended click area to whole settings' block

--- src/Files/Views/SettingsPages/About.xaml.cs
+++ src/Files/Views/SettingsPages/About.xaml.cs
@@ -1,12 +1,21 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Files.ViewModels.SettingsViewModels;
+using Windows.UI.Xaml.Controls;
 
 namespace Files.SettingsPages
 {
     public sealed partial class About : Page
     {
+        public AboutViewModel ViewModel
+        {
+            get => (AboutViewModel)DataContext;
+            set => DataContext = value;
+        }
+
         public About()
         {
             InitializeComponent();
+
+            this.ViewModel = new AboutViewModel();
         }
     }
 }
