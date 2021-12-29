9d00c8156339cad8b75200d3c5a9eacb11bd1f1a
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2021-12-29 10:45:53 -0500

    Redesigned general tab in properties window (#7378)

--- src/Files/Views/Pages/PropertiesGeneral.xaml.cs
+++ src/Files/Views/Pages/PropertiesGeneral.xaml.cs
@@ -16,16 +16,7 @@ namespace Files.Views
         public PropertiesGeneral()
         {
             this.InitializeComponent();
-            base.hashProgress = new Progress<float>();
-
-            (base.hashProgress as Progress<float>).ProgressChanged += PropertiesGeneral_ProgressChanged;
         }
-
-        private void PropertiesGeneral_ProgressChanged(object sender, float e)
-        {
-            ItemMD5HashProgress.Value = (double)e;
-        }
-
         public override async Task<bool> SaveChangesAsync(ListedItem item)
         {
             if (BaseProperties is DriveProperties driveProps)
@@ -116,7 +107,6 @@ namespace Files.Views
 
         public override void Dispose()
         {
-            (base.hashProgress as Progress<float>).ProgressChanged -= PropertiesGeneral_ProgressChanged;
         }
     }
 }
