5df1c32a6ad5b13e5e9ca9d9fc20df0f60894ca5
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-01-09 21:08:38 +0100

    Fix tests project warning, switch to windows 2019 image

--- tests/Files.InteractionTests/SessionManager.cs
+++ tests/Files.InteractionTests/SessionManager.cs
@@ -8,7 +8,7 @@ using System.Threading;
 namespace Files.InteractionTests
 {
     [TestClass]
-    internal class SessionManager
+    public internal class SessionManager
     {
         private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
         private const string FilesAppId = "49306atecsolution.FilesUWP_dwm5abbcs5pn0!App";
