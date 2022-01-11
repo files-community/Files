c6efdcd461295ba525620e4ad809349250aaf34d
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-01-11 01:41:43 +0100

    Add running interaction tests as part of CI (#7399)

--- tests/Files.InteractionTests/Tests/GeneralAppTests.cs
+++ tests/Files.InteractionTests/Tests/GeneralAppTests.cs
@@ -7,6 +7,12 @@ namespace Files.InteractionTests.Tests
         public void SessionGetsInitialized()
         {
             Assert.IsNotNull(SessionManager.Session);
+            try
+            {
+                // First run can be flaky due to external components
+                AxeHelper.AssertNoAccessibilityErrors();
+            }
+            catch (System.Exception) { }
             AxeHelper.AssertNoAccessibilityErrors();
         }
     }
