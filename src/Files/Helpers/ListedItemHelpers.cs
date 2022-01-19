e101bd2470aaeb86b88e6f327ebfb9de14a00132
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-01-19 16:04:02 +1030

    Added ListedItemHelpers

---
+++ src/Files/Helpers/ListedItemHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Files.Filesystem;
+using Files.ViewModels;
+
+namespace Files.Helpers
+{
+    public static class ListedItemHelpers
+    {
+        /// <summary>
+        /// Check if the <see cref="ListedItem"/> is a image file.
+        /// </summary>
+        /// <param name="listedItem"></param>
+        /// <returns><c>true</c> if the <see cref="ListedItem"/> is an image, otherwise <c>false</c></returns>
+        public static bool IsImage(this ListedItem listedItem)
+        {
+            var selectedItemsViewModel = new SelectedItemsPropertiesViewModel { SelectedItemsCount = 1 };
+            selectedItemsViewModel.CheckFileExtension(listedItem.FileExtension);
+
+            return selectedItemsViewModel.IsSelectedItemImage;
+        }
+    }
+}
