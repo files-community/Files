3bc857c3eb2b9e6cbb9f9ab91ff751160ea6fd9e
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-01-20 08:11:46 +1030

    Removed ListedItemHelpers, added FileExtensionHelpers

---
+++ src/Files/Helpers/FileExtensionHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Files.Helpers
+{
+    public static class FileExtensionHelpers
+    {
+        /// <summary>
+        /// Checks the file extension.
+        /// </summary>
+        /// <param name="fileExtensionToCheck">The file extension to check.</param>
+        /// <param name="fileExtensions">The file extensions.</param>
+        /// <returns><c>true</c> if the fileExtensionToCheck matches one of the fileExtensions,
+        /// <c>false</c> otherwise.</returns>
+        public static bool CheckFileExtension(string fileExtensionToCheck, IEnumerable<string> fileExtensions)
+        {
+            if (string.IsNullOrEmpty(fileExtensionToCheck))
+            {
+                return false;
+            }
+            
+            return fileExtensions.Any(ext => fileExtensionToCheck.Equals(ext, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
