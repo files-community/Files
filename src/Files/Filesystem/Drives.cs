90831f1812f0123ca9ae3b8de99b8b44bf665b09
cinqmilleans <46631671+cinqmilleans@users.noreply.github.com>
2022-02-02 18:49:43 +0100

    Cache folder sizes at runtime (#7960)

--- src/Files/Filesystem/Drives.cs
+++ src/Files/Filesystem/Drives.cs
@@ -26,6 +26,8 @@ namespace Files.Filesystem
 {
     public class DrivesManager : ObservableObject
     {
+        private static readonly IFolderSizeProvider folderSizeProvider = Ioc.Default.GetService<IFolderSizeProvider>();
+
         private IUserSettingsService UserSettingsService { get; } = Ioc.Default.GetService<IUserSettingsService>();
 
         private static readonly Logger Logger = App.Logger;
@@ -103,6 +105,7 @@ namespace Files.Filesystem
         {
             System.Diagnostics.Debug.WriteLine("DeviceWatcher_EnumerationCompleted");
             await RefreshUI();
+            folderSizeProvider.CleanCache();
         }
 
         private async Task RefreshUI()
