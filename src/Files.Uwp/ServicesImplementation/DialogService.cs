190da53ff1c3932817266e3d27a167900e122428
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-02-23 23:19:38 +0100

    Laid foundation for IDialogService

---
+++ src/Files.Uwp/ServicesImplementation/DialogService.cs
@@ -0,0 +1,40 @@
+﻿using Files.Backend.ViewModels.Dialogs;
+using Files.Backend.Services;
+using Files.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Files.ViewModels.Dialogs;
+using Files.Dialogs;
+
+namespace Files.Uwp.ServicesImplementation
+{
+    internal sealed class DialogService : IDialogService
+    {
+        private readonly Dictionary<Type, Func<ContentDialog>> _dialogs;
+
+        public DialogService()
+        {
+            this._dialogs = new()
+            {
+                { typeof(DecompressArchiveDialogViewModel), () => new DecompressArchiveDialog() }
+            };
+        }
+
+        public IDialog<TViewModel> GetDialog<TViewModel>(TViewModel viewModel)
+            where TViewModel : class, INotifyPropertyChanged
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DialogResult> ShowDialog<TViewModel>(TViewModel viewModel)
+            where TViewModel : class, INotifyPropertyChanged
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
