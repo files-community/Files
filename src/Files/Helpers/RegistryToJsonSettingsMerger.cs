276a28653b7ee3dc0b3c41657cc369a3dca97842
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2022-02-23 01:01:30 -0500

    Revert "Merge branch 'main' into 7zip"
    This reverts commit 3ca01c405e9e846f2ab141864fd0534052d2a721, reversing
    changes made to 5385d6d948a03735aed25c6ac36c6cbfb2f843c9.

---
+++ src/Files/Helpers/RegistryToJsonSettingsMerger.cs
@@ -0,0 +1,105 @@
+﻿using Files.Enums;
+using Files.Services;
+using Files.Services.Implementation;
+using Files.ViewModels;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Files.Helpers
+{
+    // TODO: Please remove this class after V2 version next since most of users would have merged the settings by that time
+    public class RegistryToJsonSettingsMerger
+    {
+        public static void MergeSettings()
+        {
+            if (!App.AppSettings.AreRegistrySettingsMergedToJson)
+            {
+                IUserSettingsService userSettingsService = Ioc.Default.GetService<IUserSettingsService>();
+                SettingsViewModel appSettings = App.AppSettings;
+
+                try
+                {
+                    // Pane
+                    userSettingsService.PaneSettingsService.Content = appSettings.Get(PaneContents.None, PaneSettingsService.ContentKey);
+                    userSettingsService.PaneSettingsService.HorizontalSizePx = appSettings.Get(300d, PaneSettingsService.HorizontalSizePxKey);
+                    userSettingsService.PaneSettingsService.VerticalSizePx = appSettings.Get(250d, PaneSettingsService.VerticalSizePxKey);
+                    userSettingsService.PaneSettingsService.MediaVolume = appSettings.Get(1.0d, PaneSettingsService.MediaVolumeKey);
+                    userSettingsService.PaneSettingsService.ShowPreviewOnly = appSettings.Get(false, PaneSettingsService.ShowPreviewOnlyKey);
+
+                    // Files and folders
+                    userSettingsService.PreferencesSettingsService.ShowFileExtensions = appSettings.Get(true, "ShowFileExtensions");
+                    userSettingsService.PreferencesSettingsService.AreHiddenItemsVisible = appSettings.Get(false, "AreHiddenItemsVisible");
+                    userSettingsService.PreferencesSettingsService.AreSystemItemsHidden = appSettings.Get(true, "AreSystemItemsHidden");
+                    userSettingsService.PreferencesSettingsService.ShowDotFiles = appSettings.Get(false, "ShowDotFiles");
+                    userSettingsService.PreferencesSettingsService.ListAndSortDirectoriesAlongsideFiles = appSettings.Get(false, "ListAndSortDirectoriesAlongsideFiles");
+                    userSettingsService.PreferencesSettingsService.OpenFilesWithOneClick = appSettings.Get(false, "OpenItemsWithOneClick");
+                    userSettingsService.PreferencesSettingsService.OpenFoldersWithOneClick = appSettings.Get(false, "OpenItemsWithOneClick");
+                    userSettingsService.PreferencesSettingsService.SearchUnindexedItems = appSettings.Get(false, "SearchUnindexedItems");
+                    userSettingsService.PreferencesSettingsService.AreLayoutPreferencesPerFolder = appSettings.Get(true, "AreLayoutPreferencesPerFolder");
+                    userSettingsService.PreferencesSettingsService.AdaptiveLayoutEnabled = appSettings.Get(true, "AdaptiveLayoutEnabled");
+                    userSettingsService.PreferencesSettingsService.AreFileTagsEnabled = appSettings.Get(false, "AreFileTagsEnabled");
+                    userSettingsService.PreferencesSettingsService.ShowFolderSize = appSettings.Get(false, "ShowFolderSize");
+
+                    // Multitasking
+                    userSettingsService.MultitaskingSettingsService.IsVerticalTabFlyoutEnabled = appSettings.Get(true, "IsVerticalTabFlyoutEnabled");
+                    userSettingsService.MultitaskingSettingsService.IsDualPaneEnabled = appSettings.Get(false, "IsDualPaneEnabled");
+                    userSettingsService.MultitaskingSettingsService.AlwaysOpenDualPaneInNewTab = appSettings.Get(false, "AlwaysOpenDualPaneInNewTab");
+
+                    // Widgets
+                    userSettingsService.WidgetsSettingsService.ShowFoldersWidget = appSettings.Get(true, "ShowFoldersWidget");
+                    userSettingsService.WidgetsSettingsService.ShowRecentFilesWidget = appSettings.Get(true, "ShowRecentFilesWidget");
+                    userSettingsService.WidgetsSettingsService.ShowDrivesWidget = appSettings.Get(true, "ShowDrivesWidget");
+                    userSettingsService.WidgetsSettingsService.ShowBundlesWidget = appSettings.Get(false, "ShowBundlesWidget");
+
+                    // Sidebar
+                    userSettingsService.AppearanceSettingsService.SidebarWidth = appSettings.Get(255d, "SidebarWidth");
+                    userSettingsService.AppearanceSettingsService.IsSidebarOpen = appSettings.Get(true, "IsSidebarOpen");
+                    userSettingsService.AppearanceSettingsService.ShowFavoritesSection = appSettings.Get(true, "ShowFavoritesSection");
+                    userSettingsService.AppearanceSettingsService.ShowLibrarySection = appSettings.Get(false, "ShowLibrarySection");
+                    userSettingsService.AppearanceSettingsService.ShowDrivesSection = appSettings.Get(true, "ShowDrivesSection");
+                    userSettingsService.AppearanceSettingsService.ShowCloudDrivesSection = appSettings.Get(true, "ShowCloudDrivesSection");
+                    userSettingsService.AppearanceSettingsService.ShowNetworkDrivesSection = appSettings.Get(true, "ShowNetworkDrivesSection");
+                    userSettingsService.AppearanceSettingsService.ShowWslSection = appSettings.Get(true, "ShowWslSection");
+                    userSettingsService.AppearanceSettingsService.PinRecycleBinToSidebar = appSettings.Get(true, "PinRecycleBinToSideBar");
+
+                    // Preferences
+                    userSettingsService.PreferencesSettingsService.ShowConfirmDeleteDialog = appSettings.Get(true, "ShowConfirmDeleteDialog");
+                    userSettingsService.PreferencesSettingsService.OpenFoldersInNewTab = appSettings.Get(false, "OpenFoldersNewTab");
+
+                    // Appearance
+                    userSettingsService.AppearanceSettingsService.MoveOverflowMenuItemsToSubMenu = appSettings.Get(true, "MoveOverflowMenuItemsToSubMenu");
+
+                    // Startup
+                    userSettingsService.PreferencesSettingsService.OpenSpecificPageOnStartup = appSettings.Get(false, "OpenASpecificPageOnStartup");
+                    userSettingsService.PreferencesSettingsService.OpenSpecificPageOnStartupPath = appSettings.Get("", "OpenASpecificPageOnStartupPath");
+                    userSettingsService.PreferencesSettingsService.ContinueLastSessionOnStartUp = appSettings.Get(false, "ContinueLastSessionOnStartUp");
+                    userSettingsService.PreferencesSettingsService.OpenNewTabOnStartup = appSettings.Get(true, "OpenNewTabPageOnStartup");
+                    userSettingsService.PreferencesSettingsService.AlwaysOpenNewInstance = appSettings.Get(false, "AlwaysOpenANewInstance");
+                    userSettingsService.PreferencesSettingsService.TabsOnStartupList = appSettings.Get<string[]>(null, "PagesOnStartupList")?.ToList();
+                    userSettingsService.PreferencesSettingsService.LastSessionTabList = appSettings.Get<string[]>(null, "LastSessionPages")?.ToList();
+
+                    // Layout settings
+                    userSettingsService.LayoutSettingsService.ShowDateColumn = appSettings.Get(true, "ShowDateColumn");
+                    userSettingsService.LayoutSettingsService.ShowDateCreatedColumn = appSettings.Get(false, "ShowDateCreatedColumn");
+                    userSettingsService.LayoutSettingsService.ShowTypeColumn = appSettings.Get(true, "ShowTypeColumn");
+                    userSettingsService.LayoutSettingsService.ShowSizeColumn = appSettings.Get(true, "ShowSizeColumn");
+                    userSettingsService.LayoutSettingsService.ShowFileTagColumn = appSettings.Get(true, "ShowFileTagColumn");
+                    userSettingsService.LayoutSettingsService.DefaultGridViewSize = appSettings.Get(Constants.Browser.GridViewBrowser.GridViewSizeSmall, "DefaultGridViewSize");
+                    userSettingsService.LayoutSettingsService.DefaultLayoutMode = (FolderLayoutModes)appSettings.Get((byte)FolderLayoutModes.DetailsView, "DefaultLayoutMode");
+                    userSettingsService.LayoutSettingsService.DefaultDirectorySortDirection = (SortDirection)appSettings.Get((byte)Microsoft.Toolkit.Uwp.UI.SortDirection.Ascending, "DefaultDirectorySortDirection");
+                    userSettingsService.LayoutSettingsService.DefaultDirectorySortOption = (SortOption)appSettings.Get((byte)SortOption.Name, "DefaultDirectorySortOption");
+                    userSettingsService.LayoutSettingsService.DefaultDirectoryGroupOption = (GroupOption)appSettings.Get((byte)GroupOption.None, "DefaultDirectoryGroupOption");
+
+                    App.AppSettings.AreRegistrySettingsMergedToJson = true;
+                }
+                catch (Exception ex)
+                {
+                    App.Logger.Warn(ex, "Merging settings failed");
+                    Debugger.Break();
+                }
+            }
+        }
+    }
+}
---
+++ src/Files/UserControls/PreviewPane.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Files.Services;
+using Files.ViewModels;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Uwp;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Files.UserControls
+{
+    public sealed partial class PreviewPane : UserControl
+    {
+        private readonly long modelChangedCallbackToken;
+
+        public static readonly DependencyProperty ModelProperty =
+            DependencyProperty.Register(nameof(Model), typeof(PreviewPaneViewModel), typeof(PreviewPane), new PropertyMetadata(null));
+
+
+        public PreviewPaneViewModel Model
+        {
+            get => (PreviewPaneViewModel)GetValue(ModelProperty);
+            set => SetValue(ModelProperty, value);
+        }
+
+        private IPaneSettingsService PaneSettingsService { get; } = Ioc.Default.GetService<IPaneSettingsService>();
+
+        private ObservableContext Context { get; } = new();
+
+        public PreviewPane()
+        {
+            InitializeComponent();
+            modelChangedCallbackToken = RegisterPropertyChangedCallback(ModelProperty, Model_DependencyPropertyChangedCallback);
+        }
+
+        private void Model_DependencyPropertyChangedCallback(DependencyObject sender, DependencyProperty dp) => Model?.TryRefresh();
+
+        private string GetLocalized(string resName) => resName.GetLocalized();
+
+        private void MenuFlyoutItem_Tapped(object sender, TappedRoutedEventArgs e) => Model?.UpdateSelectedItemPreview(true);
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            UnregisterPropertyChangedCallback(ModelProperty, modelChangedCallbackToken);
+            PreviewControlPresenter.Content = null;
+            Model = null;
+            Bindings.StopTracking();
+        }
+
+        private void Root_SizeChanged(object sender, SizeChangedEventArgs e)
+            => Context.IsHorizontal = Root.ActualWidth >= Root.ActualHeight;
+
+        private class ObservableContext : ObservableObject
+        {
+            private bool isHorizontal = false;
+            public bool IsHorizontal
+            {
+                get => isHorizontal;
+                set => SetProperty(ref isHorizontal, value);
+            }
+        }
+    }
+}
