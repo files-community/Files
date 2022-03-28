7eb47d2461e8ccfc0e221d46456392676121cef9
Van <Van@Github>
2022-03-28 18:05:10 +0700

    Update converter

--- src/Files.Uwp/Converters/BooleanToVisibilityConverter.cs
+++ src/Files.Uwp/Converters/BooleanToVisibilityConverter.cs
@@ -4,12 +4,12 @@ using Windows.UI.Xaml.Data;
 
 namespace Files.Converters
 {
-    public class BooleanToVisibilityInvertConverter : IValueConverter
+    public class BooleanToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language) =>
-            (bool)value ? Visibility.Collapsed : Visibility.Visible;
+            (bool)value ? Visibility.Visible : Visibility.Collapsed;
 
         public object ConvertBack(object value, Type targetType, object parameter, string language) =>
-            (Visibility)value == Visibility.Collapsed;
+            (Visibility)value == Visibility.Visible;
     }
 }
