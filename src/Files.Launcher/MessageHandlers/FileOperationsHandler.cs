0a2ff91daf776cf89f97cb1f6270f44dfa6a03fd
Marco Gavelli <marcogavelli2@gmail.com>
2022-04-15 22:56:09 +0200

    Removed extra namespace

--- src/Files.Launcher/MessageHandlers/FileOperationsHandler.cs
+++ src/Files.Launcher/MessageHandlers/FileOperationsHandler.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
