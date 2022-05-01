145a28a3c606bb3e0818c5aa55abda4c64af3cd3
Marco Gavelli <marcogavelli2@gmail.com>
2022-05-01 04:07:45 +0200

    Rename project Files.Launcher to Files.FullTrust (#9040)

---
+++ src/Files.FullTrust/MessageHandlers/IMessageHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace Files.FullTrust.MessageHandlers
+{
+    public interface IMessageHandler : IDisposable
+    {
+        Task ParseArgumentsAsync(PipeStream connection, Dictionary<string, object> message, string arguments);
+
+        void Initialize(PipeStream connection);
+    }
+}
