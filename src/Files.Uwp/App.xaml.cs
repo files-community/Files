0484d32a5e261fc7742525d2ead5bfff544d8790
Marco Gavelli <marcogavelli2@gmail.com>
2022-08-15 04:01:13 +0200

    Comment unsupported xaml

--- src/Files.Uwp/App.xaml.cs
+++ src/Files.Uwp/App.xaml.cs
@@ -16,12 +16,11 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Files.Uwp;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Files
+namespace Files.Uwp
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
