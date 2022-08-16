067badef0d28bf29798ee8644746b4d5f05bee36
Teissa "Quaint Mako <110472580+QuaintMako@users.noreply.github.com>
2022-08-16 15:48:16 +0200

    Feature: Support setting multiple images as desktop background (#9712)

--- src/Files.Uwp/BaseLayout.cs
+++ src/Files.Uwp/BaseLayout.cs
@@ -648,7 +648,7 @@ namespace Files.Uwp
             }
             shellContextMenuItemCancellationToken?.Cancel();
             shellContextMenuItemCancellationToken = new CancellationTokenSource();
-            SelectedItemsPropertiesViewModel.CheckFileExtension(SelectedItem?.FileExtension);
+            SelectedItemsPropertiesViewModel.CheckAllFileExtensions(this.SelectedItems.Select(selectedItem => selectedItem?.FileExtension).ToList());
             var shiftPressed = Window.Current.CoreWindow.GetKeyState(VirtualKey.Shift).HasFlag(CoreVirtualKeyStates.Down);
             var items = ContextFlyoutItemHelper.GetItemContextCommandsWithoutShellItems(currentInstanceViewModel: InstanceViewModel, workingDir: ParentShellPageInstance.FilesystemViewModel.WorkingDirectory, selectedItems: SelectedItems, selectedItemsPropertiesViewModel: SelectedItemsPropertiesViewModel, commandsViewModel: CommandsViewModel, shiftPressed: shiftPressed, showOpenMenu: false);
             ItemContextMenuFlyout.PrimaryCommands.Clear();
