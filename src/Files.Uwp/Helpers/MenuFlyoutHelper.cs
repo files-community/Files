9c2a9613255a092f4ce3d9195d13ff91dae33424
Marco Gavelli <marcogavelli2@gmail.com>
2022-08-22 22:37:26 +0200

    Fix build

--- src/Files.Uwp/Helpers/MenuFlyoutHelper.cs
+++ src/Files.Uwp/Helpers/MenuFlyoutHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using CommunityToolkit.WinUI;
+using System.Windows.Input;
 
 namespace Files.Uwp.Helpers
 {
