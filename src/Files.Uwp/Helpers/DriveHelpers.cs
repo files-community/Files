3f1ca9b873cf38babee7147ee3eafbe54ac2ee24
Marco Gavelli <marcogavelli2@gmail.com>
2022-08-23 20:02:22 +0200

    Discard result

--- src/Files.Uwp/Helpers/DriveHelpers.cs
+++ src/Files.Uwp/Helpers/DriveHelpers.cs
@@ -1,5 +1,4 @@
-﻿using Files.Shared.Extensions;
-using Files.Uwp.Interacts;
+﻿using Files.Uwp.Interacts;
 using Microsoft.Toolkit.Uwp;
 using Microsoft.Toolkit.Uwp.Notifications;
 using System;
@@ -13,15 +12,26 @@ namespace Files.Uwp.Helpers
 {
     public static class DriveHelpers
     {
+        // Eject using Shell Verb (fixes #6072, #6439)
         public static async Task EjectDeviceAsync(string path)
         {
-            // Eject using DeviceIoControl
-            //var removableDevice = new RemovableDevice(path);
-            //bool result = await removableDevice.EjectAsync();
+            var connection = await AppServiceConnectionHelper.Instance;
+            if (connection != null)
+            {
+                await connection.SendMessageAsync(new ValueSet()
+                {
+                    { "Arguments", "InvokeVerb" },
+                    { "FilePath", path },
+                    { "Verb", "eject" }
+                });
+            }
+        }
 
-            // Eject using Shell Verb (fixes #6072, #6439)
-            var result = await RequestEjectAsync(path);
-
+        // Eject using DeviceIoControl
+        /*public static async Task EjectDeviceAsync(string path)
+        {
+            var removableDevice = new RemovableDevice(path);
+            bool result = await removableDevice.EjectAsync();
             if (result)
             {
                 Debug.WriteLine("Device successfully ejected");
@@ -66,34 +76,6 @@ namespace Files.Uwp.Helpers
                     "EjectNotificationErrorDialogHeader".GetLocalized(),
                     "EjectNotificationErrorDialogBody".GetLocalized());
             }
-        }
-
-        private static async Task<bool> RequestEjectAsync(string path)
-        {
-            var connection = await AppServiceConnectionHelper.Instance;
-            if (connection != null)
-            {
-                var (status, response) = await connection.SendMessageForResponseAsync(new ValueSet()
-                {
-                    { "Arguments", "InvokeVerb" },
-                    { "FilePath", path },
-                    { "Verb", "eject" }
-                });
-                if (status == AppServiceResponseStatus.Success && response.Get("Success", false))
-                {
-                    for (int i = 0; i < 5; i++)
-                    {
-                        using var handle = NativeFileOperationsHelper.OpenFileForRead(path);
-                        if (handle.IsInvalid)
-                        {
-                            return true; // Drive was disconnected
-                        }
-                        handle.Dispose();
-                        await Task.Delay(500);
-                    }
-                }
-            }
-            return false;
-        }
+        }*/
     }
 }
