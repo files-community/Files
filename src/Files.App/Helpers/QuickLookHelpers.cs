43ab5748edd184f5b715290241eb3f54c9d79418
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2022-09-13 23:03:03 -0400

    Code cleanup

--- src/Files.App/Helpers/QuickLookHelpers.cs
+++ src/Files.App/Helpers/QuickLookHelpers.cs
@@ -8,87 +8,72 @@ namespace Files.App.Helpers;
 
 public static class QuickLookHelpers
 {
-    private const int TIMEOUT = 500;
+	private const int TIMEOUT = 500;
 
-    public static async Task ToggleQuickLook(IShellPage associatedInstance, bool switchPreview = false)
-    {
-        if (!associatedInstance.SlimContentPage.IsItemSelected || associatedInstance.SlimContentPage.IsRenamingItem)
-            return;
+	public static async Task ToggleQuickLook(IShellPage associatedInstance, bool switchPreview = false)
+	{
+		if (!associatedInstance.SlimContentPage.IsItemSelected || associatedInstance.SlimContentPage.IsRenamingItem)
+			return;
 
-        if (App.AppModel.IsQuickLookSupported == null)
-            await DetectQuickLook();
+		App.AppModel.IsQuickLookAvailable = await DetectQuickLookAvailability();
 
-        if (App.AppModel.IsQuickLookSupported == false)
-            return;
+		if (App.AppModel.IsQuickLookAvailable == false)
+			return;
 
-        App.Logger.Info("Toggle QuickLook");
+		string pipeName = $"QuickLook.App.Pipe.{WindowsIdentity.GetCurrent().User?.Value}";
+		string message = switchPreview ? "QuickLook.App.PipeMessages.Switch" : "QuickLook.App.PipeMessages.Toggle";
 
-        string pipeName = $"QuickLook.App.Pipe.{WindowsIdentity.GetCurrent().User?.Value}";
-        string message = switchPreview ? "QuickLook.App.PipeMessages.Switch" : "QuickLook.App.PipeMessages.Toggle";
+		await using var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+		try
+		{
+			await client.ConnectAsync(TIMEOUT);
 
-        await using var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
-        try
-        {
-            await client.ConnectAsync(TIMEOUT);
+			await using var writer = new StreamWriter(client);
+			await writer.WriteLineAsync($"{message}|{associatedInstance.SlimContentPage.SelectedItem.ItemPath}");
+			await writer.FlushAsync();
+		}
+		catch (TimeoutException)
+		{
+			client.Close();
+		}
+	}
 
-            await using var writer = new StreamWriter(client);
-            await writer.WriteLineAsync($"{message}|{associatedInstance.SlimContentPage.SelectedItem.ItemPath}");
-            await writer.FlushAsync();
-        }
-        catch (TimeoutException)
-        {
-            client.Close();
-        }
-    }
+	private static async Task<bool> DetectQuickLookAvailability()
+	{
+		static async Task<int> QuickLookServerAvailable()
+		{
+			string pipeName = $"QuickLook.App.Pipe.{WindowsIdentity.GetCurrent().User?.Value}";
+			string pipeSwitch = "QuickLook.App.PipeMessages.Switch";
 
-    private static async Task<bool> CheckQuickLookAvailability()
-    {
-        static async Task<int> QuickLookServerAvailable()
-        {
-            string pipeName = $"QuickLook.App.Pipe.{WindowsIdentity.GetCurrent().User?.Value}";
-            string pipeSwitch = "QuickLook.App.PipeMessages.Switch";
+			await using var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+			try
+			{
+				await client.ConnectAsync(TIMEOUT);
+				var serverInstances = client.NumberOfServerInstances;
 
-            await using var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
-            try
-            {
-                await client.ConnectAsync(TIMEOUT);
-                var serverInstances = client.NumberOfServerInstances;
+				await using var writer = new StreamWriter(client);
+				await writer.WriteLineAsync($"{pipeSwitch}|");
+				await writer.FlushAsync();
 
-                await using var writer = new StreamWriter(client);
-                await writer.WriteLineAsync($"{pipeSwitch}|");
-                await writer.FlushAsync();
+				return serverInstances;
+			}
+			catch (TimeoutException)
+			{
+				client.Close();
+				return 0;
+			}
+		}
 
-                return serverInstances;
-            }
-            catch (TimeoutException)
-            {
-                client.Close();
-                return 0;
-            }
-        }
-
-        try
-        {
-            var result = await QuickLookServerAvailable();
-            App.Logger.Info($"QuickLook detected: {result != 0}");
-            return result != 0;
-        }
-        catch (Exception ex)
-        {
-            App.Logger.Info(ex, ex.Message);
-            return false;
-        }
-    }
-
-    public static async Task DetectQuickLook()
-    {
-        try
-        {
-            App.AppModel.IsQuickLookSupported = await CheckQuickLookAvailability();
-        }
-        catch (Exception ex)
-        {
-            App.Logger.Warn(ex, ex.Message);
-        }
-    }
+		try
+		{
+			var result = await QuickLookServerAvailable();
+			App.Logger.Info($"QuickLook detected: {result != 0}");
+			return result != 0;
+		}
+		catch (Exception ex)
+		{
+			App.Logger.Info(ex, ex.Message);
+			return false;
+		}
+	}
 }
