e409719be176dd2a12fbe12395b1c64ef2778839
Teissa "Quaint Mako <110472580+QuaintMako@users.noreply.github.com>
2022-10-18 16:14:01 +0200

    Removed unused imports.

--- src/Files.App/Helpers/ThemeHelper.cs
+++ src/Files.App/Helpers/ThemeHelper.cs
@@ -1,11 +1,9 @@
+using CommunityToolkit.WinUI;
 using Files.App.Extensions;
-using System;
 using Windows.Storage;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Microsoft.UI.Xaml;
-using CommunityToolkit.WinUI;
-using Microsoft.UI;
 using Microsoft.UI.Windowing;
 
 namespace Files.App.Helpers
--- src/Files.App/Helpers/UIHelpers.cs
+++ src/Files.App/Helpers/UIHelpers.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
