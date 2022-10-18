53955f8c5cece1d6695728c42454b72c516caf0e
Teissa "Quaint Mako <110472580+QuaintMako@users.noreply.github.com>
2022-10-18 16:09:17 +0200

    Removed unused imports.

--- src/Files.App/Shell/LaunchHelpers.cs
+++ src/Files.App/Shell/LaunchHelpers.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Vanara.PInvoke;
