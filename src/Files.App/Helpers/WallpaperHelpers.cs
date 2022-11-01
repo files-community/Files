710db7f44a553a8344559ae4147527087935e9de
Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>
2022-11-01 19:01:33 +0100

    Fix: Fixed issue where setting image as wallpaper would add image to the local state folder (#10374)
    Fix: Fixed setting image as wallpaper adds image to the local state folder

--- src/Files.App/Helpers/WallpaperHelpers.cs
+++ src/Files.App/Helpers/WallpaperHelpers.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Linq;
-using Files.App.Filesystem;
-using Files.App.Filesystem.StorageItems;
 using Files.Shared.Enums;
 using Vanara.PInvoke;
 using Windows.Storage;
@@ -13,48 +11,25 @@ namespace Files.App.Helpers
 	{
 		public static async void SetAsBackground(WallpaperType type, string filePath)
 		{
-			if (UserProfilePersonalizationSettings.IsSupported())
+			if (type == WallpaperType.Desktop)
 			{
-				// Get the path of the selected file
-				BaseStorageFile sourceFile = await StorageHelpers.ToStorageItem<BaseStorageFile>(filePath);
-				if (sourceFile is null)
-				{
-					return;
-				}
-
-				// Get the app's local folder to use as the destination folder.
-				BaseStorageFolder localFolder = ApplicationData.Current.LocalFolder;
-
-				// the file to the destination folder.
-				// Generate unique name if the file already exists.
-				// If the file you are trying to set as the wallpaper has the same name as the current wallpaper,
-				// the system will ignore the request and no-op the operation
-				BaseStorageFile file = await FilesystemTasks.Wrap(() => sourceFile.CopyAsync(localFolder, sourceFile.Name, NameCollisionOption.GenerateUniqueName).AsTask());
-				if (file is null)
-				{
-					return;
-				}
-
-				UserProfilePersonalizationSettings profileSettings = UserProfilePersonalizationSettings.Current;
-				if (type == WallpaperType.Desktop)
-				{
-					// Set the desktop background
-					await profileSettings.TrySetWallpaperImageAsync(await file.ToStorageFileAsync());
-				}
-				else if (type == WallpaperType.LockScreen)
-				{
-					// Set the lockscreen background
-					await profileSettings.TrySetLockScreenImageAsync(await file.ToStorageFileAsync());
-				}
+				// Set the desktop background
+				var wallpaper = (Shell32.IDesktopWallpaper)new Shell32.DesktopWallpaper();
+				wallpaper.GetMonitorDevicePathAt(0, out var monitorId);
+				wallpaper.SetWallpaper(monitorId, filePath);
+			}
+			else if (type == WallpaperType.LockScreen)
+			{
+				// Set the lockscreen background
+				IStorageFile sourceFile = await StorageFile.GetFileFromPathAsync(filePath);
+				await LockScreen.SetImageFileAsync(sourceFile);
 			}
 		}
 
 		public static void SetSlideshow(string[] filePaths)
 		{
 			if (filePaths is null || filePaths.Any())
-			{
 				return;
-			}
 
 			var idList = filePaths.Select(Shell32.IntILCreateFromPath).ToArray();
 			Shell32.SHCreateShellItemArrayFromIDLists((uint)idList.Length, idList.ToArray(), out var shellItemArray);
