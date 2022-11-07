6e17866149ecc93392ee9ad9f4652c05d93e83d5
Yair Aichenbaum <39923744+yaichenbaum@users.noreply.github.com>
2022-11-07 01:15:26 -0500

    Clean up

--- tests/Files.InteractionTests/Tests/FolderTests.cs
+++ tests/Files.InteractionTests/Tests/FolderTests.cs
@@ -1,6 +1,8 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Interactions;
+using System;
 using System.Threading;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Files.InteractionTests.Tests
 {
@@ -45,69 +47,89 @@ namespace Files.InteractionTests.Tests
 		[TestMethod]
 		public void TestFolders()
 		{
-			var action = new Actions(SessionManager.Session);
+			NavigationTest();
 
-			// Navigation test
+			CreateFolderTest();
 
+			RenameFolderTest();
+
+			CopyPasteFolderTest();
+		}
+
+		/// <summary>
+		/// Tests folder navigation
+		/// </summary>
+		private void NavigationTest()
+		{
 			// Click on the desktop item in the sidebar
 			TestHelper.InvokeButtonById("Desktop");
+		}
 
 
-
-			// Create folder test
-
-			// User toolbar buttons to click the new folder option
+		/// <summary>
+		/// Tests folder creation and checks for accessibility issues along the way
+		/// </summary>
+		private void CreateFolderTest()
+		{
+			// Click the "New" button on the toolbar
 			TestHelper.InvokeButtonById("InnerNavigationToolbarNewButton");
+
+			// Click the "Folder" item from the menu flyout
 			TestHelper.InvokeButtonById("InnerNavigationToolbarNewFolderButton");
 
-			// Check for axe issues in the prompt
+			// Check for accessibility issues in the new folder prompt
 			AxeHelper.AssertNoAccessibilityErrors();
 
 			// Type the folder name
+			var action = new Actions(SessionManager.Session);
 			action.SendKeys("New Folder").Perform();
 
 			// Press the enter button to confirm
 			action = new Actions(SessionManager.Session);
 			action.SendKeys(Keys.Enter).Perform();
 
-			// Wait for item to be created
+			// Wait for folder to be created
 			Thread.Sleep(1000);
 
-			// Check for axe issues in the file area
+			// Check for accessibility issues in the new file area
 			AxeHelper.AssertNoAccessibilityErrors();
+		}
 
-
-
-			// Reanme folder test
-
-			// Click on the rename button
+		/// <summary>
+		/// Tests renaming a folder
+		/// </summary>
+		private void RenameFolderTest()
+		{
+			// Click the "Rename" button on the toolbar
 			TestHelper.InvokeButtonById("InnerNavigationToolbarRenameButton");
 
-			// Type the folder name
-			action = new Actions(SessionManager.Session);
+			// Type the new name into the inline text box
+			var action = new Actions(SessionManager.Session);
 			action.SendKeys("Renamed Folder").Perform();
 
 			// Press the enter button to save the new name
 			action = new Actions(SessionManager.Session);
 			action.SendKeys(Keys.Enter).Perform();
 
-			// Wait for item to be renamed
+			// Wait for the folder to be renamed
 			Thread.Sleep(1000);
+		}
 
-
-
-			// Copy and paste folder test
-
-			// Click on the copy button
+		/// <summary>
+		/// Tests copying and pasting a folder
+		/// </summary>
+		private void CopyPasteFolderTest()
+		{
+			// Click the "copy" button on the toolbar
 			TestHelper.InvokeButtonById("InnerNavigationToolbarCopyButton");
 
-			// Wait for item to be copied
+			// Wait for folder to be copied
 			Thread.Sleep(1000);
 
-			// Click on the paste button
+			// Click the "paste" button on the toolbar
 			TestHelper.InvokeButtonById("InnerNavigationToolbarPasteButton");
 
-			// Wait for item to be pasted
+			// Wait for folder to be pasted
 			Thread.Sleep(1000);
 		}
 	}
