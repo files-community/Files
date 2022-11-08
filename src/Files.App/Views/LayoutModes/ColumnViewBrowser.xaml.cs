3c9871476878f24001390400365eea47073f2960
Quaint Mako <110472580+QuaintMako@users.noreply.github.com>
2022-11-08 18:28:02 +0100

    Removed unnecessary comments

--- src/Files.App/Views/LayoutModes/ColumnViewBrowser.xaml.cs
+++ src/Files.App/Views/LayoutModes/ColumnViewBrowser.xaml.cs
@@ -219,8 +219,7 @@ namespace Files.App.Views.LayoutModes
 			if (activeBladeColumnViewBase == null)
 				return;
 
-			// QMK - This allows to deselect and reselect the parent folder, hence forcing the refocus.
-			// QMK - This is dirty as well, need to find better.
+			//This allows to deselect and reselect the parent folder, hence forcing the refocus.
 			var selectedItem = activeBladeColumnViewBase.FileList.SelectedItem;
 			activeBladeColumnViewBase.FileList.SelectedItem = null;
 			activeBladeColumnViewBase.FileList.SelectedItem = selectedItem;
