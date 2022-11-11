f5c37bdf541ce29574667b3aa45c41811b378fce
Quaint Mako <110472580+QuaintMako@users.noreply.github.com>
2022-11-11 18:36:25 +0100

    Added back ToObjectConverter class

--- src/Files.App/Converters/Converters.cs
+++ src/Files.App/Converters/Converters.cs
@@ -86,6 +86,36 @@ namespace Files.App.Converters
 	}
 
 	/// <summary>
+	/// The base class for converting instances of type T to object and vice versa.
+	/// </summary>
+	public abstract class ToObjectConverter<T> : ValueConverter<T?, object?>
+	{
+		/// <summary>
+		/// Converts a source value to the target type.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="parameter"></param>
+		/// <param name="language"></param>
+		/// <returns></returns>
+		protected override object? Convert(T? value, object? parameter, string? language)
+		{
+			return value;
+		}
+
+		/// <summary>
+		/// Converts a target value back to the source type.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="parameter"></param>
+		/// <param name="language"></param>
+		/// <returns></returns>
+		protected override T? ConvertBack(object? value, object? parameter, string? language)
+		{
+			return (T?)value;
+		}
+	}
+
+	/// <summary>
 	/// Converts a boolean to and from a visibility value.
 	/// </summary>
 	public class InverseBooleanConverter
