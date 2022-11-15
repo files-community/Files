953404cc7d8dc2f9e9c1a0735774263504398beb
d2dyno006 <53011783+d2dyno006@users.noreply.github.com>
2022-11-15 19:50:38 +0100

    Initial changes

---
+++ src/Files.App/UserControls/Widgets/TagsWidget.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Files.App.UserControls.Widgets
+{
+    public sealed partial class TagsWidget : UserControl
+    {
+        public TagsWidget()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
