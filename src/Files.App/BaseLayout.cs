3c3e8c1566461a1beb4bacb7047256eccad890ba
cinqmilleans <cinq.mille.ans@gmail.com>
2022-11-15 15:03:54 +0100

    Fix ContextMenu closes.

--- src/Files.App/BaseLayout.cs
+++ src/Files.App/BaseLayout.cs
@@ -534,13 +534,22 @@ namespace Files.App
 				BaseContextMenuFlyout.PrimaryCommands.Clear();
 				BaseContextMenuFlyout.SecondaryCommands.Clear();
 				var (primaryElements, secondaryElements) = ItemModelListToContextFlyoutHelper.GetAppBarItemsFromModel(items);
-				primaryElements.OfType<AppBarButton>().ForEach(i =>
-				{
-					i.Click += new RoutedEventHandler((s, e) => BaseContextMenuFlyout.Hide());  // Workaround for WinUI (#5508)
-				});
 				primaryElements.ForEach(i => BaseContextMenuFlyout.PrimaryCommands.Add(i));
 				secondaryElements.OfType<FrameworkElement>().ForEach(i => i.MinWidth = Constants.UI.ContextMenuItemsMaxWidth); // Set menu min width
 				secondaryElements.ForEach(i => BaseContextMenuFlyout.SecondaryCommands.Add(i));
+
+				// Workaround for WinUI (#5508)
+				var closeHandler = new RoutedEventHandler((s, e) => ItemContextMenuFlyout.Hide());
+				primaryElements
+					.OfType<AppBarButton>()
+					.ForEach(button => button.Click += closeHandler);
+				secondaryElements
+					.OfType<AppBarButton>()
+					.Select(item => item.Flyout)
+					.OfType<MenuFlyout>()
+					.SelectMany(menu => menu.Items)
+					.OfType<MenuFlyoutItem>()
+					.ForEach(button => button.Click += closeHandler);
 
 				if (!InstanceViewModel!.IsPageTypeSearchResults && !InstanceViewModel.IsPageTypeZipFolder)
 				{
@@ -587,13 +596,22 @@ namespace Files.App
 			ItemContextMenuFlyout.PrimaryCommands.Clear();
 			ItemContextMenuFlyout.SecondaryCommands.Clear();
 			var (primaryElements, secondaryElements) = ItemModelListToContextFlyoutHelper.GetAppBarItemsFromModel(items);
-			primaryElements.OfType<AppBarButton>().ForEach(i =>
-			{
-				i.Click += new RoutedEventHandler((s, e) => ItemContextMenuFlyout.Hide()); // Workaround for WinUI (#5508)
-			});
 			primaryElements.ForEach(i => ItemContextMenuFlyout.PrimaryCommands.Add(i));
 			secondaryElements.OfType<FrameworkElement>().ForEach(i => i.MinWidth = Constants.UI.ContextMenuItemsMaxWidth); // Set menu min width
 			secondaryElements.ForEach(i => ItemContextMenuFlyout.SecondaryCommands.Add(i));
+
+			// Workaround for WinUI (#5508)
+			var closeHandler = new RoutedEventHandler((s, e) => ItemContextMenuFlyout.Hide());
+			primaryElements
+				.OfType<AppBarButton>()
+				.ForEach(button => button.Click += closeHandler);
+			secondaryElements
+				.OfType<AppBarButton>()
+				.Select(item => item.Flyout)
+				.OfType<MenuFlyout>()
+				.SelectMany(menu => menu.Items)
+				.OfType<MenuFlyoutItem>()
+				.ForEach(button => button.Click += closeHandler);
 
 			if (InstanceViewModel!.CanTagFilesInPage)
 				AddNewFileTagsToMenu(ItemContextMenuFlyout);
