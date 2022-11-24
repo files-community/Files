8293d850a4129833c278fea66e43884c35193e7b
Marco Franzen <mail@marco-franzen.de>
2022-11-24 17:42:05 +0100

    Open archives in existing tab if files is set as default app

--- src/Files.App/Filesystem/StorageItems/ZipStorageFolder.cs
+++ src/Files.App/Filesystem/StorageItems/ZipStorageFolder.cs
@@ -100,7 +100,7 @@ namespace Files.App.Filesystem.StorageItems
 				var assoc = await NativeWinApiHelper.GetFileAssociationAsync(filePath);
 				if (assoc is not null)
 				{
-					return assoc == Package.Current.Id.FamilyName
+					return assoc.EndsWith("Files.App\\Files.exe", StringComparison.OrdinalIgnoreCase)
 						|| assoc.Equals(IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "explorer.exe"), StringComparison.OrdinalIgnoreCase);
 				}
 				return true;
