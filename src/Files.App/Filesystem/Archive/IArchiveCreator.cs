b87f340d8926bcd5d24508c67096d6b94ea15947
cinqmilleans <46631671+cinqmilleans@users.noreply.github.com>
2022-11-27 16:38:09 +0100

    Feature: Added advanced options when creating archives (#10524)

---
+++ src/Files.App/Filesystem/Archive/IArchiveCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Files.App.Filesystem.Archive
+{
+	public interface IArchiveCreator
+	{
+		string ArchivePath { get; }
+
+		string Directory { get; }
+		string FileName { get; }
+		string Password { get; }
+
+		IEnumerable<string> Sources { get; }
+
+		ArchiveFormats FileFormat { get; }
+		ArchiveCompressionLevels CompressionLevel { get; }
+		ArchiveSplittingSizes SplittingSize { get; }
+
+		IProgress<float> Progress { get; set; }
+
+		Task<bool> RunCreationAsync();
+	}
+}
