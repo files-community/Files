af223b898935266936169a300f780157dd3f7170
Pablo García Ruiz <55754091+hecksmosis@users.noreply.github.com>
2022-12-01 19:46:00 +0100

    formatting

--- src/Files.App/DataModels/NavigationControlItems/LocationItem.cs
+++ src/Files.App/DataModels/NavigationControlItems/LocationItem.cs
@@ -57,6 +57,6 @@ namespace Files.App.DataModels.NavigationControlItems
 
 		public ContextMenuOptions? MenuOptions { get; set; }
 
-        public int CompareTo(INavigationControlItem? other) => Text is not null ? Text.CompareTo(other?.Text) : 0;
-    }
+		public int CompareTo(INavigationControlItem? other) => Text is not null ? Text.CompareTo(other?.Text) : 0;
+	}
 }
