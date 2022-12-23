9ef6b282e63264e6d58c3760d95136b78f4fbfb8
cinqmilleans <cinq.mille.ans@gmail.com>
2022-12-23 07:35:40 +0100

    changes

--- src/Files.App/Helpers/ResourceHelpers.cs
+++ src/Files.App/Helpers/ResourceHelpers.cs
@@ -6,10 +6,10 @@ namespace Files.App.Helpers
 	[MarkupExtensionReturnType(ReturnType = typeof(string))]
 	public sealed class ResourceString : MarkupExtension
 	{
-		private static readonly ResourceLoader loader = new();
+		private static readonly ResourceLoader resourceLoader = new();
 
 		public string Name { get; set; } = string.Empty;
 
-		protected override object ProvideValue() => loader.GetString(Name);
+		protected override object ProvideValue() => resourceLoader.GetString(Name);
 	}
 }
--- src/Files.App/UserControls/Settings/SettingsBlockControl.xaml.cs
+++ src/Files.App/UserControls/Settings/SettingsBlockControl.xaml.cs
@@ -10,73 +10,22 @@ namespace Files.App.UserControls.Settings
 	[ContentProperty(Name = nameof(SettingsActionableElement))]
 	public sealed partial class SettingsBlockControl : UserControl
 	{
-		public event EventHandler<bool>? Click;
-
-		public static readonly DependencyProperty TitleProperty = DependencyProperty
-			.Register(nameof(Title), typeof(string), typeof(SettingsBlockControl), new(null));
-
-		public static readonly DependencyProperty DescriptionProperty = DependencyProperty
-			.Register(nameof(Description), typeof(string), typeof(SettingsBlockControl), new(null));
-
-		public static readonly DependencyProperty AdditionalDescriptionContentProperty = DependencyProperty
-			.Register(nameof(AdditionalDescriptionContent), typeof(FrameworkElement), typeof(SettingsBlockControl), new(null));
-
-		public static readonly DependencyProperty IconProperty = DependencyProperty
-			.Register(nameof(Icon), typeof(IconElement), typeof(SettingsBlockControl), new(null));
-
-		public static readonly DependencyProperty ButtonCommandProperty = DependencyProperty
-			.Register(nameof(ButtonCommand), typeof(ICommand), typeof(SettingsBlockControl), new(null));
-
-		public static readonly DependencyProperty IsClickableProperty = DependencyProperty
-			.Register(nameof(IsClickable), typeof(bool), typeof(SettingsBlockControl), new(false));
-
-		public static readonly DependencyProperty IsExpandedProperty = DependencyProperty
-			.Register(nameof(IsExpanded), typeof(bool), typeof(SettingsBlockControl), new(false));
-
-		public static readonly DependencyProperty ExpandableContentProperty = DependencyProperty
-			.Register(nameof(ExpandableContent), typeof(FrameworkElement), typeof(SettingsBlockControl), new(null));
-
-		public string Title
-		{
-			get => (string)GetValue(TitleProperty);
-			set => SetValue(TitleProperty, value);
-		}
-
-		public string Description
-		{
-			get => (string)GetValue(DescriptionProperty);
-			set => SetValue(DescriptionProperty, value);
-		}
-
-		public FrameworkElement AdditionalDescriptionContent
-		{
-			get => (FrameworkElement)GetValue(AdditionalDescriptionContentProperty);
-			set => SetValue(AdditionalDescriptionContentProperty, value);
-		}
-
-		public IconElement Icon
-		{
-			get => (IconElement)GetValue(IconProperty);
-			set => SetValue(IconProperty, value);
-		}
+		public FrameworkElement SettingsActionableElement { get; set; }
 
 		public ICommand ButtonCommand
 		{
-			get => (ICommand)GetValue(ButtonCommandProperty);
-			set => SetValue(ButtonCommandProperty, value);
+			get { return (ICommand)GetValue(ButtonCommandProperty); }
+			set { SetValue(ButtonCommandProperty, value); }
 		}
+		public static readonly DependencyProperty ButtonCommandProperty =
+			DependencyProperty.Register("ButtonCommand", typeof(ICommand), typeof(SettingsBlockControl), new PropertyMetadata(null));
 
-		public bool IsClickable
-		{
-			get => (bool)GetValue(IsClickableProperty);
-			set => SetValue(IsClickableProperty, value);
-		}
-
-		public bool IsExpanded
-		{
-			get => (bool)GetValue(IsExpandedProperty);
-			set => SetValue(IsExpandedProperty, value);
-		}
+		public static readonly DependencyProperty ExpandableContentProperty = DependencyProperty.Register(
+		  "ExpandableContent",
+		  typeof(FrameworkElement),
+		  typeof(SettingsBlockControl),
+		  new PropertyMetadata(null)
+		);
 
 		public FrameworkElement ExpandableContent
 		{
@@ -84,18 +33,111 @@ namespace Files.App.UserControls.Settings
 			set => SetValue(ExpandableContentProperty, value);
 		}
 
-		public FrameworkElement? SettingsActionableElement { get; set; }
+		public static readonly DependencyProperty AdditionalDescriptionContentProperty = DependencyProperty.Register(
+		  "AdditionalDescriptionContent",
+		  typeof(FrameworkElement),
+		  typeof(SettingsBlockControl),
+		  new PropertyMetadata(null)
+		);
 
-		public SettingsBlockControl() => InitializeComponent();
-
-		private void SettingsBlockControl_Loaded(object _, RoutedEventArgs e)
+		public FrameworkElement AdditionalDescriptionContent
 		{
-			Loaded -= SettingsBlockControl_Loaded;
-			if (ActionableButton is not null)
-				AutomationProperties.SetName(ActionableButton, Title);
+			get => (FrameworkElement)GetValue(AdditionalDescriptionContentProperty);
+			set => SetValue(AdditionalDescriptionContentProperty, value);
 		}
 
-		private void Expander_Collapsed(Expander _, ExpanderCollapsedEventArgs e) => Click?.Invoke(this, false);
-		private void Expander_Expanding(Expander _, ExpanderExpandingEventArgs e) => Click?.Invoke(this, true);
+		public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+		  "Title",
+		  typeof(string),
+		  typeof(SettingsBlockControl),
+		  new PropertyMetadata(null)
+		);
+
+		public string Title
+		{
+			get => (string)GetValue(TitleProperty);
+			set => SetValue(TitleProperty, value);
+		}
+
+		public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+		  "Description",
+		  typeof(string),
+		  typeof(SettingsBlockControl),
+		  new PropertyMetadata(null)
+		);
+
+		public string Description
+		{
+			get => (string)GetValue(DescriptionProperty);
+			set => SetValue(DescriptionProperty, value);
+		}
+
+		public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+		  "Icon",
+		  typeof(IconElement),
+		  typeof(SettingsBlockControl),
+		  new PropertyMetadata(null)
+		);
+
+		public IconElement Icon
+		{
+			get => (IconElement)GetValue(IconProperty);
+			set => SetValue(IconProperty, value);
+		}
+
+		public static readonly DependencyProperty IsClickableProperty = DependencyProperty.Register(
+		  "IsClickable",
+		  typeof(bool),
+		  typeof(SettingsBlockControl),
+		  new PropertyMetadata(false)
+		);
+
+		public bool IsClickable
+		{
+			get => (bool)GetValue(IsClickableProperty);
+			set => SetValue(IsClickableProperty, value);
+		}
+
+		public static readonly DependencyProperty IsExpandedProperty = DependencyProperty.Register(
+		  "IsExpanded",
+		  typeof(bool),
+		  typeof(SettingsBlockControl),
+		  new PropertyMetadata(false)
+		);
+
+		public bool IsExpanded
+		{
+			get => (bool)GetValue(IsExpandedProperty);
+			set => SetValue(IsExpandedProperty, value);
+		}
+
+		//
+		// Summary:
+		//     Occurs when a button control is clicked.
+		public event EventHandler<bool> Click;
+
+		public SettingsBlockControl()
+		{
+			this.InitializeComponent();
+			this.Loaded += SettingsBlockControl_Loaded;
+		}
+
+		private void SettingsBlockControl_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (ActionableButton is not null)
+			{
+				AutomationProperties.SetName(ActionableButton, Title);
+			}
+		}
+
+		private void Expander_Expanding(Microsoft.UI.Xaml.Controls.Expander sender, Microsoft.UI.Xaml.Controls.ExpanderExpandingEventArgs args)
+		{
+			Click?.Invoke(this, true);
+		}
+
+		private void Expander_Collapsed(Microsoft.UI.Xaml.Controls.Expander sender, Microsoft.UI.Xaml.Controls.ExpanderCollapsedEventArgs args)
+		{
+			Click?.Invoke(this, false);
+		}
 	}
 }
