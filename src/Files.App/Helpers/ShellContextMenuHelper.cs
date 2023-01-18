c8db29e73fa71679c16da97ed751efaf74ccda39
yaira2 <39923744+yaira2@users.noreply.github.com>
2023-01-18 12:47:34 -0500

    Update src/Files.App/Helpers/ShellContextMenuHelper.cs

--- src/Files.App/Helpers/ShellContextMenuHelper.cs
+++ src/Files.App/Helpers/ShellContextMenuHelper.cs
@@ -202,10 +202,6 @@ namespace Files.App.Helpers
 				void InstallFont(string path, bool asAdmin)
 				{
 					string dir = asAdmin ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Fonts") : Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Microsoft", "Windows", "Fonts");
-					if (asAdmin)
-						dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Fonts");
-					else
-						dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Microsoft", "Windows", "Fonts");
 					
 					Win32API.RunPowershellCommand($"-command \"Copy-Item '{path}' '{dir}'; New-ItemProperty -Name '{Path.GetFileNameWithoutExtension(path)}' -Path 'HKCU:\\Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts' -PropertyType string -Value '{dir}'\"", asAdmin);
 				}
