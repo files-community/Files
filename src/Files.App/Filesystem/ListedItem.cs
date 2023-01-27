8dd81765ee178b7e2ae9af8f435d258ed1839b92
Yair <39923744+yaira2@users.noreply.github.com>
2023-01-27 09:53:28 -0500

    Update ListedItem.cs

--- src/Files.App/Filesystem/ListedItem.cs
+++ src/Files.App/Filesystem/ListedItem.cs
@@ -15,6 +15,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Storage;
