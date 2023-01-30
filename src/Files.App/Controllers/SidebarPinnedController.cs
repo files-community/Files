23b89d0348c55866f0b58523b7671b0d77e071fa
Pablo García Ruiz <pgruiz727@gmail.com>
2023-01-30 20:48:24 +0100

    Add refreshing pinned items when they are modified in explorer

--- src/Files.App/Controllers/SidebarPinnedController.cs
+++ src/Files.App/Controllers/SidebarPinnedController.cs
@@ -1,6 +1,8 @@
 using Files.App.DataModels;
+using Files.App.Filesystem;
 using System;
 using System.Collections.Specialized;
+using System.IO;
 using System.Threading.Tasks;
 
 
@@ -14,8 +16,12 @@ namespace Files.App.Controllers
 		{
 			Model = new SidebarPinnedModel();
 			Model.SetController(this);
+
+			PinnedItemsManager.Default.PinnedItemsModified += LoadAsync;
 		}
-		
+
+		public async void LoadAsync(object? sender, FileSystemEventArgs e)
+			=> await LoadAsync();
 		public async Task LoadAsync()
 			=> await Model.UpdateItemsWithExplorer();
 	}
---
+++ src/Files.App/Filesystem/PinnedItemsManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Files.App.Filesystem
+{
+	public sealed class PinnedItemsManager
+	{
+		private static readonly Lazy<PinnedItemsManager> lazy = new(() => new PinnedItemsManager());
+		public FileSystemWatcher? PinnedItemsWatcher;
+		public event FileSystemEventHandler? PinnedItemsModified;
+
+		public static PinnedItemsManager Default
+		{
+			get 
+			{
+				return lazy.Value;
+			}
+		}
+
+		private PinnedItemsManager()
+		{
+			Initialize();
+		}	
+		
+		public void Initialize()
+		{
+			PinnedItemsWatcher = new()
+			{
+				Path = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Microsoft", "Windows", "Recent", "AutomaticDestinations"),
+				Filter = "f01b4d95cf55d32a.automaticDestinations-ms",
+				NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName,
+				EnableRaisingEvents = true
+			};
+
+			PinnedItemsWatcher.Changed += PinnedItemsWatcher_Changed;
+		}
+
+		private void PinnedItemsWatcher_Changed(object sender, FileSystemEventArgs e)
+			=> PinnedItemsModified?.Invoke(this, e);
+		
+	}
+}
