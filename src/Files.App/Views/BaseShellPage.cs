ad57648d9d6d68c046142e6253f1090e20389dfa
ferrariofilippo <f.filippo49@gmail.com>
2023-02-06 22:08:02 +0100

    Removed using

--- src/Files.App/Views/BaseShellPage.cs
+++ src/Files.App/Views/BaseShellPage.cs
@@ -27,7 +27,6 @@ using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
