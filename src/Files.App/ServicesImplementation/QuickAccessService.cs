b2e32885b8783cb181c2f3d0bc3f14783e76166b
Marco Gavelli <marcogavelli2@gmail.com>
2023-02-06 01:03:45 +0100

    Fix: Fixed quick access widgets when there are few items (#11169)

--- src/Files.App/ServicesImplementation/QuickAccessService.cs
+++ src/Files.App/ServicesImplementation/QuickAccessService.cs
@@ -16,31 +16,26 @@ namespace Files.App.ServicesImplementation
 	{
 		private readonly static string guid = "::{679f85cb-0220-4080-b29b-5540cc05aab6}";
 
-		public async Task<List<string>> GetPinnedFoldersAsync(bool getRecentItems = false)
+		public async Task<IEnumerable<ShellFileItem>> GetPinnedFoldersAsync()
 		{
-			var sidebarItems =  (await Win32Shell.GetShellFolderAsync(guid, "Enumerate", 0, 10000)).Enumerate
-				.Where(link => link.IsFolder)
-				.Select(link => link.FilePath).ToList();
-
-			if (sidebarItems.Count > 4 && !getRecentItems) // Avoid first opening crash #11139
-				sidebarItems.RemoveRange(sidebarItems.Count - 4, 4); // 4 is the number of recent items shown in explorer sidebar
-			
-			return sidebarItems;
+			return (await Win32Shell.GetShellFolderAsync(guid, "Enumerate", 0, int.MaxValue, "System.Home.IsPinned")).Enumerate
+				.Where(link => link.IsFolder);
 		}
-		
-		public async Task PinToSidebar(string folderPath)
-			=> await PinToSidebar(new[] { folderPath });
+
+		public Task PinToSidebar(string folderPath)
+			=> PinToSidebar(new[] { folderPath });
 		
 		public async Task PinToSidebar(string[] folderPaths)
 		{
 			await ContextMenu.InvokeVerb("pintohome", folderPaths);
+
 			await App.QuickAccessManager.Model.LoadAsync();
 			
 			App.QuickAccessManager.UpdateQuickAccessWidget?.Invoke(this, new ModifyQuickAccessEventArgs(folderPaths, true));
 		}
 		
-		public async Task UnpinFromSidebar(string folderPath)
-			=> await UnpinFromSidebar(new[] { folderPath });
+		public Task UnpinFromSidebar(string folderPath)
+			=> UnpinFromSidebar(new[] { folderPath });
 		
 		public async Task UnpinFromSidebar(string[] folderPaths)
 		{
@@ -50,7 +45,7 @@ namespace Files.App.ServicesImplementation
 
 			foreach (dynamic? fi in f2.Items())
 				if (folderPaths.Contains((string)fi.Path))
-					await SafetyExtensions.IgnoreExceptions(async () => { 
+					await SafetyExtensions.IgnoreExceptions(async () => {
 						await fi.InvokeVerb("unpinfromhome");
 					});
 
