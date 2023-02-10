d1088b2762c80c90f20af1bd64dc45332040bdad
Yair <39923744+yaira2@users.noreply.github.com>
2023-02-10 16:00:14 -0500

    Update DetailsLayoutBrowser.xaml.cs

--- src/Files.App/Views/LayoutModes/DetailsLayoutBrowser.xaml.cs
+++ src/Files.App/Views/LayoutModes/DetailsLayoutBrowser.xaml.cs
@@ -741,12 +741,12 @@ namespace Files.App.Views.LayoutModes
 
 		private void FileTag_PointerEntered(object sender, PointerRoutedEventArgs e)
 		{
-			VisualStateManager.GoToState((UserControl)sender, "PointerOver", true);
+			VisualStateManager.GoToState((Border)sender, "PointerOver", true);
 		}
 
 		private void FileTag_PointerExited(object sender, PointerRoutedEventArgs e)
 		{
-			VisualStateManager.GoToState((UserControl)sender, "Normal", true);
+			VisualStateManager.GoToState((Border)sender, "Normal", true);
 		}
 
 		private void TagIcon_Tapped(object sender, TappedRoutedEventArgs e)
