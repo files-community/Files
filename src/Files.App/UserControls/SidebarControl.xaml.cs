9f7aff469527a1f01731dc914da9f45fddda937c
Yair <39923744+yaira2@users.noreply.github.com>
2023-02-13 00:31:49 -0500

    Update SidebarControl.xaml.cs

--- src/Files.App/UserControls/SidebarControl.xaml.cs
+++ src/Files.App/UserControls/SidebarControl.xaml.cs
@@ -282,6 +282,7 @@ namespace Files.App.UserControls
 					ID = "ItemOverflow",
 					Tag = "ItemOverflow",
 					IsEnabled = false,
+					IsHidden = !options.ShowShellItems,
 				}
 			}.Where(x => x.ShowItem).ToList();
 		}
