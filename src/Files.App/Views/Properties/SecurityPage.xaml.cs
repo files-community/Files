26794cb85ce98bc45258f967e63470987327789d
U+5BFA <62196528+onein528@users.noreply.github.com>
2023-03-06 01:25:42 +0900

    Code Quality: Refactored code for security settings (#11471)

--- src/Files.App/Views/Properties/SecurityPage.xaml.cs
+++ src/Files.App/Views/Properties/SecurityPage.xaml.cs
@@ -7,7 +7,6 @@ using Files.App.Helpers;
 using Files.App.ViewModels.Properties;
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
@@ -20,108 +19,88 @@ namespace Files.App.Views.Properties
 {
 	public sealed partial class SecurityPage : BasePropertiesPage
 	{
-		public RelayCommand OpenAdvancedPropertiesCommand { get; set; }
-
-		public SecurityViewModel SecurityProperties { get; set; }
-
-		private AppWindow? propsView;
-
 		public SecurityPage()
 		{
 			InitializeComponent();
 
-			OpenAdvancedPropertiesCommand = new RelayCommand(OpenAdvancedProperties);
+			_isWinUI3 = ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8);
+
+			OpenSecurityAdvancedPageWindowCommand = new RelayCommand(OpenSecurityAdvancedPageWindow);
 		}
+
+		public SecurityViewModel? SecurityViewModel { get; set; }
+
+		private AppWindow? propsView;
+
+		private readonly bool _isWinUI3;
+
+		public RelayCommand OpenSecurityAdvancedPageWindowCommand { get; set; }
 
 		protected override void OnNavigatedTo(NavigationEventArgs e)
 		{
-			var np = e.Parameter as MainPropertiesPage.PropertyNavParam;
+			var np = (MainPropertiesPage.PropertyNavParam)e.Parameter;
 
 			if (np.navParameter is ListedItem listedItem)
-			{
-				SecurityProperties = new SecurityViewModel(listedItem);
-			}
+				SecurityViewModel = new SecurityViewModel(listedItem);
 			else if (np.navParameter is DriveItem driveitem)
-			{
-				SecurityProperties = new SecurityViewModel(driveitem);
-			}
+				SecurityViewModel = new SecurityViewModel(driveitem);
 
 			base.OnNavigatedTo(e);
 		}
 
-		public async override Task<bool> SaveChangesAsync()
+		private void OpenSecurityAdvancedPageWindow()
 		{
-			return SecurityProperties is null || SecurityProperties.SetFilePermissions();
-		}
-
-		protected override void Properties_Loaded(object sender, RoutedEventArgs e)
-		{
-			base.Properties_Loaded(sender, e);
-
-			if (SecurityProperties is not null)
-			{
-				SecurityProperties.GetFilePermissions();
-			}
-		}
-
-		public override void Dispose()
-		{
-
-		}
-
-		private void OpenAdvancedProperties()
-		{
-			if (SecurityProperties is null)
-			{
+			if (SecurityViewModel is null)
 				return;
-			}
 
-			if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 8))
+			if (_isWinUI3)
 			{
 				if (propsView is null)
 				{
-					var frame = new Frame();
-					frame.RequestedTheme = ThemeHelper.RootTheme;
-					frame.Navigate(typeof(SecurityAdvancedPage), new PropertiesPageNavigationArguments()
+					var frame = new Frame()
 					{
-						Item = SecurityProperties.Item
-					}, new SuppressNavigationTransitionInfo());
+						RequestedTheme = ThemeHelper.RootTheme
+					};
+
+					frame.Navigate(
+						typeof(SecurityAdvancedPage),
+						new PropertiesPageNavigationArguments() { Item = SecurityViewModel.Item },
+						new SuppressNavigationTransitionInfo());
 
 					// Initialize window
-					var propertiesWindow = new WinUIEx.WindowEx()
+					var newWindow = new WinUIEx.WindowEx()
 					{
 						IsMinimizable = false,
-						IsMaximizable = false
+						IsMaximizable = false,
+						Content = frame,
+
+						// Set min width/height
+						MinWidth = 850,
+						MinHeight = 550,
+
+						// Set backdrop
+						Backdrop = new WinUIEx.MicaSystemBackdrop(),
 					};
-					var appWindow = propertiesWindow.AppWindow;
+
+					var appWindow = newWindow.AppWindow;
 
 					// Set icon
 					appWindow.SetIcon(FilePropertiesHelpers.LogoPath);
 
-					// Set content
-					propertiesWindow.Content = frame;
 					if (frame.Content is SecurityAdvancedPage properties)
 					{
-						properties.window = propertiesWindow;
+						properties.window = newWindow;
 						properties.appWindow = appWindow;
 					}
 
-					// Set min size
-					propertiesWindow.MinWidth = 850;
-					propertiesWindow.MinHeight = 550;
-
-					// Set backdrop
-					propertiesWindow.Backdrop = new WinUIEx.MicaSystemBackdrop();
-
+					// Customize titlebar
 					appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
-
-					// Set window buttons background to transparent
 					appWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
 					appWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+					appWindow.Title = string.Format("SecurityAdvancedPermissionsTitle".GetLocalizedResource(), SecurityViewModel.Item.Name);
 
-					appWindow.Title = string.Format("SecurityAdvancedPermissionsTitle".GetLocalizedResource(), SecurityProperties.Item.Name);
 					appWindow.Resize(new SizeInt32(850, 550));
-					appWindow.Destroying += AppWindow_Destroying;
+					appWindow.Destroying += SecurityAdvancedPageWindow_Destroying;
 					appWindow.Show();
 
 					propsView = appWindow;
@@ -131,21 +110,27 @@ namespace Files.App.Views.Properties
 					propsView.Show(true);
 				}
 			}
-			else
+		}
+
+		public async override Task<bool> SaveChangesAsync()
+		{
+			return SecurityViewModel is null || SecurityViewModel.SaveChangedAccessControlList();
+		}
+
+		private async void SecurityAdvancedPageWindow_Destroying(AppWindow sender, object args)
+		{
+			sender.Destroying -= SecurityAdvancedPageWindow_Destroying;
+			propsView = null;
+
+			if (SecurityViewModel is not null)
 			{
-				// Unsupported
+				// Reload permissions when closing
+				await DispatcherQueue.EnqueueAsync(() => SecurityViewModel.GetAccessControlList());
 			}
 		}
 
-		private async void AppWindow_Destroying(AppWindow sender, object args)
+		public override void Dispose()
 		{
-			sender.Destroying -= AppWindow_Destroying;
-			propsView = null;
-
-			if (SecurityProperties is not null)
-			{
-				await DispatcherQueue.EnqueueAsync(() => SecurityProperties.GetFilePermissions()); // Reload permissions
-			}
 		}
 	}
 }
