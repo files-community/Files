5371f0e0e533e6789ad9ec9f504b8fd72a080d79
Poker <poker_sang@outlook.com>
2023-03-07 23:22:06 +0800

    use curly brackets for namespace scope

--- src/Files.App/Converters/GridLengthToDouble.cs
+++ src/Files.App/Converters/GridLengthToDouble.cs
@@ -2,19 +2,20 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Data;
 
-namespace Files.App.Converters;
-
-public class GridLengthToDouble : IValueConverter
+namespace Files.App.Converters
 {
-	public object Convert(object value, Type targetType, object parameter, string language)
+	public class GridLengthToDouble : IValueConverter
 	{
-		return value is GridLength { IsAbsolute: true } gridLength 
-			? gridLength.Value 
-			: throw new InvalidCastException("GridLength with a \"Star\"('*') or \"Auto\" value cannot be converted to double");
-	}
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			return value is GridLength { IsAbsolute: true } gridLength 
+				? gridLength.Value 
+				: throw new InvalidCastException("GridLength with a \"Star\"('*') or \"Auto\" value cannot be converted to double");
+		}
 
-	public object ConvertBack(object value, Type targetType, object parameter, string language)
-	{
-		return new GridLength((double)value);
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			return new GridLength((double)value);
+		}
 	}
 }
