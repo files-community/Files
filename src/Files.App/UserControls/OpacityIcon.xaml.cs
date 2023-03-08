8eb62952577e8f190d43d8db66bbc1118cebe20e
cinqmilleans <cinq.mille.ans@gmail.com>
2023-03-08 15:47:57 +0100

    Fix OpacityIcon state

--- src/Files.App/UserControls/OpacityIcon.xaml.cs
+++ src/Files.App/UserControls/OpacityIcon.xaml.cs
@@ -2,38 +2,30 @@ using CommunityToolkit.WinUI.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
-
 namespace Files.App.UserControls
 {
 	public sealed partial class OpacityIcon : Control
 	{
-		public OpacityIcon()
-		{
-			InitializeComponent();
-		}
+		public OpacityIcon() => InitializeComponent();
 
 		private void IsEnabledChange(DependencyObject sender, DependencyProperty dp)
 		{
-			var v = sender.GetValue(dp) as bool?;
-			if (v is not null && v == false)
-			{
-				VisualStateManager.GoToState(this, "Disabled", true);
-			}
-			else
-			{
-				VisualStateManager.GoToState(this, "Normal", true);
-			}
+			string state = sender.GetValue(dp) is false ? "Disabled" : "Normal";
+			VisualStateManager.GoToState(this, state, true);
 		}
 
-		private void OpacityIcon_Loading(FrameworkElement sender, object args)
+		private void OpacityIcon_Loading(FrameworkElement sender, object e)
 		{
 			// register a property change callback for the parent content presenter's foreground to allow reacting to button state changes, eg disabled
-			var p = this.FindAscendant<Control>();
-			if (p is not null)
-			{
-				p.RegisterPropertyChangedCallback(Control.IsEnabledProperty, IsEnabledChange);
-			}
+			var control = this.FindAscendant<Control>();
+			control?.RegisterPropertyChangedCallback(IsEnabledProperty, IsEnabledChange);
+		}
+
+		private void OpacityIcon_Loaded(object sender, RoutedEventArgs e)
+		{
+			var control = this.FindAscendant<Control>();
+			if (control is not null && !control.IsEnabled)
+				VisualStateManager.GoToState(this, "Disabled", true);
 		}
 	}
 }
