2eb94e9d08124aad2f58d2edccdccb68f17dc9e5
Filippo Ferrario <f.filippo49@gmail.com>
2023-03-10 17:39:37 +0100

    Interface docs

--- src/Files.App/Contexts/WidgetPage/IWidgetsPageContext.cs
+++ src/Files.App/Contexts/WidgetPage/IWidgetsPageContext.cs
@@ -7,12 +7,24 @@ namespace Files.App.Contexts
 {
 	public interface IWidgetsPageContext
 	{
+		/// <summary>
+		/// The last right clicked item
+		/// </summary>
 		WidgetCardItem? RightClickedItem { get; }
 
+		/// <summary>
+		/// The last opened widget's context menu instance
+		/// </summary>
 		CommandBarFlyout? ItemContextFlyoutMenu { get; }
 
+		/// <summary>
+		/// An Enumerable containing all the selected tagged items
+		/// </summary>
 		IEnumerable<FileTagsItemViewModel> SelectedTaggedItems { get; set; }
 
+		/// <summary>
+		/// Tells whether any item has been right clicked
+		/// </summary>
 		bool IsAnyItemRightClicked { get; }
 	}
 }
