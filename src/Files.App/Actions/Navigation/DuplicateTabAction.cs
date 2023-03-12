8756bcfc6bd6f024fdab5b7958f7ad9c91d7cd30
cinqmilleans <cinq.mille.ans@gmail.com>
2023-03-12 18:41:27 +0100

    duplicate tab

--- src/Files.App/Actions/Navigation/DuplicateTabAction.cs
+++ src/Files.App/Actions/Navigation/DuplicateTabAction.cs
@@ -1,12 +1,16 @@
-﻿using Files.App.Extensions;
+﻿using Files.App.Commands;
+using Files.App.Extensions;
 using Files.App.ViewModels;
 using System.Threading.Tasks;
+using Windows.System;
 
 namespace Files.App.Actions
 {
 	internal class DuplicateTabAction : IAction
 	{
 		public string Label { get; } = "DuplicateTab".GetLocalizedResource();
+
+		public HotKey HotKey { get; } = new(VirtualKey.K, VirtualKeyModifiers.Control | VirtualKeyModifiers.Shift);
 
 		public Task ExecuteAsync() => MainPageViewModel.DuplicateTabAsync();
 	}
