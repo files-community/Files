c2461658b8efc3019f61d7f759386401ac068e9f
Marco Franzen <mail@marco-franzen.de>
2023-03-21 20:50:18 +0100

    skip network share authentication for wsl distributions

--- src/Files.App/ViewModels/ItemViewModel.cs
+++ src/Files.App/ViewModels/ItemViewModel.cs
@@ -1474,7 +1474,10 @@ namespace Files.App.ViewModels
 		{
 			// Flag to use FindFirstFileExFromApp or StorageFolder enumeration - Use storage folder for Box Drive (#4629)
 			var isBoxFolder = App.CloudDrivesManager.Drives.FirstOrDefault(x => x.Text == "Box")?.Path?.TrimEnd('\\') is string boxFolder && path.StartsWith(boxFolder);
-			bool isNetwork = path.StartsWith(@"\\", StringComparison.Ordinal) && !path.StartsWith(@"\\?\", StringComparison.Ordinal);
+			bool isWslDistro = App.WSLDistroManager.TryGetDistro(path, out _);
+			bool isNetwork = path.StartsWith(@"\\", StringComparison.Ordinal) 
+				&& !path.StartsWith(@"\\?\", StringComparison.Ordinal)
+				&& !isWslDistro;
 			bool enumFromStorageFolder = isBoxFolder;
 
 			BaseStorageFolder? rootFolder = null;
