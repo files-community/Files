c826fc2f1712cfba71bb14c98cbc85f7996a939f
Filippo Ferrario <f.filippo49@gmail.com>
2023-03-22 23:24:12 +0100

    Archieve Name in banner & progress report

--- src/Files.App/Filesystem/Archive/ArchiveCreator.cs
+++ src/Files.App/Filesystem/Archive/ArchiveCreator.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace Files.App.Filesystem.Archive
@@ -15,7 +16,11 @@ namespace Files.App.Filesystem.Archive
 		private int processedItems = 0;
 
 		private string archivePath = string.Empty;
-		public string ArchivePath => archivePath;
+		public string ArchivePath
+		{
+			get => archivePath;
+			set => archivePath = value;
+		}
 
 		public string Directory { get; init; } = string.Empty;
 		public string FileName { get; init; } = string.Empty;
@@ -27,13 +32,24 @@ namespace Files.App.Filesystem.Archive
 		public ArchiveCompressionLevels CompressionLevel { get; init; } = ArchiveCompressionLevels.Normal;
 		public ArchiveSplittingSizes SplittingSize { get; init; } = ArchiveSplittingSizes.None;
 
-		public IProgress<FileSystemProgress> Progress { get; set; } = new Progress<FileSystemProgress>();
-		private readonly FileSystemProgress fsProgress;
+		private IProgress<FileSystemProgress> progress = new Progress<FileSystemProgress>();
+		public IProgress<FileSystemProgress> Progress
+		{
+			get => progress;
+			set
+			{
+				progress = value;
+				fsProgress = new(Progress, true, Shared.Enums.FileSystemStatusCode.InProgress);
+				fsProgress.Report(0);
+			}
+		}
+
+		private FileSystemProgress fsProgress;
 
 		public ArchiveCreator()
 		{
 			fsProgress = new(Progress, true, Shared.Enums.FileSystemStatusCode.InProgress);
-			fsProgress.Report();
+			fsProgress.Report(0);
 		}
 
 		private string ArchiveExtension => FileFormat switch
@@ -78,15 +94,14 @@ namespace Files.App.Filesystem.Archive
 			_ => throw new ArgumentOutOfRangeException(nameof(SplittingSize)),
 		};
 
+		public string GetArchivePath(string suffix = "")
+		{
+			return Path.Combine(Directory, $"{FileName}{suffix}{ArchiveExtension}");
+		}
+
 		public async Task<bool> RunCreationAsync()
 		{
-			var path = Path.Combine(Directory, FileName + ArchiveExtension);
 			string[] sources = Sources.ToArray();
-
-			int index = 1;
-			while (File.Exists(path) || System.IO.Directory.Exists(path))
-				path = Path.Combine(Directory, $"{FileName} ({++index}){ArchiveExtension}");
-			archivePath = path;
 
 			var compressor = new SevenZipCompressor
 			{
@@ -111,16 +126,16 @@ namespace Files.App.Filesystem.Archive
 
 				foreach (string directory in directories)
 				{
-					await compressor.CompressDirectoryAsync(directory, path, Password);
+					await compressor.CompressDirectoryAsync(directory, archivePath, Password);
 					compressor.CompressionMode = CompressionMode.Append;
 				}
 
 				if (files.Any())
 				{
 					if (string.IsNullOrEmpty(Password))
-						await compressor.CompressFilesAsync(path, files);
+						await compressor.CompressFilesAsync(archivePath, files);
 					else
-						await compressor.CompressFilesEncryptedAsync(path, Password, files);
+						await compressor.CompressFilesEncryptedAsync(archivePath, Password, files);
 				}
 
 				return true;
@@ -128,7 +143,7 @@ namespace Files.App.Filesystem.Archive
 			catch (Exception ex)
 			{
 				var logger = Ioc.Default.GetService<ILogger>();
-				logger?.Warn(ex, $"Error compressing folder: {path}");
+				logger?.Warn(ex, $"Error compressing folder: {archivePath}");
 
 				return false;
 			}
@@ -144,14 +159,14 @@ namespace Files.App.Filesystem.Archive
 			else
 			{
 				fsProgress.Percentage = processedItems * 100 / itemsAmount;
-				fsProgress.Report();
+				fsProgress.Report(fsProgress.Percentage);
 			}
 		}
 
 		private void Compressor_Compressing(object? _, ProgressEventArgs e)
 		{
 			fsProgress.Percentage += e.PercentDelta / itemsAmount;
-			fsProgress.Report();
+			fsProgress.Report(fsProgress.Percentage);
 		}
 	}
 }
