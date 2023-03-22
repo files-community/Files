a599c683fe076d561e0f4d82e87a00660cb68351
Yair <39923744+yaira2@users.noreply.github.com>
2023-03-22 17:28:35 -0400

    Update src/Files.App/Actions/Content/RefreshItemsAction.cs
    Co-authored-by: Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>

--- src/Files.App/Actions/Content/RefreshItemsAction.cs
+++ src/Files.App/Actions/Content/RefreshItemsAction.cs
@@ -3,7 +3,6 @@ using CommunityToolkit.Mvvm.DependencyInjection;
 using Files.App.Commands;
 using Files.App.Contexts;
 using Files.App.Extensions;
-using Files.App.Helpers;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using Windows.System;
