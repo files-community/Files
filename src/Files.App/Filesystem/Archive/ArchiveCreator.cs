889aa7b2aa5962d608fb14c924a8de0ed3c0757c
Filippo Ferrario <f.filippo49@gmail.com>
2023-03-22 23:30:56 +0100

    Usings

--- src/Files.App/Filesystem/Archive/ArchiveCreator.cs
+++ src/Files.App/Filesystem/Archive/ArchiveCreator.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 
 namespace Files.App.Filesystem.Archive
