710587bd95a3151008a87c59e31f1f7ad6059153
Luke Blevins <lukeblevins15@outlook.com>
2023-03-23 09:40:54 -0400

    docs: resources service

--- src/Files.App/ServicesImplementation/ResourcesService.cs
+++ src/Files.App/ServicesImplementation/ResourcesService.cs
@@ -7,19 +7,11 @@ namespace Files.App.ServicesImplementation;
 
 public class ResourcesService : IResourcesService
 {
-	/// <summary>
-	/// Overrides the xaml resource for App.Theme.BackgroundBrush
-	/// </summary>
-	/// <param name="appThemeBackgroundColor"></param>
 	public void SetAppThemeBackgroundColor(Color appThemeBackgroundColor)
 	{
 		Application.Current.Resources["App.Theme.BackgroundBrush"] = appThemeBackgroundColor.ToWindowsColor();
 	}
 
-	/// <summary>
-	/// Overrides the xaml resource for App.Theme.AddressBar.BackgroundBrush
-	/// </summary>
-	/// <param name="appThemeAddressBarBackgroundColor"></param>
 	public void SetAppThemeAddressBarBackgroundColor(Color appThemeAddressBarBackgroundColor)
 	{
 		Application.Current.Resources["App.Theme.AddressBar.BackgroundBrush"] = appThemeAddressBarBackgroundColor.ToWindowsColor();
@@ -28,37 +20,22 @@ public class ResourcesService : IResourcesService
 		Application.Current.Resources["TabViewItemHeaderBackgroundSelected"] = appThemeAddressBarBackgroundColor.ToWindowsColor();
 	}
 
-	/// <summary>
-	/// Overrides the xaml resource for App.Theme.Sidebar.BackgroundBrush
-	/// </summary>
-	/// <param name="appThemeSidebarBackgroundColor"></param>
+	
 	public void SetAppThemeSidebarBackgroundColor(Color appThemeSidebarBackgroundColor)
 	{
 		Application.Current.Resources["App.Theme.Sidebar.BackgroundBrush"] = appThemeSidebarBackgroundColor.ToWindowsColor();
 	}
 
-	/// <summary>
-	/// Overrides the xaml resource for App.Theme.FileArea.BackgroundBrush
-	/// </summary>
-	/// <param name="appThemeFileAreaBackgroundColor"></param>
 	public void SetAppThemeFileAreaBackgroundColor(Color appThemeFileAreaBackgroundColor)
 	{
 		Application.Current.Resources["App.Theme.FileArea.BackgroundBrush"] = appThemeFileAreaBackgroundColor.ToWindowsColor();
 	}
 
-	/// <summary>
-	/// Overrides the xaml resource for ContentControlThemeFontFamily
-	/// </summary>
-	/// <param name="contentControlThemeFontFamily"></param>
 	public void SetAppThemeFontFamily(string contentControlThemeFontFamily)
 	{
 		Application.Current.Resources["ContentControlThemeFontFamily"] = contentControlThemeFontFamily;
 	}
 
-	/// <summary>
-	/// Overrides the xaml resource for the list view item height
-	/// </summary>
-	/// <param name="useCompactSpacing"></param>
 	public void SetCompactSpacing(bool useCompactSpacing)
 	{
 		var listItemHeight = useCompactSpacing ? 24 : 36;
@@ -68,9 +45,6 @@ public class ResourcesService : IResourcesService
 		Application.Current.Resources["NavigationViewItemOnLeftMinHeight"] = navigationViewItemOnLeftMinHeight;
 	}
 
-	/// <summary>
-	/// Applies updated resource styles
-	/// </summary>
 	public void ApplyResources()
 	{
 		// Get the index of the current theme
--- src/Files.Shared/Services/IResourcesService.cs
+++ src/Files.Shared/Services/IResourcesService.cs
@@ -2,13 +2,49 @@
 
 namespace Files.Shared.Services;
 
+/// <summary>
+/// Contains methods related to modifying various app theme resources
+/// </summary>
 public interface IResourcesService
 {
+	/// <summary>
+	/// Applies updated resource styles
+	/// </summary>
 	void ApplyResources();
+
+	/// <summary>
+	/// Overrides the XAML resource for App.Theme.BackgroundBrush
+	/// </summary>
+	/// <param name="appThemeBackgroundColor"></param>
 	void SetAppThemeBackgroundColor(Color appThemeBackgroundColor);
+
+	/// <summary>
+	/// Overrides the XAML resource for App.Theme.AddressBar.BackgroundBrush
+	/// </summary>
+	/// <param name="appThemeAddressBarBackgroundColor"></param>
 	void SetAppThemeAddressBarBackgroundColor(Color appThemeAddressBarBackgroundColor);
+
+	/// <summary>
+	/// Overrides the XAML resource for App.Theme.Sidebar.BackgroundBrush
+	/// </summary>
+	/// <param name="appThemeSidebarBackgroundColor"></param>
 	void SetAppThemeSidebarBackgroundColor(Color appThemeSidebarBackgroundColor);
+
+	/// <summary>
+	/// Overrides the XAML resource for App.Theme.FileArea.BackgroundBrush
+	/// </summary>
+	/// <param name="appThemeFileAreaBackgroundColor"></param>
 	void SetAppThemeFileAreaBackgroundColor(Color appThemeFileAreaBackgroundColor);
+
+	/// <summary>
+	/// Overrides the XAML resource for ContentControlThemeFontFamily
+	/// </summary>
+	/// <param name="contentControlThemeFontFamily"></param>
 	void SetAppThemeFontFamily(string contentControlThemeFontFamily);
+
+	/// <summary>
+	/// Overrides the XAML resource for the list view item height
+	/// </summary>
+	/// <param name="useCompactSpacing"></param>
 	void SetCompactSpacing(bool useCompactSpacing);
 }
