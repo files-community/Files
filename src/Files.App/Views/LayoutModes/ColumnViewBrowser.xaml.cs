077a96007b61d15c783c6c07383712e74f9999ed
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2023-03-30 11:07:14 +0900

    Clear highlight in last blade

--- src/Files.App/Views/LayoutModes/ColumnViewBrowser.xaml.cs
+++ src/Files.App/Views/LayoutModes/ColumnViewBrowser.xaml.cs
@@ -429,7 +429,10 @@ namespace Files.App.Views.LayoutModes
 			var destComponents = StorageFileExtensions.GetDirectoryPathComponents(column.NavPathParam);
 			var (_, relativeIndex) = GetLastCommonAndRelativeIndex(destComponents, columnPath, columnFirstPath);
 			if (relativeIndex >= 0)
+			{
+				ColumnHost.ActiveBlades[relativeIndex].FindDescendant<ColumnViewBase>()?.ClearOpenedFolderSelectionIndicator();
 				DismissOtherBlades(relativeIndex);
+			}
 		}
 
 		private (int, int) GetLastCommonAndRelativeIndex(List<PathBoxItem> destComponents, string columnPath, string columnFirstPath)
