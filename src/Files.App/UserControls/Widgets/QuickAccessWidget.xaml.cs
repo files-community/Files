8ba27621d7c0c08cd1c16c5691db24c549303012
Pablo García Ruiz <55754091+hecksmosis@users.noreply.github.com>
2023-04-06 13:59:12 +0200

    Update src/Files.App/UserControls/Widgets/QuickAccessWidget.xaml.cs
    Co-authored-by: Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>

--- src/Files.App/UserControls/Widgets/QuickAccessWidget.xaml.cs
+++ src/Files.App/UserControls/Widgets/QuickAccessWidget.xaml.cs
@@ -18,7 +18,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
