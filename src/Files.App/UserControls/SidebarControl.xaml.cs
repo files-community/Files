2ea246117585238bc67c49893d9e46f0c49aa0ae
Pablo García Ruiz <pgruiz727@gmail.com>
2023-04-06 14:23:23 +0200

    Fix bugs

--- src/Files.App/UserControls/SidebarControl.xaml.cs
+++ src/Files.App/UserControls/SidebarControl.xaml.cs
@@ -190,7 +190,7 @@ namespace Files.App.UserControls
 
 			var isDriveItem = item is DriveItem;
 			var isDriveItemPinned = isDriveItem && ((DriveItem)item).IsPinned;
-			var isShellFolder = item is LocationItem shellItem && ShellStorageFolder.IsShellPath(shellItem.Path);
+			var isShellFolder = false ;
 
 			return new List<ContextMenuFlyoutItemViewModel>()
 			{
