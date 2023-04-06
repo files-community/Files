45e7da4cf01fb4c0e2074d53dbbfaab1c246b933
Pablo García Ruiz <55754091+hecksmosis@users.noreply.github.com>
2023-04-06 14:00:05 +0200

    Update src/Files.App/UserControls/Widgets/QuickAccessWidget.xaml.cs
    Co-authored-by: Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>

--- src/Files.App/UserControls/Widgets/QuickAccessWidget.xaml.cs
+++ src/Files.App/UserControls/Widgets/QuickAccessWidget.xaml.cs
@@ -8,7 +8,6 @@ using Files.App.ViewModels;
 using Files.App.ViewModels.Widgets;
 using Files.Backend.Services.Settings;
 using Files.Shared;
-using Files.Shared.Extensions;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
