e2b7a311a809478c1938e60637e24d14028980f9
Luke Blevins <lukeblevins15@outlook.com>
2023-04-10 03:52:57 -0400

    chore: add license header

--- src/Files.App/ServicesImplementation/NetworkDrivesService.cs
+++ src/Files.App/ServicesImplementation/NetworkDrivesService.cs
@@ -1,4 +1,7 @@
-﻿using Files.App.DataModels.NavigationControlItems;
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Files.App.DataModels.NavigationControlItems;
 using Files.App.Shell;
 using Files.Backend.Services;
 using Files.Sdk.Storage.LocatableStorage;
