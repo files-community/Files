fc1c63fc5c39646d748f8cf688c09b0708c07df8
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-04-20 00:30:50 +0900

    Enabled

---
+++ src/Files.App/GlobalUsings.cs
@@ -0,0 +1,22 @@
+﻿// System
+global using global::System;
+global using global::System.Collections;
+global using global::System.Collections.Generic;
+global using global::System.Collections.ObjectModel;
+global using global::System.Linq;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+// Windows Community Toolkit
+global using global::CommunityToolkit.Mvvm.ComponentModel;
+global using global::CommunityToolkit.Mvvm.Input;
+global using global::CommunityToolkit.Mvvm.Messaging;
+
+// Files Front-end
+global using global::Files.App.Helpers;
+global using global::Files.App.Extensions;
+global using global::Files.App.Filesystem;
+
+// Files Back-end
+
+// Vanara
