3cf5b86daba91a1fa0b3ee9026056172dbcc2cc6
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-04-19 23:10:39 +0900

    Feature: Enabled and Added GlobalUsings(ImplicitUsings)

---
+++ src/Files.App/GlobalUsings.cs
@@ -0,0 +1,27 @@
+﻿// System
+global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.Collections.ObjectModel;
+global using global::System.ComponentModel;
+global using global::System.Linq;
+global using global::System.Text;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+// Windows Community Toolkit
+global using global::CommunityToolkit.Mvvm.ComponentModel;
+global using global::CommunityToolkit.Mvvm.DependencyInjection;
+global using global::CommunityToolkit.Mvvm.Input;
+global using global::CommunityToolkit.Mvvm.Messaging;
+
+// Files front-end
+global using global::Files.App.Helpers;
+global using global::Files.App.Extensions;
+global using global::Files.App.Filesystem;
+
+// Files back-end
+global using global::Files.Shared;
+global using global::Files.App.Storage;
+global using global::Files.Sdk.Storage;
+
+// Vanara
