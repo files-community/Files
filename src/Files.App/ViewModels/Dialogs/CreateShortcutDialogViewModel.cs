c9afcd8baf2f7e52eb51a57628f382f92e7498bc
Filippo Ferrario <f.filippo49@gmail.com>
2023-04-21 23:33:49 +0200

    Remove warning for empty shortcut

--- src/Files.App/ViewModels/Dialogs/CreateShortcutDialogViewModel.cs
+++ src/Files.App/ViewModels/Dialogs/CreateShortcutDialogViewModel.cs
@@ -29,6 +29,7 @@ namespace Files.App.ViewModels.Dialogs
 				if (!SetProperty(ref _destinationItemPath, value))
 					return;
 
+				OnPropertyChanged(nameof(ShowWarningTip));
 				if (string.IsNullOrWhiteSpace(DestinationItemPath))
 				{
 					IsLocationValid = false;
@@ -60,8 +61,10 @@ namespace Files.App.ViewModels.Dialogs
 		public bool IsLocationValid
 		{
 			get => _isLocationValid;
-			set => SetProperty(ref _isLocationValid, value);
+			set => SetProperty(ref _isLocationValid, value, nameof(ShowWarningTip));
 		}
+
+		public bool ShowWarningTip => !string.IsNullOrEmpty(DestinationItemPath) && !_isLocationValid;
 
 		// Command invoked when the user clicks the 'Browse' button
 		public ICommand SelectDestinationCommand { get; private set; }
@@ -105,10 +108,10 @@ namespace Files.App.ViewModels.Dialogs
 				if (string.IsNullOrEmpty(destinationName))
 				{
 					var destinationPath = DestinationItemPath.Replace('/', '\\');
-					
+
 					if (destinationPath.EndsWith('\\'))
 						destinationPath = destinationPath.Substring(0, destinationPath.Length - 1);
-					
+
 					destinationName = destinationPath.Substring(destinationPath.LastIndexOf('\\') + 1);
 				}
 			}
