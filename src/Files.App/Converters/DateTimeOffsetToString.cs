b347cf7d4dede1181b8021088a1bc5f8f2d94562
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-04-22 00:20:58 +0900

    Added

--- src/Files.App/Converters/DateTimeOffsetToString.cs
+++ src/Files.App/Converters/DateTimeOffsetToString.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
 using CommunityToolkit.Mvvm.DependencyInjection;
 using Files.Shared.Services.DateTimeFormatter;
 using Microsoft.UI.Xaml.Data;
--- src/Files.App/Converters/DoubleToString.cs
+++ src/Files.App/Converters/DoubleToString.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
 using Microsoft.UI.Xaml.Data;
 using System;
 
