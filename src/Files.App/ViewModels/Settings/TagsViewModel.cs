11efd8e56fdc8adfd5ba551ad97aa52d6fd8c19b
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-04-22 18:49:46 +0900

    Added

--- src/Files.App/ViewModels/Settings/TagsViewModel.cs
+++ src/Files.App/ViewModels/Settings/TagsViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Files.Backend.ViewModels.FileTags;
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Files.Backend.ViewModels.FileTags;
 using System.Windows.Input;
 
 namespace Files.App.ViewModels.Settings
