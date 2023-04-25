a7f97f908c6d80b2b758cd85851107f9eccac5bc
Luke Blevins <lukeblevins15@outlook.com>
2023-04-25 16:12:06 -0400

    Codebase Quality: Network drives service and view model (#12176)
    Co-authored-by: Yair <39923744+yaira2@users.noreply.github.com>

---
+++ src/Files.Backend/Services/INetworkDrivesService.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Files.Sdk.Storage.LocatableStorage;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Files.Backend.Services
+{
+	public interface INetworkDrivesService
+	{
+		/// <summary>
+		/// Enumerates network storage devices
+		/// </summary>
+		/// <returns>A collection of network storage devices</returns>
+		IAsyncEnumerable<ILocatableFolder> GetDrivesAsync();
+
+		/// <summary>
+		/// Displays the operating system dialog for connecting to a network storage device
+		/// </summary>
+		/// <returns></returns>
+		Task OpenMapNetworkDriveDialogAsync();
+
+		/// <summary>
+		/// Disconnects an existing network storage device
+		/// </summary>
+		/// <param name="drive">An item representing the network storage device to disconnect from</param>
+		/// <returns>True or false to indicate status</returns>
+		bool DisconnectNetworkDrive(ILocatableFolder drive);
+	}
+}
