9c4cb9ede7598f4e4a5d3b4ad8e4e1472be69c82
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-05-02 01:57:46 +0900

    Code Quality: Update UWPToWinAppSDKUpgradeHelpers (#12238)

--- src/Files.App/Views/MainPage.xaml.cs
+++ src/Files.App/Views/MainPage.xaml.cs
@@ -20,7 +20,6 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.Windows.ApplicationModel.Resources;
 using System.Runtime.CompilerServices;
-using UWPToWinAppSDKUpgradeHelpers;
 using Windows.ApplicationModel;
 using Windows.Services.Store;
 using Windows.Storage;
