daf6e2e3b3136eb9e2291768b61627fd1c6fba15
Filippo Ferrario <f.filippo49@gmail.com>
2023-05-05 22:36:33 +0200

    Fix: Issue with file preview in column layout

--- src/Files.App/Helpers/DriveHelpers.cs
+++ src/Files.App/Helpers/DriveHelpers.cs
@@ -136,9 +136,13 @@ namespace Files.App.Helpers
 			};
 		}
 
-		public static async Task<StorageItemThumbnail> GetThumbnailAsync(StorageFolder folder)
-			=> (StorageItemThumbnail)await FilesystemTasks.Wrap(()
-				=> folder.GetThumbnailAsync(ThumbnailMode.SingleItem, 40, ThumbnailOptions.UseCurrentScale).AsTask()
+		public static async Task<StorageItemThumbnail?> GetThumbnailAsync(StorageFolder folder)
+		{
+			return folder is null
+				? null
+				: (StorageItemThumbnail)await FilesystemTasks.Wrap(()
+					=> folder.GetThumbnailAsync(ThumbnailMode.SingleItem, 40, ThumbnailOptions.UseCurrentScale).AsTask()
 			);
+		}
 	}
 }
