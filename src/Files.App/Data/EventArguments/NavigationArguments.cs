52ab789cbd3707224d3d7598fea207c2e2eac865
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-05-06 17:49:13 +0900

    Add license headers

--- src/Files.App/Data/EventArguments/NavigationArguments.cs
+++ src/Files.App/Data/EventArguments/NavigationArguments.cs
@@ -1,4 +1,7 @@
-﻿namespace Files.App.Data.EventArguments
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Data.EventArguments
 {
 	public class NavigationArguments
 	{
--- src/Files.App/Data/Items/PathBoxItem.cs
+++ src/Files.App/Data/Items/PathBoxItem.cs
@@ -1,4 +1,7 @@
-﻿namespace Files.App.Data.Items
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Data.Items
 {
 	public class PathBoxItem
 	{
