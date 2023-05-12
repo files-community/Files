305b1b985b67e13532c25ec66660e5e98b4e40f4
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2023-05-12 15:57:49 +0900

    Added missing using

--- src/Files.App/Helpers/BitmapHelper.cs
+++ src/Files.App/Helpers/BitmapHelper.cs
@@ -1,7 +1,9 @@
 using Files.App.Filesystem;
 using Files.App.Filesystem.StorageItems;
 using Microsoft.UI.Xaml.Media.Imaging;
+using System;
 using System.IO;
+using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
