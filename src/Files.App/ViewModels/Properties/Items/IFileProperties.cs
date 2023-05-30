ad3a3262815d0f3e732fed4e0fb5cb46ed375cfd
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2023-05-30 11:41:20 +0900

    Added comments

--- src/Files.App/ViewModels/Properties/Items/IFileProperties.cs
+++ src/Files.App/ViewModels/Properties/Items/IFileProperties.cs
@@ -5,10 +5,22 @@ namespace Files.App.ViewModels.Properties
 {
 	internal interface IFileProperties
 	{
+		/// <summary>
+		/// Loads metadata stored in files.
+		/// </summary>
+		/// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
 		Task GetSystemFilePropertiesAsync();
 
+		/// <summary>
+		/// Saves edited metadata to files.
+		/// </summary>
+		/// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
 		Task SyncPropertyChangesAsync();
 
+		/// <summary>
+		/// Clears metadata stored in files.
+		/// </summary>
+		/// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
 		Task ClearPropertiesAsync();
 	}
 }
