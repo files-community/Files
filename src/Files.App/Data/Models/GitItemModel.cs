a7e46799de3151e72ca7640e82029c75809ce920
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-06-14 08:53:09 +0900

    Feature: Added Git columns to the details layout (#12485)

---
+++ src/Files.App/Data/Models/GitItemModel.cs
@@ -0,0 +1,36 @@
+﻿using LibGit2Sharp;
+
+namespace Files.App.Data.Models
+{
+	/// <summary>
+	/// Represents a model for Git items
+	/// </summary>
+	internal class GitItemModel
+	{
+		/// <summary>
+		/// Gets or initializes file change kind
+		/// </summary>
+		/// <remarks>
+		/// This is often showed as A(Add), D(Delete), M(Modified), U(Untracked) in VS Code.
+		/// </remarks>
+		public ChangeKind Status { get; init; }
+
+		/// <summary>
+		/// Gets or initializes file change kind humanized string
+		/// </summary>
+		/// <remarks>
+		/// This is often showed as A(Add), D(Delete), M(Modified), U(Untracked) in VS Code.
+		/// </remarks>
+		public string? StatusHumanized { get; init; }
+
+		/// <summary>
+		/// Gets or initializes file last commit information including author, committed date, and SHA.
+		/// </summary>
+		public Commit? LastCommit { get; init; }
+
+		/// <summary>
+		/// Gets or initializes file path
+		/// </summary>
+		public string? Path { get; init; }
+	}
+}
