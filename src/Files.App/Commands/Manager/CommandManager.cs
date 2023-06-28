fd002b575afd49ab3b2b2a0f8c8ba6597b459848
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2023-06-28 12:13:43 +0900

    Feature: Support changing behavior of buttons with modifier keys (#12728)

--- src/Files.App/Commands/Manager/CommandManager.cs
+++ src/Files.App/Commands/Manager/CommandManager.cs
@@ -362,48 +362,8 @@ namespace Files.App.Commands
 				UpdateHotKeys();
 		}
 
-		[DebuggerDisplay("Command None")]
-		private class NoneCommand : IRichCommand
-		{
-			public event EventHandler? CanExecuteChanged { add {} remove {} }
-			public event PropertyChangingEventHandler? PropertyChanging { add {} remove {} }
-			public event PropertyChangedEventHandler? PropertyChanged { add {} remove {} }
-
-			public CommandCodes Code => CommandCodes.None;
-
-			public string Label => string.Empty;
-			public string LabelWithHotKey => string.Empty;
-			public string AutomationName => string.Empty;
-
-			public string Description => string.Empty;
-
-			public RichGlyph Glyph => RichGlyph.None;
-			public object? Icon => null;
-			public FontIcon? FontIcon => null;
-			public Style? OpacityStyle => null;
-
-			public bool IsCustomHotKeys => false;
-			public string? HotKeyText => null;
-			public HotKeyCollection HotKeys
-			{
-				get => HotKeyCollection.Empty;
-				set => throw new InvalidOperationException("This command is readonly.");
-			}
-
-			public bool IsToggle => false;
-			public bool IsOn { get => false; set {} }
-			public bool IsExecutable => false;
-
-			public bool CanExecute(object? parameter) => false;
-			public void Execute(object? parameter) {}
-			public Task ExecuteAsync() => Task.CompletedTask;
-			public void ExecuteTapped(object sender, TappedRoutedEventArgs e) {}
-
-			public void ResetHotKeys() {}
-		}
-
 		[DebuggerDisplay("Command {Code}")]
-		private class ActionCommand : ObservableObject, IRichCommand
+		internal class ActionCommand : ObservableObject, IRichCommand
 		{
 			public event EventHandler? CanExecuteChanged;
 
