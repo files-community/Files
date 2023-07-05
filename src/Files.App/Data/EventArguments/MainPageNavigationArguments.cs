6174023f88b955d8aa7e125c5a8c2540e3118346
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2023-07-05 11:48:37 +0900

    Fixed OpenInNewWindow

---
+++ src/Files.App/Data/EventArguments/MainPageNavigationArguments.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Data.EventArguments
+{
+	internal class MainPageNavigationArguments
+	{
+		public object? Parameter { get; set; }
+
+		public bool IgnoreStartupSettings { get; set; }
+	}
+}
