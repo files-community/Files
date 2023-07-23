f826f3b9fe4f18c6b0aef47431ed8ee19f928c36
Marco Gavelli <marcogavelli2@gmail.com>
2023-07-23 16:16:15 +0200

    Remove extra code from /FileOperationsHelpers

--- src/Files.App.Elevated/FileOperationsHelpers.cs
+++ src/Files.App.Elevated/FileOperationsHelpers.cs
@@ -4,7 +4,6 @@
 using Files.Core.Data.Items;
 using Files.Shared.Extensions;
 using System.ComponentModel;
-using System.Diagnostics;
 using Vanara.PInvoke;
 using Vanara.Windows.Shell;
 
@@ -13,9 +12,6 @@ namespace Files.App.Elevated
 	/// <WARN>Code in this class is a close duplicate of Files.App.Utils.Storage</WARN>
 	public class FileOperationsHelpers
 	{
-		private static readonly Ole32.PROPERTYKEY PKEY_FilePlaceholderStatus = new Ole32.PROPERTYKEY(new Guid("B2F9B9D6-FEC4-4DD5-94D7-8957488C807B"), 2);
-		private const uint PS_CLOUDFILE_PLACEHOLDER = 8;
-
 		public static async Task<(bool, ShellOperationResult)> CreateItemAsync(string filePath, string fileOp, string template = "", byte[]? dataBytes = null)
 		{
 			//return Win32API.StartSTATask(async () =>
@@ -76,94 +72,6 @@ namespace Files.App.Elevated
 				}
 
 				return (await createTcs.Task, shellOperationResult);
-			}//);
-		}
-
-		public static async Task<(bool, ShellOperationResult)> TestRecycleAsync(string[] fileToDeletePath)
-		{
-			//return Win32API.StartSTATask(async () =>
-			{
-				using var op = new ShellFileOperations();
-
-				op.Options = ShellFileOperations.OperationFlags.Silent
-							| ShellFileOperations.OperationFlags.NoConfirmation
-							| ShellFileOperations.OperationFlags.NoErrorUI;
-				op.Options |= ShellFileOperations.OperationFlags.RecycleOnDelete;
-
-				var shellOperationResult = new ShellOperationResult();
-				var tryDelete = false;
-
-				for (var i = 0; i < fileToDeletePath.Length; i++)
-				{
-					if (!SafetyExtensions.IgnoreExceptions(() =>
-					{
-						using var shi = new ShellItem(fileToDeletePath[i]);
-						using var file = SafetyExtensions.IgnoreExceptions(() => GetFirstFile(shi)) ?? shi;
-						if (file.Properties.GetProperty<uint>(PKEY_FilePlaceholderStatus) == PS_CLOUDFILE_PLACEHOLDER)
-						{
-							// Online only files cannot be tried for deletion, so they are treated as to be permanently deleted.
-							shellOperationResult.Items.Add(new ShellOperationItemResult()
-							{
-								Succeeded = false,
-								Source = fileToDeletePath[i],
-								HResult = HRESULT.COPYENGINE_E_RECYCLE_BIN_NOT_FOUND
-							});
-						}
-						else
-						{
-							op.QueueDeleteOperation(file);
-							tryDelete = true;
-						}
-					}))
-					{
-						shellOperationResult.Items.Add(new ShellOperationItemResult()
-						{
-							Succeeded = false,
-							Source = fileToDeletePath[i],
-							HResult = -1
-						});
-					}
-				}
-
-				if (!tryDelete)
-					return (true, shellOperationResult);
-
-				var deleteTcs = new TaskCompletionSource<bool>();
-				op.PreDeleteItem += [DebuggerHidden] (s, e) =>
-				{
-					if (!e.Flags.HasFlag(ShellFileOperations.TransferFlags.DeleteRecycleIfPossible))
-					{
-						shellOperationResult.Items.Add(new ShellOperationItemResult()
-						{
-							Succeeded = false,
-							Source = GetParsingPath(e.SourceItem),
-							HResult = HRESULT.COPYENGINE_E_RECYCLE_BIN_NOT_FOUND
-						});
-						throw new Win32Exception(HRESULT.COPYENGINE_E_RECYCLE_BIN_NOT_FOUND); // E_FAIL, stops operation
-					}
-					else
-					{
-						shellOperationResult.Items.Add(new ShellOperationItemResult()
-						{
-							Succeeded = true,
-							Source = GetParsingPath(e.SourceItem),
-							HResult = HRESULT.COPYENGINE_E_USER_CANCELLED
-						});
-						throw new Win32Exception(HRESULT.COPYENGINE_E_USER_CANCELLED); // E_FAIL, stops operation
-					}
-				};
-				op.FinishOperations += (s, e) => deleteTcs.TrySetResult(e.Result.Succeeded);
-
-				try
-				{
-					op.PerformOperations();
-				}
-				catch
-				{
-					deleteTcs.TrySetResult(false);
-				}
-
-				return (await deleteTcs.Task, shellOperationResult);
 			}//);
 		}
 
@@ -419,28 +327,6 @@ namespace Files.App.Elevated
 
 				return (await copyTcs.Task, shellOperationResult);
 			}//);
-		}
-
-		private static ShellItem? GetFirstFile(ShellItem shi)
-		{
-			if (!shi.IsFolder || shi.Attributes.HasFlag(ShellItemAttribute.Stream))
-			{
-				return shi;
-			}
-			using var shf = new ShellFolder(shi);
-			if (shf.FirstOrDefault(x => !x.IsFolder || x.Attributes.HasFlag(ShellItemAttribute.Stream)) is ShellItem item)
-			{
-				return item;
-			}
-			foreach (var shsfi in shf.Where(x => x.IsFolder && !x.Attributes.HasFlag(ShellItemAttribute.Stream)))
-			{
-				using var shsf = new ShellFolder(shsfi);
-				if (GetFirstFile(shsf) is ShellItem item2)
-				{
-					return item2;
-				}
-			}
-			return null;
 		}
 
 		private static string GetParsingPath(ShellItem item)
