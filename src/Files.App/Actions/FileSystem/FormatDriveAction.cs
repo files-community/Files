7679e78caee62a9bb19adb73f48fa42c16b297eb
Marco Franzen <mail@marco-franzen.de>
2023-08-06 21:20:14 +0200

    Fix: Fixed an issue were installing a font would freeze the app (#13125)

--- src/Files.App/Actions/FileSystem/FormatDriveAction.cs
+++ src/Files.App/Actions/FileSystem/FormatDriveAction.cs
@@ -32,9 +32,7 @@ namespace Files.App.Actions
 
 		public Task ExecuteAsync()
 		{
-			Win32API.OpenFormatDriveDialog(context.Folder?.ItemPath ?? string.Empty);
-
-			return Task.CompletedTask;
+			return Win32API.OpenFormatDriveDialog(context.Folder?.ItemPath ?? string.Empty);
 		}
 
 		public void Context_PropertyChanged(object? sender, PropertyChangedEventArgs e)
