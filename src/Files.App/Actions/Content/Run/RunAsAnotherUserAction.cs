f59290adb3f9370d62d1dbd405a201a9ecf08b59
Filippo Ferrario <f.filippo49@gmail.com>
2023-08-08 11:24:27 +0200

    RunAs base class

--- src/Files.App/Actions/Content/Run/RunAsAnotherUserAction.cs
+++ src/Files.App/Actions/Content/Run/RunAsAnotherUserAction.cs
@@ -3,46 +3,19 @@
 
 namespace Files.App.Actions
 {
-	internal class RunAsAnotherUserAction : ObservableObject, IAction
+	internal class RunAsAnotherUserAction : BaseRunAsAction
 	{
-		public IContentPageContext context;
-
-		public bool IsExecutable =>
-			context.SelectedItem is not null &&
-			(FileExtensionHelpers.IsExecutableFile(context.SelectedItem.FileExtension) ||
-			(context.SelectedItem is ShortcutItem shortcut &&
-			shortcut.IsExecutable));
-
-		public string Label
+		public override string Label
 			=> "BaseLayoutContextFlyoutRunAsAnotherUser/Text".GetLocalizedResource();
 
-		public string Description
+		public override string Description
 			=> "RunAsAnotherUserDescription".GetLocalizedResource();
 
-		public RichGlyph Glyph
+		public override RichGlyph Glyph
 			=> new("\uE7EE");
 
-		public RunAsAnotherUserAction()
+		public RunAsAnotherUserAction() : base("runasuser")
 		{
-			context = Ioc.Default.GetRequiredService<IContentPageContext>();
-
-			context.PropertyChanged += Context_PropertyChanged;
-		}
-
-		public async Task ExecuteAsync()
-		{
-			await ContextMenu.InvokeVerb("runasuser", context.SelectedItem!.ItemPath);
-		}
-
-		public void Context_PropertyChanged(object? sender, PropertyChangedEventArgs e)
-		{
-			switch (e.PropertyName)
-			{
-				case nameof(IContentPageContext.SelectedItems):
-				case nameof(IContentPageContext.Folder):
-					OnPropertyChanged(nameof(IsExecutable));
-					break;
-			}
 		}
 	}
 }
