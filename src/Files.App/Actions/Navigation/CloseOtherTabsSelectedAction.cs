d0d78b04dd30280bef5b5a29a4b88f710fb19679
Filippo Ferrario <f.filippo49@gmail.com>
2023-08-08 13:11:02 +0200

    Close tab base class

--- src/Files.App/Actions/Navigation/CloseOtherTabsSelectedAction.cs
+++ src/Files.App/Actions/Navigation/CloseOtherTabsSelectedAction.cs
@@ -3,48 +3,23 @@
 
 namespace Files.App.Actions
 {
-	internal class CloseOtherTabsSelectedAction : ObservableObject, IAction
+	internal sealed class CloseOtherTabsSelectedAction : CloseTabBaseAction
 	{
-		private readonly IMultitaskingContext context;
-
-		public string Label
+		public override string Label
 			=> "CloseOtherTabs".GetLocalizedResource();
 
-		public string Description
+		public override string Description
 			=> "CloseOtherTabsSelectedDescription".GetLocalizedResource();
-
-		public bool IsExecutable
-			=> GetIsExecutable();
 
 		public CloseOtherTabsSelectedAction()
 		{
-			context = Ioc.Default.GetRequiredService<IMultitaskingContext>();
-
-			context.PropertyChanged += Context_PropertyChanged;
 		}
 
-		public Task ExecuteAsync()
+		public override Task ExecuteAsync()
 		{
-			if (context.Control is not null)
-				MultitaskingTabsHelpers.CloseOtherTabs(context.SelectedTabItem, context.Control);
+			MultitaskingTabsHelpers.CloseOtherTabs(context.SelectedTabItem, context.Control!);
 
 			return Task.CompletedTask;
-		}
-
-		private bool GetIsExecutable()
-		{
-			return context.Control is not null && context.TabCount > 1;
-		}
-
-		private void Context_PropertyChanged(object? sender, PropertyChangedEventArgs e)
-		{
-			switch (e.PropertyName)
-			{
-				case nameof(IMultitaskingContext.Control):
-				case nameof(IMultitaskingContext.TabCount):
-					OnPropertyChanged(nameof(IsExecutable));
-					break;
-			}
 		}
 	}
 }
---
+++ src/Files.App/Actions/Navigation/CloseTabBaseAction.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Actions
+{
+	internal abstract class CloseTabBaseAction : ObservableObject, IAction
+	{
+		protected readonly IMultitaskingContext context;
+
+		public abstract string Label { get; }
+
+		public abstract string Description { get; }
+			
+		public bool IsExecutable
+			=> GetIsExecutable();
+
+		public virtual HotKey HotKey
+			=> HotKey.None;
+		
+		public virtual HotKey SecondHotKey
+			=> HotKey.None;
+
+		public virtual RichGlyph Glyph
+			=> RichGlyph.None;
+
+		public CloseTabBaseAction()
+		{
+			context = Ioc.Default.GetRequiredService<IMultitaskingContext>();
+
+			context.PropertyChanged += Context_PropertyChanged;
+		}
+
+		public abstract Task ExecuteAsync();
+
+		protected virtual bool GetIsExecutable()
+		{
+			return context.Control is not null && context.TabCount > 1;
+		}
+
+		protected virtual void Context_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			switch (e.PropertyName)
+			{
+				case nameof(IMultitaskingContext.Control):
+				case nameof(IMultitaskingContext.TabCount):
+					OnPropertyChanged(nameof(IsExecutable));
+					break;
+			}
+		}
+	}
+}
