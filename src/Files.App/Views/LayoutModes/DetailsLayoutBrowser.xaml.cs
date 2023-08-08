b332f0db48a596009627e7c8508e7707756fc1b6
Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>
2023-08-08 18:41:00 +0200

    Update src/Files.App/Views/LayoutModes/DetailsLayoutBrowser.xaml.cs
    Co-authored-by: hishitetsu <66369541+hishitetsu@users.noreply.github.com>

--- src/Files.App/Views/LayoutModes/DetailsLayoutBrowser.xaml.cs
+++ src/Files.App/Views/LayoutModes/DetailsLayoutBrowser.xaml.cs
@@ -880,7 +880,7 @@ namespace Files.App.Views.LayoutModes
 		private void FileList_LosingFocus(UIElement sender, LosingFocusEventArgs args)
 		{
 			// Fixes an issue where clicking an empty space would scroll to the top of the file list
-			if (args.NewFocusedElement is ListView)
+			if (args.NewFocusedElement == FileList)
 				args.TryCancel();
 		}
 	}
