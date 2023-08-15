68e2d6e83712bc95d82f8a26ebb536f34ad7593f
0x5bfa <onein528@outlook.com>
2023-08-15 22:28:15 +0900

    Initial commit

---
+++ src/Files.App/UserControls/KeyboardShortcut/KeyboardShortcut.Properties.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Files.App.UserControls.KeyboardShortcut
+{
+	public sealed partial class KeyboardShortcut
+	{
+		public static readonly DependencyProperty ShortcutTextProperty =
+			DependencyProperty.Register(
+				nameof(ShortcutText),
+				typeof(string),
+				typeof(KeyboardShortcut),
+				new PropertyMetadata(defaultValue: string.Empty, (d, e) => ((KeyboardShortcut)d).OnShortcutTextPropertyChanged((string)e.OldValue, (string)e.NewValue)));
+
+		public string ShortcutText
+		{
+			get => (string)GetValue(ShortcutTextProperty);
+			set => SetValue(ShortcutTextProperty, value);
+		}
+
+		private void OnShortcutTextPropertyChanged(string oldValue, string newValue)
+		{
+			OnShortcutTextChanged();
+		}
+	}
+}
---
+++ src/Files.App/UserControls/KeyboardShortcut/KeyboardShortcut.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using CommunityToolkit.WinUI.UI;
+using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace Files.App.UserControls.KeyboardShortcut
+{
+	public sealed partial class KeyboardShortcut : Control
+	{
+		internal const string KeyboardShortcutItemsRepeater = "PART_KeyboardShortcutItemsRepeater";
+
+		public KeyboardShortcut()
+		{
+			DefaultStyleKey = typeof(KeyboardShortcut);
+		}
+
+		private void OnShortcutTextChanged()
+		{
+			// Generate items
+			var splitItems = ShortcutText.Split('+').ToList();
+			List<KeyboardShortcutItem> items = new();
+			int index = 0;
+
+			foreach (var item in splitItems)
+			{
+				index++;
+
+				items.Add(new() { ShortcutText = item });
+
+				if (index != splitItems.Count)
+					items.Add(new() { ShortcutText = "+", IsReveal = true });
+			}
+
+			// Set value
+			if (GetTemplateChild(KeyboardShortcutItemsRepeater) is ItemsRepeater keyboardShortcutItemsRepeater)
+			{
+				keyboardShortcutItemsRepeater.ItemsSource = items;
+			}
+		}
+	}
+}
