a34b0840232555fab66150f6a346bbc0a4a02759
Jerrod Estell <jerrod.estell@wsu.edu>
2023-09-01 12:50:41 -0700

    Sorted getters in ICommandManager.cs

--- src/Files.App/Data/Commands/Manager/ICommandManager.cs
+++ src/Files.App/Data/Commands/Manager/ICommandManager.cs
@@ -9,168 +9,149 @@ namespace Files.App.Data.Commands
 		IRichCommand this[string code] { get; }
 		IRichCommand this[HotKey customHotKey] { get; }
 
-		IRichCommand None { get; }
-
-		IRichCommand OpenHelp { get; }
-		IRichCommand ToggleFullScreen { get; }
-		IRichCommand EnterCompactOverlay { get; }
-		IRichCommand ExitCompactOverlay { get; }
-		IRichCommand ToggleCompactOverlay { get; }
-		IRichCommand Search { get; }
-		IRichCommand SearchUnindexedItems { get; }
-		IRichCommand EditPath { get; }
-		IRichCommand Redo { get; }
-		IRichCommand Undo { get; }
-
-		IRichCommand ToggleShowHiddenItems { get; }
-		IRichCommand ToggleShowFileExtensions { get; }
-		IRichCommand TogglePreviewPane { get; }
-
-		IRichCommand CopyItem { get; }
-		IRichCommand CopyPath { get; }
-		IRichCommand CutItem { get; }
-		IRichCommand PasteItem { get; }
-		IRichCommand PasteItemToSelection { get; }
-		IRichCommand DeleteItem { get; }
-		IRichCommand DeleteItemPermanently { get; }
-		IRichCommand SelectAll { get; }
-		IRichCommand InvertSelection { get; }
-		IRichCommand ClearSelection { get; }
-		IRichCommand ToggleSelect { get; }
-		IRichCommand ShareItem { get; }
-		IRichCommand CreateFolder { get; }
-		IRichCommand CreateFolderWithSelection { get; }
 		IRichCommand AddItem { get; }
-		IRichCommand CreateShortcut { get; }
-		IRichCommand CreateShortcutFromDialog { get; }
-		IRichCommand EmptyRecycleBin { get; }
-		IRichCommand RestoreRecycleBin { get; }
-		IRichCommand RestoreAllRecycleBin { get; }
-		IRichCommand FormatDrive { get; }
-		IRichCommand OpenItem { get; }
-		IRichCommand OpenItemWithApplicationPicker { get; }
-		IRichCommand OpenParentFolder { get; }
-		IRichCommand OpenFileLocation { get; }
-		IRichCommand RefreshItems { get; }
-		IRichCommand Rename { get; }
-
-		IRichCommand PinToStart { get; }
-		IRichCommand UnpinFromStart { get; }
-		IRichCommand PinItemToFavorites { get; }
-		IRichCommand UnpinItemFromFavorites { get; }
-
-		IRichCommand SetAsWallpaperBackground { get; }
-		IRichCommand SetAsSlideshowBackground { get; }
-		IRichCommand SetAsLockscreenBackground { get; }
-
-		IRichCommand InstallFont { get; }
-		IRichCommand InstallInfDriver { get; }
-		IRichCommand InstallCertificate { get; }
-
-		IRichCommand RunAsAdmin { get; }
-		IRichCommand RunAsAnotherUser { get; }
-		IRichCommand RunWithPowershell { get; }
-
-		IRichCommand LaunchPreviewPopup { get; }
-
-		IRichCommand CompressIntoArchive { get; }
-		IRichCommand CompressIntoSevenZip { get; }
-		IRichCommand CompressIntoZip { get; }
-		IRichCommand DecompressArchive { get; }
-		IRichCommand DecompressArchiveHere { get; }
-		IRichCommand DecompressArchiveToChildFolder { get; }
-
-		IRichCommand RotateLeft { get; }
-		IRichCommand RotateRight { get; }
-
-		IRichCommand OpenInVS { get; }
-		IRichCommand OpenInVSCode { get; }
-		IRichCommand OpenProperties { get; }
-		IRichCommand OpenSettings { get; }
-		IRichCommand OpenTerminal { get; }
-		IRichCommand OpenTerminalAsAdmin { get; }
-		IRichCommand OpenCommandPalette { get; }
-
-		IRichCommand LayoutDecreaseSize { get; }
-		IRichCommand LayoutIncreaseSize { get; }
-		IRichCommand LayoutDetails { get; }
-		IRichCommand LayoutTiles { get; }
-		IRichCommand LayoutGridSmall { get; }
-		IRichCommand LayoutGridMedium { get; }
-		IRichCommand LayoutGridLarge { get; }
-		IRichCommand LayoutColumns { get; }
-		IRichCommand LayoutAdaptive { get; }
-
-		IRichCommand SortByName { get; }
-		IRichCommand SortByDateModified { get; }
-		IRichCommand SortByDateCreated { get; }
-		IRichCommand SortBySize { get; }
-		IRichCommand SortByType { get; }
-		IRichCommand SortBySyncStatus { get; }
-		IRichCommand SortByTag { get; }
-		IRichCommand SortByPath { get; }
-		IRichCommand SortByOriginalFolder { get; }
-		IRichCommand SortByDateDeleted { get; }
-		IRichCommand SortAscending { get; }
-		IRichCommand SortDescending { get; }
-		IRichCommand ToggleSortDirection { get; }
-		IRichCommand ToggleSortDirectoriesAlongsideFiles { get; }
-
-		IRichCommand GroupByNone { get; }
-		IRichCommand GroupByName { get; }
-		IRichCommand GroupByDateModified { get; }
-		IRichCommand GroupByDateCreated { get; }
-		IRichCommand GroupBySize { get; }
-		IRichCommand GroupByType { get; }
-		IRichCommand GroupBySyncStatus { get; }
-		IRichCommand GroupByTag { get; }
-		IRichCommand GroupByOriginalFolder { get; }
-		IRichCommand GroupByDateDeleted { get; }
-		IRichCommand GroupByFolderPath { get; }
-		IRichCommand GroupByDateModifiedYear { get; }
-		IRichCommand GroupByDateModifiedMonth { get; }
-		IRichCommand GroupByDateCreatedYear { get; }
-		IRichCommand GroupByDateCreatedMonth { get; }
-		IRichCommand GroupByDateDeletedYear { get; }
-		IRichCommand GroupByDateDeletedMonth { get; }
-		IRichCommand GroupAscending { get; }
-		IRichCommand GroupDescending { get; }
-		IRichCommand ToggleGroupDirection { get; }
-		IRichCommand GroupByYear { get; }
-		IRichCommand GroupByMonth { get; }
-		IRichCommand ToggleGroupByDateUnit { get; }
-
-		IRichCommand NewTab { get; }
-		IRichCommand NavigateBack { get; }
-		IRichCommand NavigateForward { get; }
-		IRichCommand NavigateUp { get; }
-
-		IRichCommand DuplicateCurrentTab { get; }
-		IRichCommand DuplicateSelectedTab { get; }
+		IRichCommand ClearSelection { get; }
+		IRichCommand CloseOtherTabsCurrent { get; }
+		IRichCommand CloseOtherTabsSelected { get; }
+		IRichCommand ClosePane { get; }
+		IRichCommand CloseSelectedTab { get; }
 		IRichCommand CloseTabsToTheLeftCurrent { get; }
 		IRichCommand CloseTabsToTheLeftSelected { get; }
 		IRichCommand CloseTabsToTheRightCurrent { get; }
 		IRichCommand CloseTabsToTheRightSelected { get; }
-		IRichCommand CloseOtherTabsCurrent { get; }
-		IRichCommand CloseOtherTabsSelected { get; }
-		IRichCommand OpenDirectoryInNewPaneAction { get; }
-		IRichCommand OpenDirectoryInNewTabAction { get; }
-		IRichCommand OpenInNewWindowItemAction { get; }
-		IRichCommand ReopenClosedTab { get; }
-		IRichCommand PreviousTab { get; }
-		IRichCommand NextTab { get; }
-		IRichCommand CloseSelectedTab { get; }
-		IRichCommand OpenNewPane { get; }
-		IRichCommand ClosePane { get; }
-    
-		IRichCommand PlayAll { get; }
-
+		IRichCommand CompressIntoArchive { get; }
+		IRichCommand CompressIntoSevenZip { get; }
+		IRichCommand CompressIntoZip { get; }
+		IRichCommand CopyItem { get; }
+		IRichCommand CopyPath { get; }
+		IRichCommand CreateFolder { get; }
+		IRichCommand CreateFolderWithSelection { get; }
+		IRichCommand CreateShortcut { get; }
+		IRichCommand CreateShortcutFromDialog { get; }
+		IRichCommand CutItem { get; }
+		IRichCommand DecompressArchive { get; }
+		IRichCommand DecompressArchiveHere { get; }
+		IRichCommand DecompressArchiveToChildFolder { get; }
+		IRichCommand DeleteItem { get; }
+		IRichCommand DeleteItemPermanently { get; }
+		IRichCommand DuplicateCurrentTab { get; }
+		IRichCommand DuplicateSelectedTab { get; }
+		IRichCommand EditPath { get; }
+		IRichCommand EmptyRecycleBin { get; }
+		IRichCommand EnterCompactOverlay { get; }
+		IRichCommand ExitCompactOverlay { get; }
+		IRichCommand FormatDrive { get; }
 		IRichCommand GitFetch { get; }
 		IRichCommand GitInit { get; }
 		IRichCommand GitPull { get; }
 		IRichCommand GitPush { get; }
 		IRichCommand GitSync { get; }
-
+		IRichCommand GroupAscending { get; }
+		IRichCommand GroupByDateCreated { get; }
+		IRichCommand GroupByDateCreatedMonth { get; }
+		IRichCommand GroupByDateCreatedYear { get; }
+		IRichCommand GroupByDateDeleted { get; }
+		IRichCommand GroupByDateDeletedMonth { get; }
+		IRichCommand GroupByDateDeletedYear { get; }
+		IRichCommand GroupByDateModified { get; }
+		IRichCommand GroupByDateModifiedMonth { get; }
+		IRichCommand GroupByDateModifiedYear { get; }
+		IRichCommand GroupByFolderPath { get; }
+		IRichCommand GroupByMonth { get; }
+		IRichCommand GroupByName { get; }
+		IRichCommand GroupByNone { get; }
+		IRichCommand GroupByOriginalFolder { get; }
+		IRichCommand GroupBySize { get; }
+		IRichCommand GroupBySyncStatus { get; }
+		IRichCommand GroupByTag { get; }
+		IRichCommand GroupByType { get; }
+		IRichCommand GroupByYear { get; }
+		IRichCommand GroupDescending { get; }
+		IRichCommand InstallCertificate { get; }
+		IRichCommand InstallFont { get; }
+		IRichCommand InstallInfDriver { get; }
+		IRichCommand InvertSelection { get; }
+		IRichCommand LaunchPreviewPopup { get; }
+		IRichCommand LayoutAdaptive { get; }
+		IRichCommand LayoutColumns { get; }
+		IRichCommand LayoutDecreaseSize { get; }
+		IRichCommand LayoutDetails { get; }
+		IRichCommand LayoutGridLarge { get; }
+		IRichCommand LayoutGridMedium { get; }
+		IRichCommand LayoutGridSmall { get; }
+		IRichCommand LayoutIncreaseSize { get; }
+		IRichCommand LayoutTiles { get; }
+		IRichCommand NavigateBack { get; }
+		IRichCommand NavigateForward { get; }
+		IRichCommand NavigateUp { get; }
+		IRichCommand NewTab { get; }
+		IRichCommand NextTab { get; }
+		IRichCommand None { get; }
 		IRichCommand OpenAllTaggedItems { get; }
+		IRichCommand OpenCommandPalette { get; }
+		IRichCommand OpenDirectoryInNewPaneAction { get; }
+		IRichCommand OpenDirectoryInNewTabAction { get; }
+		IRichCommand OpenFileLocation { get; }
+		IRichCommand OpenHelp { get; }
+		IRichCommand OpenInNewWindowItemAction { get; }
+		IRichCommand OpenInVS { get; }
+		IRichCommand OpenInVSCode { get; }
+		IRichCommand OpenItem { get; }
+		IRichCommand OpenItemWithApplicationPicker { get; }
+		IRichCommand OpenNewPane { get; }
+		IRichCommand OpenParentFolder { get; }
+		IRichCommand OpenProperties { get; }
+		IRichCommand OpenSettings { get; }
+		IRichCommand OpenTerminal { get; }
+		IRichCommand OpenTerminalAsAdmin { get; }
+		IRichCommand PasteItem { get; }
+		IRichCommand PasteItemToSelection { get; }
+		IRichCommand PinItemToFavorites { get; }
+		IRichCommand PinToStart { get; }
+		IRichCommand PlayAll { get; }
+		IRichCommand PreviousTab { get; }
+		IRichCommand Redo { get; }
+		IRichCommand RefreshItems { get; }
+		IRichCommand Rename { get; }
+		IRichCommand ReopenClosedTab { get; }
+		IRichCommand RestoreAllRecycleBin { get; }
+		IRichCommand RestoreRecycleBin { get; }
+		IRichCommand RotateLeft { get; }
+		IRichCommand RotateRight { get; }
+		IRichCommand RunAsAdmin { get; }
+		IRichCommand RunAsAnotherUser { get; }
+		IRichCommand RunWithPowershell { get; }
+		IRichCommand Search { get; }
+		IRichCommand SearchUnindexedItems { get; }
+		IRichCommand SelectAll { get; }
+		IRichCommand SetAsLockscreenBackground { get; }
+		IRichCommand SetAsSlideshowBackground { get; }
+		IRichCommand SetAsWallpaperBackground { get; }
+		IRichCommand ShareItem { get; }
+		IRichCommand SortAscending { get; }
+		IRichCommand SortByDateCreated { get; }
+		IRichCommand SortByDateDeleted { get; }
+		IRichCommand SortByDateModified { get; }
+		IRichCommand SortByName { get; }
+		IRichCommand SortByOriginalFolder { get; }
+		IRichCommand SortByPath { get; }
+		IRichCommand SortBySize { get; }
+		IRichCommand SortBySyncStatus { get; }
+		IRichCommand SortByTag { get; }
+		IRichCommand SortByType { get; }
+		IRichCommand SortDescending { get; }
+		IRichCommand ToggleCompactOverlay { get; }
+		IRichCommand ToggleFullScreen { get; }
+		IRichCommand ToggleGroupByDateUnit { get; }
+		IRichCommand ToggleGroupDirection { get; }
+		IRichCommand TogglePreviewPane { get; }
+		IRichCommand ToggleSelect { get; }
+		IRichCommand ToggleShowFileExtensions { get; }
+		IRichCommand ToggleShowHiddenItems { get; }
+		IRichCommand ToggleSortDirection { get; }
+		IRichCommand ToggleSortDirectoriesAlongsideFiles { get; }
+		IRichCommand Undo { get; }
+		IRichCommand UnpinFromStart { get; }
+		IRichCommand UnpinItemFromFavorites { get; }
 	}
 }
