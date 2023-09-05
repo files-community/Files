5966acf7b9d412deb857ff6e33d25f4ad12fcb95
Yair <39923744+yaira2@users.noreply.github.com>
2023-09-05 10:54:55 -0400

    Feature: Added support for lzh & Appxbundle

--- src/Files.Shared/Helpers/FileExtensionHelpers.cs
+++ src/Files.Shared/Helpers/FileExtensionHelpers.cs
@@ -52,7 +52,7 @@ namespace Files.Shared.Helpers
 		/// <returns><c>true</c> if the fileExtensionToCheck is a zip bundle file; otherwise, <c>false</c>.</returns>
 		public static bool IsZipFile(string? fileExtensionToCheck)
 		{
-			return HasExtension(fileExtensionToCheck, ".zip", ".msix", ".appx", ".msixbundle", ".7z", ".rar", ".tar", ".mcpack", ".mcworld", ".jar", ".gz");
+			return HasExtension(fileExtensionToCheck, ".zip", ".msix", ".appx", ".msixbundle", ".appxbundle", ".7z", ".rar", ".tar", ".mcpack", ".mcworld", ".jar", ".gz", ".lzh");
 		}
 
 		public static bool IsBrowsableZipFile(string? filePath, out string? ext)
