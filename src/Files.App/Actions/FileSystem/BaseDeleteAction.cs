e245e7ab6c617e18d1e0380d28cc613833a6c976
Quaint Mako <110472580+QuaintMako@users.noreply.github.com>
2023-09-06 20:02:36 +0200

    When removing a file

--- src/Files.App/Actions/FileSystem/BaseDeleteAction.cs
+++ src/Files.App/Actions/FileSystem/BaseDeleteAction.cs
@@ -35,8 +35,8 @@ namespace Files.App.Actions
 							: FilesystemItemType.Directory));
 
 			await context.ShellPage!.FilesystemHelpers.DeleteItemsAsync(items, settings.DeleteConfirmationPolicy, permanently, true);
-
 			await context.ShellPage.FilesystemViewModel.ApplyFilesAndFoldersChangesAsync();
+			Ioc.Default.GetRequiredService<SidebarViewModel>().SidebarPinnedModel.UpdateRecycleBinIcon();
 		}
 
 		private void Context_PropertyChanged(object? sender, PropertyChangedEventArgs e)
