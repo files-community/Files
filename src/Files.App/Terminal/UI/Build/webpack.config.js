d640652f9a8cf00add4a441279f50a65f3e834ee
Marco Gavelli <marcogavelli2@gmail.com>
2023-10-25 23:03:29 +0200

    Add build files

---
+++ src/Files.App/Terminal/UI/Build/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
