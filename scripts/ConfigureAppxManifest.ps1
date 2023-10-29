4d9c5c7be5884ec771f25b5363353943f06e944a
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2023-10-29 08:02:05 +0000

    Added ps script for configuring appx manifest

---
+++ scripts/ConfigureAppxManifest.ps1
@@ -0,0 +1,16 @@
+<#
+    .SYNOPSIS 
+    A
+
+    .DESCRIPTION
+    B
+
+    .PARAMETER Configuration
+    Runs against main vs. current branch
+
+    .EXAMPLE
+    PS> .\C.ps1
+#>
+param(
+    [string]$Configuration = "Dev",
+)
