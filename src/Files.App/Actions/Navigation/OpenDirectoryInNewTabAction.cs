ea792cf857e77c7b936d52f5e12c2b86c41f3d92
Yair <39923744+yaira2@users.noreply.github.com>
2023-11-09 10:25:01 -0500

    Fix: Fixed NullReferenceException in OpenDirectoryInNewTabAction

--- src/Files.App/Actions/Navigation/OpenDirectoryInNewTabAction.cs
+++ src/Files.App/Actions/Navigation/OpenDirectoryInNewTabAction.cs
@@ -21,6 +21,8 @@ namespace Files.App.Actions
 			=> new(opacityStyle: "ColorIconOpenInNewTab");
 
 		public bool IsExecutable =>
+			context.ShellPage is not null &&
+			context.ShellPage.SlimContentPage is not null &&
 			context.SelectedItems.Count <= 5 &&
 			context.SelectedItems.Where(x => x.IsFolder == true).Count() == context.SelectedItems.Count &&
 			userSettingsService.GeneralSettingsService.ShowOpenInNewTab;
@@ -36,7 +38,7 @@ namespace Files.App.Actions
 
 		public async Task ExecuteAsync()
 		{
-			foreach (ListedItem listedItem in context.ShellPage.SlimContentPage.SelectedItems)
+			foreach (ListedItem listedItem in context.ShellPage?.SlimContentPage?.SelectedItems)
 			{
 				await MainWindow.Instance.DispatcherQueue.EnqueueOrInvokeAsync(async () =>
 				{
