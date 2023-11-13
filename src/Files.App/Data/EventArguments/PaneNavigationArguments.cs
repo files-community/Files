2478d54e673809192dde5f753576d6ef10e7c5e7
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2023-11-14 01:22:53 +0900

    Fix tab icon at launch

--- src/Files.App/Data/EventArguments/PaneNavigationArguments.cs
+++ src/Files.App/Data/EventArguments/PaneNavigationArguments.cs
@@ -12,5 +12,24 @@ namespace Files.App.Data.EventArguments
 		public string? RightPaneNavPathParam { get; set; }
 
 		public string? RightPaneSelectItemParam { get; set; }
+
+		public static bool operator ==(PaneNavigationArguments? a1, PaneNavigationArguments? a2)
+		{
+			if (a1 is null && a2 is null)
+				return true;
+
+			if (a1 is null || a2 is null)
+				return false;
+
+			return a1.LeftPaneNavPathParam == a2.LeftPaneNavPathParam &&
+				a1.LeftPaneSelectItemParam == a2.LeftPaneSelectItemParam &&
+				a1.RightPaneNavPathParam == a2.RightPaneNavPathParam &&
+				a1.RightPaneSelectItemParam == a2.RightPaneSelectItemParam;
+		}
+
+		public static bool operator !=(PaneNavigationArguments? a1, PaneNavigationArguments? a2)
+		{
+			return !(a1 == a2);
+		}
 	}
 }
