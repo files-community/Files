2e43a1aadb0b3578e8007751e9825d19cb4d6a73
Tony Barrera <antonioseet1@gmail.com>
2023-11-26 11:57:02 -0800

    Fix: Fixed an issue with dropping multiple items onto executable files (#14067)

--- src/Files.App/Utils/Storage/Operations/FilesystemHelpers.cs
+++ src/Files.App/Utils/Storage/Operations/FilesystemHelpers.cs
@@ -255,16 +255,12 @@ namespace Files.App.Utils.Storage
 				else if (operation.HasFlag(DataPackageOperation.Link))
 				{
 					// Open with piggybacks off of the link operation, since there isn't one for it
-					if (isTargetExecutable)
+					if (isTargetExecutable || isTargetPythonFile)
 					{
 						var items = await GetDraggedStorageItems(packageView);
+						if (isTargetPythonFile && !SoftwareHelpers.IsPythonInstalled())
+							return ReturnResult.Cancelled;
 						NavigationHelpers.OpenItemsWithExecutableAsync(associatedInstance, items, destination);
-						return ReturnResult.Success;
-					}
-					else if (isTargetPythonFile)
-					{
-						var items = await GetDraggedStorageItems(packageView);
-						NavigationHelpers.OpenItemsWithPythonAsync(associatedInstance, items, destination);
 						return ReturnResult.Success;
 					}
 					else
