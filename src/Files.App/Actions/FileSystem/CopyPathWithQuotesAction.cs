9a1a10ffb5e496ddd27add52844904c7682b9cce
Yair <39923744+yaira2@users.noreply.github.com>
2023-11-28 11:39:13 -0500

    Update CopyPathWithQuotesAction.cs

--- src/Files.App/Actions/FileSystem/CopyPathWithQuotesAction.cs
+++ src/Files.App/Actions/FileSystem/CopyPathWithQuotesAction.cs
@@ -34,13 +34,11 @@ namespace Files.App.Actions
 			if (context.ShellPage?.SlimContentPage is not null)
 			{
 				var path = context.ShellPage.SlimContentPage.SelectedItems is not null
-					? context.ShellPage.SlimContentPage.SelectedItems.Select(x => x.ItemPath).Aggregate((accum, current) => accum + "\n" + current)
+					? context.ShellPage.SlimContentPage.SelectedItems.Select(x => x.ItemPath).Aggregate((accum, current) => accum + "\n" + "\"" + current + "\"")
 					: context.ShellPage.FilesystemViewModel.WorkingDirectory;
 
 				if (FtpHelpers.IsFtpPath(path))
 					path = path.Replace("\\", "/", StringComparison.Ordinal);
-
-				path = "\"" + path + "\"";
 
 				SafetyExtensions.IgnoreExceptions(() =>
 				{
