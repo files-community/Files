2e9bdac77573c5184a0bb2309631ee228651b236
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2023-12-20 02:04:53 +0900

    Code Quality: Introduced SideBarContext (#14116)

---
+++ src/Files.App/Data/Contexts/SideBar/SideBarContext.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Data.Contexts
+{
+	/// <inheritdoc cref="ISidebarContext"/>
+	internal class SidebarContext : ObservableObject, ISidebarContext
+	{
+		private readonly SidebarPinnedModel favoriteModel = App.QuickAccessManager.Model;
+
+		private int FavoriteIndex =>
+			IsItemRightClicked
+				? favoriteModel.IndexOfItem(_RightClickedItem!)
+				: -1;
+
+		private INavigationControlItem? _RightClickedItem = null;
+		public INavigationControlItem? RightClickedItem => _RightClickedItem;
+
+		public bool IsItemRightClicked =>
+			_RightClickedItem is not null;
+
+		public bool IsFavoriteItem =>
+			IsItemRightClicked &&
+			_RightClickedItem!.Section is SectionType.Favorites &&
+			FavoriteIndex is not -1;
+
+		public DriveItem? OpenDriveItem
+			=> _RightClickedItem as DriveItem;
+
+		public SidebarContext()
+		{
+			SidebarViewModel.RightClickedItemChanged += SidebarControl_RightClickedItemChanged;
+		}
+
+		public void SidebarControl_RightClickedItemChanged(object? sender, INavigationControlItem? e)
+		{
+			if (SetProperty(ref _RightClickedItem, e, nameof(RightClickedItem)))
+			{
+				OnPropertyChanged(nameof(IsItemRightClicked));
+				OnPropertyChanged(nameof(FavoriteIndex));
+				OnPropertyChanged(nameof(IsFavoriteItem));
+				OnPropertyChanged(nameof(OpenDriveItem));
+			}
+		}
+	}
+}
