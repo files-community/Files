40e9e4b0b65601734361a8ab9e9f45d7558357b8
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2023-12-22 01:15:07 +0000

    unsealed OpenTerminalAction

--- src/Files.App/Actions/Open/OpenTerminalAction.cs
+++ src/Files.App/Actions/Open/OpenTerminalAction.cs
@@ -6,7 +6,7 @@ using Windows.Storage;
 
 namespace Files.App.Actions
 {
-	internal sealed class OpenTerminalAction : ObservableObject, IAction
+	internal abstract class OpenTerminalAction : ObservableObject, IAction
 	{
 		private readonly IContentPageContext context;
 
