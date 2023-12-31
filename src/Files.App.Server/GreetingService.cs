e29c50f2a67734a3ef74cdd2edd7b71dd847e2b6
Steven He <hez2010@outlook.com>
2023-12-31 18:44:04 +0900

    Add an out-of-proc WinRT server

---
+++ src/Files.App.Server/GreetingService.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Notifications;
+
+namespace Files.App.Server;
+
+public sealed class GreetingService
+{
+	public static void ShowGreet()
+	{
+		var doc = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
+		doc.SelectSingleNode("""//text[@id="1"]""").InnerText = "Welcome to Files!";
+		doc.SelectSingleNode("""//text[@id="2"]""").InnerText = "This is a toast notification sent from Files.App.Server";
+		ToastNotificationManager.CreateToastNotifier("App").Show(new ToastNotification(doc));
+	}
+}
--- src/Files.App/Program.cs
+++ src/Files.App/Program.cs
@@ -57,7 +57,10 @@ namespace Files.App
 		{
 			WinRT.ComWrappersSupport.InitializeComWrappers();
 
-			var proc = Process.GetCurrentProcess();
+#if DEBUG
+			Files.App.Server.GreetingService.ShowGreet();
+#endif
+
 			var OpenTabInExistingInstance = ApplicationData.Current.LocalSettings.Values.Get("OpenTabInExistingInstance", true);
 			var activatedArgs = AppInstance.GetCurrent().GetActivatedEventArgs();
 
@@ -76,7 +79,7 @@ namespace Files.App
 							case ParsedCommandType.ExplorerShellCommand:
 								if (!Constants.UserEnvironmentPaths.ShellPlaces.ContainsKey(command.Payload.ToUpperInvariant()))
 								{
-									OpenShellCommandInExplorer(command.Payload, proc.Id);
+									OpenShellCommandInExplorer(command.Payload, Environment.ProcessId);
 
 									// Exit
 									return;
@@ -157,11 +160,11 @@ namespace Files.App
 				}
 			}
 
-			var currentInstance = AppInstance.FindOrRegisterForKey((-proc.Id).ToString());
+			var currentInstance = AppInstance.FindOrRegisterForKey((-Environment.ProcessId).ToString());
 			if (currentInstance.IsCurrent)
 				currentInstance.Activated += OnActivated;
 
-			ApplicationData.Current.LocalSettings.Values["INSTANCE_ACTIVE"] = -proc.Id;
+			ApplicationData.Current.LocalSettings.Values["INSTANCE_ACTIVE"] = -Environment.ProcessId;
 
 			Application.Start((p) =>
 			{
@@ -178,7 +181,7 @@ namespace Files.App
 		private static async void OnActivated(object? sender, AppActivationArguments args)
 		{
 			// WINUI3: Verify if needed or OnLaunched is called
-      if (App.Current is App thisApp)
+			if (App.Current is App thisApp)
 				await thisApp.OnActivatedAsync(args);
 		}
 
