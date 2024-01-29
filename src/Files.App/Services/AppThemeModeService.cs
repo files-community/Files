0422ee52387f1537bf85bf17d9c062dd0e716df5
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2024-01-29 07:51:56 +0000

    Initial commit

---
+++ src/Files.App/Services/AppThemeModeService.cs
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.Core.Services
+{
+	public class AppThemeModeService : IAppThemeModeService
+	{
+	}
+}
