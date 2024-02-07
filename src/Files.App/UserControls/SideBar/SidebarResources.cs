2d533ede74812452fccf9ec3be4baa83a1cd1798
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2024-02-07 02:32:42 +0000

    Revert

---
+++ src/Files.App/UserControls/SideBar/SidebarResources.cs
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Microsoft.UI.Xaml;
+
+namespace Files.App.UserControls.Sidebar
+{
+	/// <summary>
+	/// Backing dictionary for Sidebar Resources to support x:Bind
+	/// </summary>
+	partial class SidebarResources : ResourceDictionary
+    {
+    }
+}
