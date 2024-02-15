11f2eeb11f4cc138df8788cb5e2d3adac8e6176a
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-02-16 00:51:25 +0900

    Replace drive card item context flyout with RichCommand

---
+++ src/Files.App/Data/Factories/WidgetDriveCardItemContextFlyoutFactory.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Data.Factories
+{
+	public static class WidgetDriveCardItemContextFlyoutFactory
+	{
+		private static ICommandManager CommandManager { get; } = Ioc.Default.GetRequiredService<ICommandManager>();
+
+		public static List<ContextMenuFlyoutItemViewModel> Generate()
+		{
+			return new List<ContextMenuFlyoutItemViewModel>()
+			{
+				new ContextMenuFlyoutItemViewModelBuilder(CommandManager.OpenDirectoryInNewTabAction).Build(),
+				new ContextMenuFlyoutItemViewModelBuilder(CommandManager.OpenInNewWindowItemAction).Build(),
+				new ContextMenuFlyoutItemViewModelBuilder(CommandManager.OpenDirectoryInNewPaneAction).Build(),
+				new ContextMenuFlyoutItemViewModelBuilder(CommandManager.PinItemToFavorites).Build(),
+				new ContextMenuFlyoutItemViewModelBuilder(CommandManager.UnpinItemFromFavorites).Build(),
+				//new ContextMenuFlyoutItemViewModelBuilder(CommandManager.EjectDriveAction).Build(),
+				new ContextMenuFlyoutItemViewModelBuilder(CommandManager.FormatDrive).Build(),
+				new ContextMenuFlyoutItemViewModelBuilder(CommandManager.OpenProperties).Build(),
+				new()
+				{
+					Text = "TurnOnBitLocker".GetLocalizedResource(),
+					Tag = "TurnOnBitLockerPlaceholder",
+					CollapseLabel = true,
+					IsEnabled = false,
+				},
+				new()
+				{
+					Text = "ManageBitLocker".GetLocalizedResource(),
+					Tag = "ManageBitLockerPlaceholder",
+					CollapseLabel = true,
+					IsEnabled = false
+				},
+				new()
+				{
+					ItemType = ContextMenuFlyoutItemType.Separator,
+					Tag = "OverflowSeparator",
+				},
+				new()
+				{
+					Text = "Loading".GetLocalizedResource(),
+					Glyph = "\xE712",
+					Items = [],
+					ID = "ItemOverflow",
+					Tag = "ItemOverflow",
+					IsEnabled = false,
+				}
+			}.Where(x => x.ShowItem).ToList();
+		}
+	}
+}
