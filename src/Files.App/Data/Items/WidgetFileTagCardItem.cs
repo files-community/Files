811a3a37f9fb6ca700036a57bb04b1cc0690bde5
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2024-02-17 10:25:42 +0900

    Update WidgetFileTagCardItem.cs

--- src/Files.App/Data/Items/WidgetFileTagCardItem.cs
+++ src/Files.App/Data/Items/WidgetFileTagCardItem.cs
@@ -56,10 +56,10 @@ namespace Files.App.Data.Items
 			_Path = associatedStorable.TryGetPath();
 			Item = this;
 
-			ClickCommand = new AsyncRelayCommand<CancellationToken?>(ClickAsync);
+			ClickCommand = new AsyncRelayCommand<object?>(ClickAsync);
 		}
 
-		private Task ClickAsync(CancellationToken? cancellationToken)
+		private Task ClickAsync(object? cancellationToken)
 		{
 			return _openAction(_associatedStorable.Id);
 		}
