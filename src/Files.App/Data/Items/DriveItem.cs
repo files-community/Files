d641b698645f6a7bd4078ad1ce89c74bd05ae487
Yair <39923744+yaira2@users.noreply.github.com>
2024-02-21 12:02:35 -0500

    Update DriveItem.cs

--- src/Files.App/Data/Items/DriveItem.cs
+++ src/Files.App/Data/Items/DriveItem.cs
@@ -2,10 +2,6 @@
 // Licensed under the MIT License. See the LICENSE.
 
 using Files.App.Storage.WindowsStorage;
-using Files.Core.Storage;
-using Files.Core.Storage.Enums;
-using Files.Core.Storage.LocatableStorage;
-using Files.Core.Storage.NestedStorage;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media.Imaging;
