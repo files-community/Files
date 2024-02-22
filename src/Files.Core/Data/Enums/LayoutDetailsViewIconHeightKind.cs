e24fa63310397e609f97fc4f0b81a35a2a5c1d33
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-02-22 17:00:19 +0900

    Add license header

--- src/Files.Core/Data/Enums/LayoutDetailsViewIconHeightKind.cs
+++ src/Files.Core/Data/Enums/LayoutDetailsViewIconHeightKind.cs
@@ -1,4 +1,7 @@
-﻿namespace Files.Core.Data.Enums
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.Core.Data.Enums
 {
 	public enum LayoutDetailsViewIconHeightKind
 	{
