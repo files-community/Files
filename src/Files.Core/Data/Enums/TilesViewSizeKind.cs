83132c91ab227f27be85136fa06893d883a543b5
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2024-02-28 02:24:14 +0900

    Feature: Added tooltips to Layout Picker (#14852)

--- src/Files.Core/Data/Enums/TilesViewSizeKind.cs
+++ src/Files.Core/Data/Enums/TilesViewSizeKind.cs
@@ -3,8 +3,14 @@
 
 namespace Files.Core.Data.Enums
 {
+	/// <summary>
+	/// Defines constants that specify the size in the Tiles View layout.
+	/// </summary>
 	public enum TilesViewSizeKind
 	{
+		/// <summary>
+		/// The size is small.
+		/// </summary>
 		Small = 1,
 	}
 }
