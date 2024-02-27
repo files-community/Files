8820632e22062df6857816c3fbeb9ce97c40498f
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-02-27 23:33:25 +0900

    Initial commit

---
+++ src/Files.App/Converters/EnumValueToEnumNameConverter.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Microsoft.UI.Xaml.Data;
+
+namespace Files.App.Converters
+{
+	internal sealed class EnumValueToEnumNameConverter : IValueConverter
+	{
+		public string EnumTypeName { get; set; } = string.Empty;
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			var stringValue = value.ToString() ?? string.Empty;
+
+			return EnumTypeName switch
+			{
+				"DetailsViewSizeKind" => Enum.Parse<DetailsViewSizeKind>(stringValue).ToString(),
+				"ListViewSizeKind" => Enum.Parse<ListViewSizeKind>(stringValue).ToString(),
+				"TilesViewSizeKind" => Enum.Parse<TilesViewSizeKind>(stringValue).ToString(),
+				"GridViewSizeKind" => Enum.Parse<GridViewSizeKind>(stringValue).ToString(),
+				"ColumnsViewSizeKind" => Enum.Parse<ColumnsViewSizeKind>(stringValue).ToString(),
+				_ => string.Empty,
+			};
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
