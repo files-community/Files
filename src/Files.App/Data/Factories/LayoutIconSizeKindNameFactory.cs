b675aa041ff964a3eb498018af8d93aabba91ab8
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-02-28 01:21:48 +0900

    Added template for the factory

---
+++ src/Files.App/Data/Factories/LayoutIconSizeKindNameFactory.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2023 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Data.Factories
+{
+	internal sealed class LayoutIconSizeKindNameFactory
+	{
+	}
+}
