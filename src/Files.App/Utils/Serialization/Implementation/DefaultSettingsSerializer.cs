b47823cb57525ee1e6b5fbbe97be241cba4b3ad2
gumbarros <gustavomauriciodebarros@gmail.com>
2024-04-14 19:42:08 -0300

    Pass `null` to CreateDirectoryFromApp

--- src/Files.App/Utils/Serialization/Implementation/DefaultSettingsSerializer.cs
+++ src/Files.App/Utils/Serialization/Implementation/DefaultSettingsSerializer.cs
@@ -3,7 +3,6 @@
 
 using System.IO;
 using Windows.Win32;
-using Windows.Win32.Security;
 using static Files.App.Helpers.NativeFileOperationsHelper;
 
 namespace Files.App.Utils.Serialization.Implementation
@@ -14,7 +13,7 @@ namespace Files.App.Utils.Serialization.Implementation
 
 		public bool CreateFile(string path)
 		{
-			PInvoke.CreateDirectoryFromApp(Path.GetDirectoryName(path), new SECURITY_ATTRIBUTES());
+			PInvoke.CreateDirectoryFromApp(Path.GetDirectoryName(path), null);
 
 			var hFile = CreateFileFromApp(path, GENERIC_READ, FILE_SHARE_READ, IntPtr.Zero, OPEN_ALWAYS, (uint)File_Attributes.BackupSemantics, IntPtr.Zero);
 			if (hFile.IsHandleInvalid())
