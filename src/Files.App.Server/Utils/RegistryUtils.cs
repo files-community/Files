c7fba820d100ba7747836b20434ec534e1d45be4
Steve <hez2010@outlook.com>
2024-04-19 09:05:34 +0900

    Update src/Files.App.Server/Utils/RegistryUtils.cs
    Co-authored-by: hishitetsu <66369541+hishitetsu@users.noreply.github.com>

--- src/Files.App.Server/Utils/RegistryUtils.cs
+++ src/Files.App.Server/Utils/RegistryUtils.cs
@@ -7,16 +7,16 @@ internal sealed class RegistryUtils
 {
 	internal static string CombineKeys(string key1, string key2)
 	{
-		key1 = key1.Replace('/', '\\').TrimStart('\\').TrimEnd('\\');
-		key2 = key2.Replace('/', '\\').TrimStart('\\').TrimEnd('\\');
+		key1 = key1.Replace('/', '\\').Trim('\\');
+		key2 = key2.Replace('/', '\\').Trim('\\');
 		return $"{key1}\\{key2}";
 	}
 
 	internal static string CombineKeys(string key1, string key2, string key3)
 	{
-		key1 = key1.Replace('/', '\\').TrimStart('\\').TrimEnd('\\');
-		key2 = key2.Replace('/', '\\').TrimStart('\\').TrimEnd('\\');
-		key3 = key3.Replace('/', '\\').TrimStart('\\').TrimEnd('\\');
+		key1 = key1.Replace('/', '\\').Trim('\\');
+		key2 = key2.Replace('/', '\\').Trim('\\');
+		key3 = key3.Replace('/', '\\').Trim('\\');
 		return $"{key1}\\{key2}\\{key3}";
 	}
 }
