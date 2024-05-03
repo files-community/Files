fbc46fa36017897c1a41f6ce502e943402fa998a
0x5BFA <onein528@outlook.com>
2024-05-03 11:35:28 +0900

    Added comments

--- src/Files.App/Data/Items/ContextFlyoutItemModel.cs
+++ src/Files.App/Data/Items/ContextFlyoutItemModel.cs
@@ -6,54 +6,143 @@ using Microsoft.UI.Xaml.Media.Imaging;
 using System.Windows.Input;
 using Windows.System;
 
-namespace Files.App.Data.Models
+namespace Files.App.Data.Items
 {
+	/// <summary>
+	/// Represents flyout item model for <see cref="MenuFlyoutItem"/> and <see cref="CommandBarFlyout"/>.
+	/// </summary>
 	public sealed class ContextFlyoutItemModel
 	{
+		/// <summary>
+		/// Gets flyout item type.
+		/// </summary>
 		public ContextFlyoutItemType ItemType { get; set; }
 
+		/// <summary>
+		/// Gets command that gets invoked when clicked.
+		/// </summary>
+		/// <remarks>
+		/// Supports <see cref="IRichCommand"/> as well.
+		/// </remarks>
 		public ICommand? Command { get; set; }
 
+		/// <summary>
+		/// Gets command parameter that passed to its command when gets invoked.
+		/// </summary>
 		public object? CommandParameter { get; set; }
 
+		/// <summary>
+		/// Gets the text shown in the item.
+		/// </summary>
 		public string? Text { get; set; }
 
+		/// <summary>
+		/// Gets the icon shown in the item.
+		/// </summary>
 		public BitmapImage? BitmapIcon { get; set; }
 
+		/// <summary>
+		/// Gets the glyph shown in the item.
+		/// </summary>
 		public string? Glyph { get; set; }
 
+		/// <summary>
+		/// Gets the glyph's font family name.
+		/// </summary>
 		public string? GlyphFontFamilyName { get; set; }
 
+		/// <summary>
+		/// Gets the keyboard shortcut that can invoke the item.
+		/// </summary>
 		public KeyboardAccelerator? KeyboardAccelerator { get; set; }
 
+		/// <summary>
+		/// Gets the humanized keyboard shortcut.
+		/// </summary>
 		public string? KeyboardAcceleratorTextOverride { get; set; }
 
+		/// <summary>
+		/// Gets the tag that is handled internally.
+		/// </summary>
 		public object? Tag { get; set; }
 
+		/// <summary>
+		/// Gets the action to load sub menu items.
+		/// </summary>
 		public Func<Task>? LoadSubMenuAction { get; set; }
 
 		public string? ID { get; set; }
 
+		/// <summary>
+		/// Gets the opacity icon shown in the item.
+		/// </summary>
 		public OpacityIconModel? OpacityIcon { get; set; }
 
+		/// <summary>
+		/// Gets the sub menu items.
+		/// </summary>
 		public List<ContextFlyoutItemModel>? Items { get; set; }
 
+		/// <summary>
+		/// Gets the value that indicates whether the item is available in the flyout.
+		/// </summary>
 		public bool IsAvailable { get; set; } = true;
+
+		/// <summary>
+		/// Gets the value that indicates whether the item is visible in the flyout.
+		/// </summary>
 		public bool IsVisible { get; set; } = true;
+
+		/// <summary>
+		/// Gets the value that indicates whether the item type is Checkable and checked.
+		/// </summary>
 		public bool IsChecked { get; set; }
+
+		/// <summary>
+		/// Gets the value that indicates whether the item is enabled and cannot be interacted with.
+		/// </summary>
 		public bool IsEnabled { get; set; } = true;
+
+		/// <summary>
+		/// Gets the value that indicates whether the item is included in the <see cref="CommandBarFlyout.PrimaryCommands"/>.
+		/// </summary>
 		public bool IsPrimary { get; set; }
 
+		/// <summary>
+		/// Gets the value that indicates whether the item is visible when user invoked the flyout with Shift key tapped.
+		/// </summary>
 		public bool ShowOnShift { get; set; }
+
+		/// <summary>
+		/// Gets the value that indicates whether the item is visible in Recycle Bin folders.
+		/// </summary>
 		public bool ShowInRecycleBin { get; set; }
+
+		/// <summary>
+		/// Gets the value that indicates whether the item is visible in Files Search page.
+		/// </summary>
 		public bool ShowInSearchPage { get; set; }
+
+		/// <summary>
+		/// Gets the value that indicates whether the item is visible in FTP file system folders.
+		/// </summary>
 		public bool ShowInFtpPage { get; set; }
+
+		/// <summary>
+		/// Gets the value that indicates whether the item is visible in archive folders.
+		/// </summary>
 		public bool ShowInZipPage { get; set; }
 
 		public bool SingleItemOnly { get; set; }
 
+		/// <summary>
+		/// Gets the value that indicates whether the item's text is visible.
+		/// </summary>
 		public bool CollapseLabel { get; set; }
 
+		/// <summary>
+		/// Gets the value that indicates whether the item is loading.
+		/// </summary>
 		public bool ShowLoadingIndicator { get; set; }
 
 		/// <summary>
