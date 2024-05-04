2b74be5f37164b38f484296fff3677421de2752e
0x5BFA <onein528@outlook.com>
2024-05-04 14:19:40 +0900

    Fix Ioc

--- src/Files.App/Actions/ExecutableContextType.cs
+++ src/Files.App/Actions/ExecutableContextType.cs
@@ -3,14 +3,29 @@
 
 namespace Files.App.Actions
 {
+	/// <summary>
+	/// Defines constant that specifies the <see cref="IRichCommand"/> executable type.
+	/// </summary>
 	public enum ExecutableContextType
 	{
+		/// <summary>
+		/// Unknown executable context type,
+		/// </summary>
 		None,
 
+		/// <summary>
+		/// executable context type is <see cref="IContentPageContext"/>,
+		/// </summary>
 		ContentPageContext,
 
+		/// <summary>
+		/// executable context type is <see cref="IHomePageContext"/>,
+		/// </summary>
 		HomePageContext,
 
+		/// <summary>
+		/// executable context type is <see cref="ISidebarContext"/>,
+		/// </summary>
 		SidebarContext,
 	}
 }
