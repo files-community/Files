731efa371cb1dbab233e721988e7ac4c9b9531ed
0x5BFA <onein528@outlook.com>
2024-05-06 20:39:31 +0900

    Move the compression model classes

--- src/Files.App/Data/Contracts/ICompressArchiveModel.cs
+++ src/Files.App/Data/Contracts/ICompressArchiveModel.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2024 Files Community
 // Licensed under the MIT License. See the LICENSE.
 
-namespace Files.App.Utils.Archives
+namespace Files.App.Data.Contracts
 {
 	/// <summary>
 	/// Represents an interface for archive creation support.
