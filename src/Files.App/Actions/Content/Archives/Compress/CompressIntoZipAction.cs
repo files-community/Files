bd68da0704bf4e83a9c85418fa1743ecc536a744
0x5BFA <onein528@outlook.com>
2024-05-06 18:10:07 +0900

    Init

--- src/Files.App/Actions/Content/Archives/Compress/CompressIntoZipAction.cs
+++ src/Files.App/Actions/Content/Archives/Compress/CompressIntoZipAction.cs
@@ -20,7 +20,22 @@ namespace Files.App.Actions
 			if (context.ShellPage is null)
 				return Task.CompletedTask;
 
-			var (sources, directory, fileName) = CompressHelper.GetCompressDestination(context.ShellPage);
+			string[] sources = context.SelectedItems.Select(item => item.ItemPath).ToArray();
+			string directory = string.Empty;
+			string fileName = string.Empty;
+
+			if (sources.Length is not 0)
+			{
+				// Get the current directory path
+				directory = context.ShellPage.FilesystemViewModel.WorkingDirectory.Normalize();
+
+				// Get the library save folder if the folder is library item
+				if (App.LibraryManager.TryGetLibrary(directory, out var library) && !library.IsEmpty)
+					directory = library.DefaultSaveFolder;
+
+				// Gets the file name from the directory path
+				fileName = SystemIO.Path.GetFileName(sources.Length is 1 ? sources[0] : directory);
+			}
 
 			ICompressArchiveModel creator = new CompressArchiveModel(
 				sources,
---
+++ src/Files.App/Services/Storage/StorageArchiveService.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Files.Shared.Helpers;
+using System.IO;
+using Windows.Storage;
+using Windows.Win32;
+
+namespace Files.App.Services
+{
+	internal class StorageArchiveService
+	{
+		private StatusCenterViewModel StatusCenterViewModel { get; } = Ioc.Default.GetRequiredService<StatusCenterViewModel>();
+
+		public bool CanCompress(IReadOnlyList<ListedItem> items)
+		{
+			return
+				CanDecompress(items) is false ||
+				items.Count > 1;
+		}
+
+		public bool CanDecompress(IReadOnlyList<ListedItem> items)
+		{
+			return
+				items.Any() &&
+				(items.All(x => x.IsArchive) ||
+				items.All(x =>
+					x.PrimaryItemAttribute == StorageItemTypes.File &&
+					FileExtensionHelpers.IsZipFile(x.FileExtension)));
+		}
+
+		public async Task CompressAsync(ICompressArchiveModel creator)
+		{
+			var archivePath = creator.GetArchivePath();
+
+			int index = 1;
+
+			while (File.Exists(archivePath) || Directory.Exists(archivePath))
+				archivePath = creator.GetArchivePath($" ({++index})");
+
+			creator.ArchivePath = archivePath;
+
+			var banner = StatusCenterHelper.AddCard_Compress(
+				creator.Sources,
+				archivePath.CreateEnumerable(),
+				ReturnResult.InProgress,
+				creator.Sources.Count());
+
+			creator.Progress = banner.ProgressEventSource;
+			creator.CancellationToken = banner.CancellationToken;
+
+			bool isSuccess = await creator.RunCreationAsync();
+
+			StatusCenterViewModel.RemoveItem(banner);
+
+			if (isSuccess)
+			{
+				StatusCenterHelper.AddCard_Compress(
+					creator.Sources,
+					archivePath.CreateEnumerable(),
+					ReturnResult.Success,
+					creator.Sources.Count());
+			}
+			else
+			{
+				PInvoke.DeleteFileFromApp(archivePath);
+
+				StatusCenterHelper.AddCard_Compress(
+					creator.Sources,
+					archivePath.CreateEnumerable(),
+					creator.CancellationToken.IsCancellationRequested
+						? ReturnResult.Cancelled
+						: ReturnResult.Failed,
+					creator.Sources.Count());
+			}
+		}
+	}
+}
