5a8e031904801228c60215114cced913a65badc4
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2024-05-13 08:55:50 +0900

    Update SeerProProvider.cs

--- src/Files.App/Services/PreviewPopupProviders/SeerProProvider.cs
+++ src/Files.App/Services/PreviewPopupProviders/SeerProProvider.cs
@@ -72,8 +72,8 @@ namespace Files.App.Services.PreviewPopupProviders
 			var keyName = @"HKEY_CURRENT_USER\Software\Corey\Seer";
 			var value = Registry.GetValue(keyName, "tracking_file", null);
 
-			if (value?.ToString() == "false")
-				return Task.FromResult(false);
+			if (bool.TryParse(value?.ToString(), out var result))
+				return Task.FromResult(result);
 
 			// List of possible paths for the Seer Pro settings file
 			string[] paths =
