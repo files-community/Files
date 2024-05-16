94bbbffe896ec8c1f8022d63d2859e422a324bd4
Yair <39923744+yaira2@users.noreply.github.com>
2024-05-16 15:57:27 -0400

    Update BasePropertiesPage.cs

--- src/Files.App/ViewModels/Properties/BasePropertiesPage.cs
+++ src/Files.App/ViewModels/Properties/BasePropertiesPage.cs
@@ -86,7 +86,7 @@ namespace Files.App.ViewModels.Properties
 				string[] extensions =
 				[
 					"BitmapFiles".GetLocalizedResource(), "*.bmp",
-					"JPEG", "*.jpg;*.jpeg;*.jpe",
+					"JPEG", "*.jpg;*.jpeg",
 					"PNG", "*.png",
 				];
 
