d7e0ac47038c4a0e3cfe276a2992dd0b773a326a
Luan <42214331+Krytan@users.noreply.github.com>
2024-06-05 19:51:48 +0200

    Update RecentFilesWidget.xaml.cs
    License added back

--- src/Files.App/UserControls/Widgets/RecentFilesWidget.xaml.cs
+++ src/Files.App/UserControls/Widgets/RecentFilesWidget.xaml.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Windows.ApplicationModel.DataTransfer;
