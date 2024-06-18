07b9e8f89a9e28332466e248477e3bc36fbcdbf3
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2024-06-18 19:34:05 +0900

    SentryLogger

---
+++ src/Files.App/Utils/Logger/SentryLoggerProvider.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Microsoft.Extensions.Logging;
+
+namespace Files.App.Utils.Logger
+{
+	public sealed class SentryLoggerProvider : ILoggerProvider
+	{
+		public ILogger CreateLogger(string categoryName)
+		{
+			return new SentryLogger();
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
