67d89313bf5996e5461ea8abe7d2ab5484e021ce
Lukáš Laštůvka <lastuvka.lukas3@gmail.com>
2024-06-23 16:56:23 +0200

    Recreate to IIncrementalGenerator

---
+++ src/Files.Core.SourceGenerator/Data/AdditionalTextWithHash.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.IO;
+
+namespace Files.Core.SourceGenerator.Data
+{
+	public readonly record struct AdditionalTextWithHash(AdditionalText File, Guid Hash)
+	{
+		public bool Equals(AdditionalTextWithHash other) => File.Path.Equals(other.File.Path) && Hash.Equals(other.Hash);
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (File.GetHashCode() * 397) ^ Hash.GetHashCode();
+			}
+		}
+
+		public override string ToString() => $"{nameof(File)}: {File?.Path}, {nameof(Hash)}: {Hash}";
+	}
+}
