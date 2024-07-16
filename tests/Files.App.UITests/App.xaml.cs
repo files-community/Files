51066503462bfc5a45ef402ea66e75faaae26b40
Martin Anderson <mdta@live.co.uk>
2024-07-16 12:23:10 +0100

    Added Custom Titlebar to the UITests Window

--- tests/Files.App.UITests/App.xaml.cs
+++ tests/Files.App.UITests/App.xaml.cs
@@ -2,22 +2,6 @@
 // Licensed under the MIT License. See the LICENSE.
 
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 namespace Files.App.UITests
 {
@@ -42,6 +26,9 @@ namespace Files.App.UITests
 		protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
 		{
 			m_window = new MainWindow();
+
+			m_window.ExtendsContentIntoTitleBar = true;
+
 			m_window.Activate();
 		}
 
