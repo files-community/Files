752cdd83306e2f15e49e879d3a5c6f3ab4b5ee13
0x5bfa <62196528+0x5bfa@users.noreply.github.com>
2024-07-26 16:24:58 +0900

    Init

--- tests/Files.App.UITests/MainWindow.xaml.cs
+++ tests/Files.App.UITests/MainWindow.xaml.cs
@@ -1,18 +1,32 @@
 // Copyright (c) 2024 Files Community
 // Licensed under the MIT License. See the LICENSE.
 
+using Files.App.UITests.Views;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace Files.App.UITests
 {
-	/// <summary>
-	/// An empty window that can be used on its own or navigated to within a Frame.
-	/// </summary>
 	public sealed partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
-			this.InitializeComponent();
+			InitializeComponent();
+
+			MainFrame.Navigate(typeof(MainPage));
+		}
+
+		private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+		{
+			if (args.SelectedItem is not NavigationViewItem item || item.Tag is not string tag)
+				return;
+
+			switch (tag)
+			{
+				case "ThemedIconPage":
+					MainFrame.Navigate(typeof(ThemedIconPage));
+					break;
+			}
 		}
 	}
 }
---
+++ tests/Files.App.UITests/Views/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Files.App.UITests.Views
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
