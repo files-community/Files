443a42290e5f351ed1facf879f82e8de227d207a
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2024-07-28 22:46:50 +0900

    Update SortingHelper.cs

--- src/Files.App/Utils/Storage/Collection/SortingHelper.cs
+++ src/Files.App/Utils/Storage/Collection/SortingHelper.cs
@@ -10,7 +10,7 @@ namespace Files.App.Utils.Storage
 		private static object OrderByNameFunc(ListedItem item)
 			=> item.Name;
 
-		public static Func<ListedItem, object>? GetSortFunc(SortOption directorySortOption)
+		public static Func<ListedItem, object> GetSortFunc(SortOption directorySortOption)
 		{
 			return directorySortOption switch
 			{
@@ -24,7 +24,7 @@ namespace Files.App.Utils.Storage
 				SortOption.Path => item => item.ItemPath,
 				SortOption.OriginalFolder => item => (item as RecycleBinItem)?.ItemOriginalFolder,
 				SortOption.DateDeleted => item => (item as RecycleBinItem)?.ItemDateDeletedReal,
-				_ => null,
+				_ => item => item.Name,
 			};
 		}
 
