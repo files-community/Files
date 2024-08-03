cc39610abb06ff739842ca2eacd5b601a31c42d0
Martin Anderson <mdta@live.co.uk>
2024-08-03 18:48:55 +0100

    Added basic ToolbarToggleButton template

--- src/Files.App.Controls/Toolbar/ToolbarItem/ToolbarToggleButton/ToolbarToggleButton.Properties.cs
+++ src/Files.App.Controls/Toolbar/ToolbarItem/ToolbarToggleButton/ToolbarToggleButton.Properties.cs
@@ -15,5 +15,95 @@ namespace Files.App.Controls
 {
 	public partial class ToolbarToggleButton : ToggleButton
 	{
+
+
+		#region Label (string)
+
+		/// <summary>
+		/// The backing <see cref="DependencyProperty"/> for the <see cref="Label"/> property.
+		/// </summary>
+		public static readonly DependencyProperty LabelProperty =
+			DependencyProperty.Register(
+				nameof(Label),
+				typeof(string),
+				typeof(ToolbarToggleButton),
+				new PropertyMetadata(string.Empty, (d, e) => ((ToolbarToggleButton)d).OnLabelPropertyChanged((string)e.OldValue, (string)e.NewValue)));
+
+
+
+		/// <summary>
+		/// Gets or sets the Label as a String
+		/// </summary>
+		public string Label
+		{
+			get => (string)GetValue( LabelProperty );
+			set => SetValue( LabelProperty , value );
+		}
+
+
+
+		protected virtual void OnLabelPropertyChanged(string oldValue , string newValue)
+		{
+			if ( oldValue != newValue )
+			{
+				LabelChanged( newValue );
+			}
+		}
+
+		#endregion
+
+
+
+		#region ThemedIcon (style)
+
+		/// <summary>
+		/// The backing <see cref="DependencyProperty"/> for the <see cref="ThemedIcon"/> property.
+		/// </summary>
+		public static readonly DependencyProperty ThemedIconProperty =
+			DependencyProperty.Register(
+				nameof(ThemedIcon),
+				typeof(Style),
+				typeof(ToolbarToggleButton),
+				new PropertyMetadata(null, (d, e) => ((ToolbarToggleButton)d).OnThemedIconPropertyChanged((Style)e.OldValue, (Style)e.NewValue)));
+
+
+
+		/// <summary>
+		/// Gets or sets the Style value for the item's ThemedIcon
+		/// </summary>
+		public Style ThemedIcon
+		{
+			get => (Style)GetValue( ThemedIconProperty );
+			set => SetValue( ThemedIconProperty , value );
+		}
+
+
+
+		protected virtual void OnThemedIconPropertyChanged(Style oldValue , Style newValue)
+		{
+			if ( newValue != oldValue )
+			{
+				ThemedIconChanged( newValue );
+			}
+		}
+
+		#endregion
+
+
+
+		#region ButtonBase Events
+
+		/// <inheritdoc/>
+		protected override void OnContentChanged(object oldContent , object newContent)
+		{
+			if ( newContent != oldContent )
+			{
+				ContentChanged( newContent );
+				base.OnContentChanged( oldContent , newContent );
+			}
+		}
+
+		#endregion
+
 	}
 }
