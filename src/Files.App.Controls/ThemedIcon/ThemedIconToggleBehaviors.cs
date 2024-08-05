6ef0428561df9c6bf1273c694a7f7c5e3c851ab6
Martin Anderson <mdta@live.co.uk>
2024-08-05 20:20:44 +0100

    Changed IsToggled to ToggleBehavior

---
+++ src/Files.App.Controls/ThemedIcon/ThemedIconToggleBehaviors.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+namespace Files.App.Controls
+{
+	/// <summary>
+	/// Defines IconTypes for <see cref="ThemedIcon"/>.
+	/// </summary>
+	public enum ToggleBehaviors
+	{
+		/// <summary>
+		/// Toggle Behavior type of <see cref="ThemedIcon"/> is Auto.
+		/// </summary>
+		Auto,
+
+		/// <summary>
+		/// Toggle Behavior type of <see cref="ThemedIcon"/> is On.
+		/// </summary>
+		On,
+
+		/// <summary>
+		/// Toggle Behavior type of <see cref="ThemedIcon"/> is Off.
+		/// </summary>
+		Off,
+	}
+}
