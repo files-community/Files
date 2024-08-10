c3537ab961105655c4bffe41b64fa81443d86123
0x5BFA <onein528@outlook.com>
2024-08-10 12:28:10 +0900

    Brush up

--- tests/Files.App.UITests/Views/MainPage.xaml.cs
+++ tests/Files.App.UITests/Views/MainPage.xaml.cs
@@ -6,9 +6,6 @@ using Microsoft.UI.Xaml.Controls;
 
 namespace Files.App.UITests.Views
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
 	public sealed partial class MainPage : Page
 	{
 		public MainPage()
