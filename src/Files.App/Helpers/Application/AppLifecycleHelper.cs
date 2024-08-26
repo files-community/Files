46cbf0267488baa7cfd3f5cc9c6613fe2ad3e50a
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2024-08-26 09:30:03 +0900

    Revert "Code Quality: Ignore exceptions in AppLifecycleHelper.HandleAppUnhandledException (#15958)"
    This reverts commit 7f63febdf1aabf3d2f092674028b0c933da1e70a.

--- src/Files.App/Helpers/Application/AppLifecycleHelper.cs
+++ src/Files.App/Helpers/Application/AppLifecycleHelper.cs
@@ -252,99 +252,94 @@ namespace Files.App.Helpers
 		/// </summary>
 		public static void HandleAppUnhandledException(Exception? ex, bool showToastNotification)
 		{
-			SafetyExtensions.IgnoreExceptions(() =>
+			var generalSettingsService = Ioc.Default.GetRequiredService<IGeneralSettingsService>();
+
+			StringBuilder formattedException = new()
 			{
-				var generalSettingsService = Ioc.Default.GetRequiredService<IGeneralSettingsService>();
+				Capacity = 200
+			};
 
-				StringBuilder formattedException = new()
+			formattedException.AppendLine("--------- UNHANDLED EXCEPTION ---------");
+
+			if (ex is not null)
+			{
+				ex.Data[Mechanism.HandledKey] = false;
+				ex.Data[Mechanism.MechanismKey] = "Application.UnhandledException";
+
+				SentrySdk.CaptureException(ex, scope =>
 				{
-					Capacity = 200
-				};
+					scope.User.Id = generalSettingsService?.UserId;
+					scope.Level = SentryLevel.Fatal;
+				});
 
-				formattedException.AppendLine("--------- UNHANDLED EXCEPTION ---------");
+				formattedException.AppendLine($">>>> HRESULT: {ex.HResult}");
 
-				if (ex is not null)
+				if (ex.Message is not null)
 				{
-					ex.Data[Mechanism.HandledKey] = false;
-					ex.Data[Mechanism.MechanismKey] = "Application.UnhandledException";
-
-					SentrySdk.CaptureException(ex, scope =>
-					{
-						scope.User.Id = generalSettingsService?.UserId;
-						scope.Level = SentryLevel.Fatal;
-					});
-
-					formattedException.AppendLine($">>>> HRESULT: {ex.HResult}");
-
-					if (ex.Message is not null)
-					{
-						formattedException.AppendLine("--- MESSAGE ---");
-						formattedException.AppendLine(ex.Message);
-					}
-					if (ex.StackTrace is not null)
-					{
-						formattedException.AppendLine("--- STACKTRACE ---");
-						formattedException.AppendLine(ex.StackTrace);
-					}
-					if (ex.Source is not null)
-					{
-						formattedException.AppendLine("--- SOURCE ---");
-						formattedException.AppendLine(ex.Source);
-					}
-					if (ex.InnerException is not null)
-					{
-						formattedException.AppendLine("--- INNER ---");
-						formattedException.AppendLine(ex.InnerException.ToString());
-					}
+					formattedException.AppendLine("--- MESSAGE ---");
+					formattedException.AppendLine(ex.Message);
 				}
-				else
+				if (ex.StackTrace is not null)
 				{
-					formattedException.AppendLine("Exception data is not available.");
+					formattedException.AppendLine("--- STACKTRACE ---");
+					formattedException.AppendLine(ex.StackTrace);
 				}
+				if (ex.Source is not null)
+				{
+					formattedException.AppendLine("--- SOURCE ---");
+					formattedException.AppendLine(ex.Source);
+				}
+				if (ex.InnerException is not null)
+				{
+					formattedException.AppendLine("--- INNER ---");
+					formattedException.AppendLine(ex.InnerException.ToString());
+				}
+			}
+			else
+			{
+				formattedException.AppendLine("Exception data is not available.");
+			}
 
-				formattedException.AppendLine("---------------------------------------");
+			formattedException.AppendLine("---------------------------------------");
 
-				Debug.WriteLine(formattedException.ToString());
+			Debug.WriteLine(formattedException.ToString());
 
-				// Please check "Output Window" for exception details (View -> Output Window) (CTRL + ALT + O)
-				Debugger.Break();
-
-				App.Logger?.LogError(ex, ex?.Message ?? "An unhandled error occurred.");
-			});
+			// Please check "Output Window" for exception details (View -> Output Window) (CTRL + ALT + O)
+			Debugger.Break();
 
 			// Save the current tab list in case it was overwriten by another instance
-			SafetyExtensions.IgnoreExceptions(SaveSessionTabs);
+			SaveSessionTabs();
+			App.Logger?.LogError(ex, ex?.Message ?? "An unhandled error occurred.");
 
 			if (!showToastNotification)
 				return;
 
-			SafetyExtensions.IgnoreExceptions(AppToastNotificationHelper.ShowUnhandledExceptionToast);
-
 			SafetyExtensions.IgnoreExceptions(() =>
 			{
-				// Restart the app
-				var userSettingsService = Ioc.Default.GetRequiredService<IUserSettingsService>();
-				var lastSessionTabList = userSettingsService.GeneralSettingsService.LastSessionTabList;
-
-				if (userSettingsService.GeneralSettingsService.LastCrashedTabList?.SequenceEqual(lastSessionTabList) ?? false)
-				{
-					// Avoid infinite restart loop
-					userSettingsService.GeneralSettingsService.LastSessionTabList = null;
-				}
-				else
-				{
-					userSettingsService.AppSettingsService.RestoreTabsOnStartup = true;
-					userSettingsService.GeneralSettingsService.LastCrashedTabList = lastSessionTabList;
-
-					// Try to re-launch and start over
-					MainWindow.Instance.DispatcherQueue.EnqueueOrInvokeAsync(async () =>
-					{
-						await Launcher.LaunchUriAsync(new Uri("files-uwp:"));
-					})
-					.Wait(100);
-				}
+				AppToastNotificationHelper.ShowUnhandledExceptionToast();
 			});
 
+			// Restart the app
+			var userSettingsService = Ioc.Default.GetRequiredService<IUserSettingsService>();
+			var lastSessionTabList = userSettingsService.GeneralSettingsService.LastSessionTabList;
+
+			if (userSettingsService.GeneralSettingsService.LastCrashedTabList?.SequenceEqual(lastSessionTabList) ?? false)
+			{
+				// Avoid infinite restart loop
+				userSettingsService.GeneralSettingsService.LastSessionTabList = null;
+			}
+			else
+			{
+				userSettingsService.AppSettingsService.RestoreTabsOnStartup = true;
+				userSettingsService.GeneralSettingsService.LastCrashedTabList = lastSessionTabList;
+
+				// Try to re-launch and start over
+				MainWindow.Instance.DispatcherQueue.EnqueueOrInvokeAsync(async () =>
+				{
+					await Launcher.LaunchUriAsync(new Uri("files-uwp:"));
+				})
+				.Wait(100);
+			}
 			Process.GetCurrentProcess().Kill();
 		}
 
