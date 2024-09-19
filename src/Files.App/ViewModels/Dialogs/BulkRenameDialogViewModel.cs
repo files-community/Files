19e01c76fbc03a46a43845810124fcf879b867f1
Yair <39923744+yaira2@users.noreply.github.com>
2024-09-19 13:05:35 -0400

    Finished

--- src/Files.App/ViewModels/Dialogs/BulkRenameDialogViewModel.cs
+++ src/Files.App/ViewModels/Dialogs/BulkRenameDialogViewModel.cs
@@ -1,26 +1,23 @@
 ﻿// Copyright (c) 2024 Files Community
 // Licensed under the MIT License. See the LICENSE.
 
-using System.IO;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Windows.Input;
-using Windows.Storage.Pickers;
-using Files.Shared.Helpers;
+using Windows.Storage;
 
 namespace Files.App.ViewModels.Dialogs
 {
 	public sealed class BulkRenameDialogViewModel : ObservableObject
 	{
+		private IContentPageContext context { get; } = Ioc.Default.GetRequiredService<IContentPageContext>();
+
 		// Properties
 
 		public bool IsNameValid =>
-			FilesystemHelpers.IsValidForFilename(fileName);
-		
+			FilesystemHelpers.IsValidForFilename(fileName) && !fileName.Contains(".");
+
 		public bool ShowNameWarning =>
 			!string.IsNullOrEmpty(fileName) && !IsNameValid;
 
-		
+
 		private string fileName = string.Empty;
 		public string FileName
 		{
@@ -35,9 +32,31 @@ namespace Files.App.ViewModels.Dialogs
 			}
 		}
 
+		// Commands
+		public IAsyncRelayCommand CommitRenameCommand { get; private set; }
+
 		public BulkRenameDialogViewModel()
 		{
-			
+			CommitRenameCommand = new AsyncRelayCommand(DoCommitRenameAsync);
+		}
+
+		private async Task DoCommitRenameAsync()
+		{
+			if (context.ShellPage is null)
+				return;
+
+			await Task.WhenAll(context.SelectedItems.Select(item =>
+			{
+				var itemType = item.PrimaryItemAttribute == StorageItemTypes.Folder ? FilesystemItemType.Directory : FilesystemItemType.File;
+				return context.ShellPage.FilesystemHelpers.RenameAsync(
+					StorageHelpers.FromPathAndType(item.ItemPath, itemType),
+					fileName + item.FileExtension,
+					NameCollisionOption.GenerateUniqueName,
+					true,
+					false
+				);
+			}));
+
 		}
 	}
 }
