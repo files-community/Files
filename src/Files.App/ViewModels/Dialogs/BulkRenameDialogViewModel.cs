c4c5fdc5372ff36953a1443f20dfa51653dcadbb
Yair <39923744+yaira2@users.noreply.github.com>
2024-09-19 11:55:47 -0400

    1

---
+++ src/Files.App/ViewModels/Dialogs/BulkRenameDialogViewModel.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Input;
+using Windows.Storage.Pickers;
+using Files.Shared.Helpers;
+
+namespace Files.App.ViewModels.Dialogs
+{
+	public sealed class BulkRenameDialogViewModel : ObservableObject
+	{
+		public BulkRenameDialogViewModel()
+		{
+			
+		}
+	}
+}
