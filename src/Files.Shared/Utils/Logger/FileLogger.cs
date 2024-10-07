df9fac6093debdd6a0fd15cedf8cd10ce3fa8a90
hishitetsu <66369541+hishitetsu@users.noreply.github.com>
2024-10-08 00:41:50 +0900

    Code Quality: Use lock statements in FileLogger (#16311)

--- src/Files.Shared/Utils/Logger/FileLogger.cs
+++ src/Files.Shared/Utils/Logger/FileLogger.cs
@@ -6,13 +6,12 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading;
 
 namespace Files.Shared
 {
 	public sealed class FileLogger : ILogger
 	{
-		private readonly SemaphoreSlim semaphoreSlim = new(1);
+		private readonly object syncRoot = new();
 		private readonly string filePath;
 
 		public FileLogger(string filePath)
@@ -34,21 +33,19 @@ namespace Files.Shared
 		{
 			if (formatter is null)
 				return;
-			semaphoreSlim.Wait();
 
 			try
 			{
 				var message = exception?.ToString() ?? formatter(state, exception);
 
-				File.AppendAllText(filePath, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffff}|{logLevel}|{message}" + Environment.NewLine);
+				lock (syncRoot)
+				{
+					File.AppendAllText(filePath, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss.ffff}|{logLevel}|{message}" + Environment.NewLine);
+				}
 			}
 			catch (Exception e)
 			{
 				Debug.WriteLine($"Writing to log file failed with the following exception:\n{e}");
-			}
-			finally
-			{
-				semaphoreSlim.Release();
 			}
 		}
 
@@ -57,24 +54,21 @@ namespace Files.Shared
 			if (!File.Exists(filePath))
 				return;
 
-			semaphoreSlim.Wait();
-
 			try
 			{
-				var lines = File.ReadAllLines(filePath);
-				if (lines.Length > numberOfLinesKept)
+				lock (syncRoot)
 				{
-					var lastLines = lines.Skip(Math.Max(0, lines.Length - numberOfLinesKept));
-					File.WriteAllLines(filePath, lastLines);
+					var lines = File.ReadAllLines(filePath);
+					if (lines.Length > numberOfLinesKept)
+					{
+						var lastLines = lines.Skip(Math.Max(0, lines.Length - numberOfLinesKept));
+						File.WriteAllLines(filePath, lastLines);
+					}
 				}
 			}
 			catch (Exception e)
 			{
 				Debug.WriteLine($"Purging the log file failed with the following exception:\n{e}");
-			}
-			finally
-			{
-				semaphoreSlim.Release();
 			}
 		}
 	}
