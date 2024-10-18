95db974993f325327bc8e28c7090091575d8e35e
Lamparter <notlamparter@outlook.com>
2024-10-18 18:25:00 +0100

    Revert changes to `Files.Core.Storage` global using directives

--- src/Files.Core.Storage/GlobalUsings.cs
+++ src/Files.Core.Storage/GlobalUsings.cs
@@ -2,13 +2,13 @@
 // Licensed under the MIT License. See the LICENSE.
 
 // System
-global using global::Files.Core.Storage.Enums;
-global using global::Files.Core.Storage.EventArguments;
-// Files.Core.Storage
-
-global using global::Files.Core.Storage.Storables;
 global using global::System;
 global using global::System.Collections.Generic;
 global using global::System.Threading;
 global using global::System.Threading.Tasks;
 global using SystemIO = global::System.IO;
+
+// Files.Core.Storage
+global using global::Files.Core.Storage.Enums;
+global using global::Files.Core.Storage.EventArguments;
+global using global::Files.Core.Storage.Storables;
