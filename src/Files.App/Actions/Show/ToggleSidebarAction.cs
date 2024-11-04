2450492116150468dbbc2ddee1f8defeedb5eb4c
Gness Erquint <gness.na@gmail.com>
2024-11-04 22:18:32 +0300

    Revert to ef98d2e841bdbd3154605aa1227576819b206b0f.

--- src/Files.App/Actions/Show/ToggleSidebarAction.cs
+++ src/Files.App/Actions/Show/ToggleSidebarAction.cs
@@ -1,13 +1,11 @@
 ﻿// Copyright (c) 2024 Files Community
 // Licensed under the MIT License. See the LICENSE.
 
-using Files.App.UserControls.Sidebar;
-
 namespace Files.App.Actions
 {
-	internal sealed class ToggleSidebarAction : ObservableObject, IToggleAction
+	internal sealed class ToggleSidebarAction : IToggleAction
 	{
-		private readonly SidebarViewModel SidebarViewModel = Ioc.Default.GetRequiredService<SidebarViewModel>();
+		private IAppearanceSettingsService AppearanceSettingsService { get; } = Ioc.Default.GetRequiredService<IAppearanceSettingsService>();
 
 		public string Label
 			=> "ToggleSidebar".GetLocalizedResource();
@@ -18,27 +16,24 @@ namespace Files.App.Actions
 		public HotKey HotKey
 			=> new(Keys.S, KeyModifiers.CtrlAlt);
 
-		public bool IsOn =>
-			SidebarViewModel.SidebarDisplayMode is SidebarDisplayMode.Expanded
-				? true
-				: false;
+		public bool IsOn
+			=> AppearanceSettingsService.IsSidebarOpen;
 
 		public ToggleSidebarAction()
 		{
-			SidebarViewModel.PropertyChanged += ViewModel_PropertyChanged;
+			AppearanceSettingsService.PropertyChanged += ViewModel_PropertyChanged;
 		}
 
 		public Task ExecuteAsync(object? parameter = null)
 		{
-			SidebarViewModel.SidebarDisplayMode = !IsOn
-				? SidebarDisplayMode.Expanded
-				: SidebarDisplayMode.Compact;
+			AppearanceSettingsService.IsSidebarOpen = !IsOn;
+
 			return Task.CompletedTask;
 		}
 
 		private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
 		{
-			if (e.PropertyName is nameof(SidebarViewModel.SidebarDisplayMode))
+			if (e.PropertyName is nameof(AppearanceSettingsService.IsSidebarOpen))
 				OnPropertyChanged(nameof(IsOn));
 		}
 	}
