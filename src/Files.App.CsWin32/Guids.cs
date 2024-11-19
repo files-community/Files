8946c43ba736f7d17f506f482b838da9e22a5701
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-11-19 14:40:32 +0000

    Use GuidRVAGen for Guid*

---
+++ src/Files.App.CsWin32/Guids.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+
+namespace Windows.Win32
+{
+	public static unsafe partial class PInvoke
+	{
+		[GuidRVAGen.Guid("B92B56A9-8B55-4E14-9A89-0199BBB6F93B")]
+		public static partial Guid* IID_DesktopWallpaper { get; }
+
+		[GuidRVAGen.Guid("B92B56A9-8B55-4E14-9A89-0199BBB6F93B")]
+		public static partial Guid* CLSID_DesktopWallpaper { get; }
+	}
+}
