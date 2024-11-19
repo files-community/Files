dd214506d5583955d7a18cefc2371488c5889f42
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2024-11-19 14:54:22 +0000

    Separate the PInvoke classe

--- src/Files.App.CsWin32/Guids.cs
+++ src/Files.App.CsWin32/Guids.cs
@@ -1,17 +1,28 @@
 ﻿// Copyright (c) 2024 Files Community
 // Licensed under the MIT License. See the LICENSE.
 
-using System.Runtime.InteropServices;
-using Windows.Win32.Foundation;
+using System;
 
 namespace Windows.Win32
 {
+	// CLSIDs
+	public static unsafe partial class PInvoke
+	{
+		[GuidRVAGen.Guid("C2CF3110-460E-4FC1-B9D0-8A1C0C9CC4BD")]
+		public static partial Guid* CLSID_DesktopWallpaper { get; }
+	}
+
+	// IIDs
 	public static unsafe partial class PInvoke
 	{
 		[GuidRVAGen.Guid("B92B56A9-8B55-4E14-9A89-0199BBB6F93B")]
-		public static partial Guid* IID_DesktopWallpaper { get; }
+		public static partial Guid* IID_IDesktopWallpaper { get; }
+	}
 
-		[GuidRVAGen.Guid("B92B56A9-8B55-4E14-9A89-0199BBB6F93B")]
-		public static partial Guid* CLSID_DesktopWallpaper { get; }
+	// Misc
+	public static unsafe partial class PInvoke
+	{
+		[GuidRVAGen.Guid("94f60519-2850-4924-aa5a-d15e84868039")]
+		public static partial Guid* BHID_EnumItems { get; }
 	}
 }
