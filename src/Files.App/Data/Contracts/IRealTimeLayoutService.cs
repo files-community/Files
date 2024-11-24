d515296ffcb11fbecb8808ccf52718494de4d4fa
Lukáš Laštůvka <lastuvka.lukas3@gmail.com>
2024-11-24 17:56:58 +0100

    CQ: Add licence header

--- src/Files.App/Data/Contracts/IRealTimeLayoutService.cs
+++ src/Files.App/Data/Contracts/IRealTimeLayoutService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.UI.Xaml;
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Microsoft.UI.Xaml;
 using System.Globalization;
 
 namespace Files.App.Data.Contracts
--- src/Files.App/Services/Content/RealTimeLayoutService.cs
+++ src/Files.App/Services/Content/RealTimeLayoutService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.UI.Xaml;
+﻿// Copyright (c) 2024 Files Community
+// Licensed under the MIT License. See the LICENSE.
+
+using Microsoft.UI.Xaml;
 using System.Globalization;
 using Windows.Win32;
 using Windows.Win32.Foundation;
