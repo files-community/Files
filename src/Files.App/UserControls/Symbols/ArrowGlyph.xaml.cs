37e34a963f2bd3ca07ec3bdccab03e0376b7a2b2
Lukáš Laštůvka <lastuvka.lukas3@gmail.com>
2024-11-27 01:48:32 +0100

    CQ: Add for Animated Icon

--- src/Files.App/UserControls/Symbols/ArrowGlyph.xaml.cs
+++ src/Files.App/UserControls/Symbols/ArrowGlyph.xaml.cs
@@ -1,26 +1,33 @@
-﻿using Microsoft.UI;
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.AnimatedVisuals;
 using Microsoft.UI.Xaml.Media;
+using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Files.App.UserControls.Symbols
 {
 	[DependencyProperty<double>("FontSize", DefaultValue = "(double)12.0")]
-	[DependencyProperty<Brush>("Color", nameof(OnColorChange), DefaultValue = "null")]
+	[DependencyProperty<Brush>("Foreground", nameof(OnColorChange), DefaultValue = "null")]
 	[DependencyProperty<ArrowSymbolType>("Arrow", nameof(OnArrowChange), DefaultValue = "Files.App.Data.Enums.ArrowSymbolType.ChevronRight")]
+	[DependencyProperty<bool>("UseAnimatedIcon", nameof(OnUseAnimatedIconChanged), DefaultValue = "false")]
 	[DependencyProperty<string>("Glyph", DefaultValue = "null")]
-	public sealed partial class ArrowGlyph : UserControl, IRealTimeControl
+	public partial class ArrowGlyph : UserControl, INotifyPropertyChanged, IRealTimeControl
 	{
 		private static ICommandManager Commands { get; } = Ioc.Default.GetRequiredService<ICommandManager>();
 
 		private string ForwardGlyph { get; } = Commands.NavigateForward.Glyph.BaseGlyph;
-
 		private string BackGlyph { get; } = Commands.NavigateBack.Glyph.BaseGlyph;
-
 		private string ChevronLeft { get; } = "\uE76B";
 		private string ChevronRight { get; } = "\uE76C";
 
 		private long _token;
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public bool IsStaticIconVisible => !UseAnimatedIcon;
+
+		public bool IsAnimatedIconVisible => UseAnimatedIcon;
 
 		public ArrowGlyph()
 		{
@@ -30,8 +37,25 @@ namespace Files.App.UserControls.Symbols
 			RealTimeLayoutService.AddCallback(this, UpdateGlyph);
 		}
 
+
+		protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
 		private void UpdateGlyph()
 		{
+			if (UseAnimatedIcon)
+			{
+				SymbolGrid.Rotation = Arrow switch
+				{
+					ArrowSymbolType.Forward => FlowDirection == FlowDirection.LeftToRight ? 180 : 0,
+					ArrowSymbolType.Back => FlowDirection == FlowDirection.LeftToRight ? 0 : 180,
+					_ => 0
+				};
+				return;
+			}
+
 			Glyph = Arrow switch
 			{
 				ArrowSymbolType.Forward => FlowDirection == FlowDirection.LeftToRight ? ForwardGlyph : BackGlyph,
@@ -55,6 +79,26 @@ namespace Files.App.UserControls.Symbols
 		{
 			if (oldValue != newValue)
 				UpdateGlyph();
+		}
+
+		private void OnUseAnimatedIconChanged(bool oldValue, bool newValue)
+		{
+			if (oldValue != newValue)
+			{
+				UpdateGlyph();
+				OnPropertyChanged(nameof(IsStaticIconVisible));
+				OnPropertyChanged(nameof(IsAnimatedIconVisible));
+			}
+		}
+
+		private void SymbolGrid_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (UseAnimatedIcon)
+			{
+				var centerX = SymbolGrid.ActualWidth / 2;
+				var centerY = SymbolGrid.ActualHeight / 2;
+				SymbolGrid.CenterPoint = new Vector3((float)centerX, (float)centerY, 0);
+			}
 		}
 	}
 
