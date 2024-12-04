5b5541dfa827aee802e214fb2fe6f41f49b982e0
d2dyno <53011783+d2dyno1@users.noreply.github.com>
2024-12-04 21:58:42 +0100

    Feature: Added 'compress' attribute option to the properties window (#16319)

--- src/Files.App/Helpers/Win32/Win32PInvoke.Consts.cs
+++ src/Files.App/Helpers/Win32/Win32PInvoke.Consts.cs
@@ -22,19 +22,14 @@ namespace Files.App.Helpers
 		public const int FILE_NOTIFY_CHANGE_SECURITY = 256;
 
 		public const int INVALID_HANDLE_VALUE = -1;
-		public const uint GENERIC_READ = 0x80000000;
-		public const uint GENERIC_WRITE = 0x40000000;
 		public const int FILE_SHARE_READ = 0x00000001;
 		public const int FILE_SHARE_WRITE = 0x00000002;
 		public const uint FILE_SHARE_DELETE = 0x00000004;
 		public const int OPEN_EXISTING = 3;
-		public const int FSCTL_LOCK_VOLUME = 0x00090018;
-		public const int FSCTL_DISMOUNT_VOLUME = 0x00090020;
 		public const int IOCTL_STORAGE_EJECT_MEDIA = 0x2D4808;
 		public const int IOCTL_STORAGE_MEDIA_REMOVAL = 0x002D4804;
 
 		public const uint FILE_APPEND_DATA = 0x0004;
-		public const uint FILE_WRITE_ATTRIBUTES = 0x100;
 
 
 		public const uint FILE_BEGIN = 0;
@@ -45,8 +40,10 @@ namespace Files.App.Helpers
 		public const uint OPEN_ALWAYS = 4;
 		public const uint TRUNCATE_EXISTING = 5;
 
-		public const int MAXIMUM_REPARSE_DATA_BUFFER_SIZE = 16 * 1024;
+		// FSCTL
 		public const int FSCTL_GET_REPARSE_POINT = 0x000900A8;
+
+		public const int MAXIMUM_REPARSE_DATA_BUFFER_SIZE = 16 * 1024;
 		public const uint IO_REPARSE_TAG_MOUNT_POINT = 0xA0000003;
 		public const uint IO_REPARSE_TAG_SYMLINK = 0xA000000C;
 
