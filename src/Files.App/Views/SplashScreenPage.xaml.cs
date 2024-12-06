c65b5e5103a00b089be6a557ffca95a2696999a2
Yair <39923744+yaira2@users.noreply.github.com>
2024-12-05 22:44:09 -0500

    Update SplashScreenPage.xaml.cs

--- src/Files.App/Views/SplashScreenPage.xaml.cs
+++ src/Files.App/Views/SplashScreenPage.xaml.cs
@@ -15,8 +15,7 @@ namespace Files.App.Views
 			AppLifecycleHelper.AppEnvironment switch
 			{
 				AppEnvironment.Dev => "Dev",
-				AppEnvironment.StorePreview => "Preview",
-				AppEnvironment.SideloadPreview => "Preview",
+				AppEnvironment.SideloadPreview or AppEnvironment.StorePreview => "Preview",
 				_ => string.Empty,
 			};
 
