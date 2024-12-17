6089e6f4c484fdbdbb33184c311d1947e30d34f8
Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>
2024-12-17 22:02:15 +0100

    Fix: Fixed an issue where tags would sometimes display with the wrong color (#16603)

--- src/Files.App.Controls/ThemedIcon/ThemedIcon.Properties.cs
+++ src/Files.App.Controls/ThemedIcon/ThemedIcon.Properties.cs
@@ -32,6 +32,7 @@ namespace Files.App.Controls
 		protected virtual void OnColorPropertyChanged(Brush oldValue, Brush newValue)
 		{
 			OnIconTypeChanged();
+			OnIconColorChanged();
 		}
 
 		protected virtual void OnIconTypePropertyChanged(ThemedIconTypes oldValue, ThemedIconTypes newValue)
