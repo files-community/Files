285e3dbf45b7bf4eae0f17f5142e5023f830bb1a
Yair <39923744+yaira2@users.noreply.github.com>
2025-01-01 11:07:11 -0500

    Revert "Update ToolbarFlyoutOpeningEventArgs.cs"
    This reverts commit 491c85513c99435abe16ad4d29b2cf8543ea34fa.

--- src/Files.App/Data/EventArguments/ToolbarFlyoutOpeningEventArgs.cs
+++ src/Files.App/Data/EventArguments/ToolbarFlyoutOpeningEventArgs.cs
@@ -7,6 +7,6 @@ namespace Files.App.Data.EventArguments
 {
 	public sealed class ToolbarFlyoutOpeningEventArgs
 	{
-		public MenuFlyout OpeningFlyout { get; }
+		public MenuFlyout OpeningFlyout { get; set; }
 	}
 }
