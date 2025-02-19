a3193eecdbc6802d58b61596a8809b215e98bc90
Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>
2025-02-19 22:23:10 +0100

    Suggested UI Changes

--- src/Files.App/Views/Settings/DevToolsPage.xaml.cs
+++ src/Files.App/Views/Settings/DevToolsPage.xaml.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Files Community
 // Licensed under the MIT License.
 
+using Files.App.ViewModels.Settings;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
@@ -11,6 +12,13 @@ namespace Files.App.Views.Settings
 		public DevToolsPage()
 		{
 			InitializeComponent();
+			ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+		}
+
+		private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName is nameof(DevToolsViewModel.IsEditingIDEConfig))
+				SetVisualState(ActualWidth);
 		}
 
 		private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -18,11 +26,15 @@ namespace Files.App.Views.Settings
 			if (e.NewSize.Width == e.PreviousSize.Width)
 				return;
 
+			SetVisualState(e.NewSize.Width);
+		}
+
+		private void SetVisualState(double width)
+		{
 			var defaultPathWidth = 300;
 			var pickIDEWidth = PickIDEExe.ActualWidth == 0 ? 64 : PickIDEExe.ActualWidth;
-
 			var minWidth = defaultPathWidth + pickIDEWidth;
-			var state = minWidth > e.NewSize.Width / 1.6 ? "CompactState" : "DefaultState";
+			var state = minWidth > width / 1.6 ? "CompactState" : "DefaultState";
 			VisualStateManager.GoToState(this, state, false);
 		}
 	}
