1f86e2fc61a072507370d8e25d4687cd28915527
Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>
2025-02-25 23:20:54 +0100

    Use State triggers

--- src/Files.App/Views/Settings/DevToolsPage.xaml.cs
+++ src/Files.App/Views/Settings/DevToolsPage.xaml.cs
@@ -1,8 +1,6 @@
 // Copyright (c) Files Community
 // Licensed under the MIT License.
 
-using Files.App.ViewModels.Settings;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace Files.App.Views.Settings
@@ -12,30 +10,6 @@ namespace Files.App.Views.Settings
 		public DevToolsPage()
 		{
 			InitializeComponent();
-			ViewModel.PropertyChanged += ViewModel_PropertyChanged;
-		}
-
-		private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName is nameof(DevToolsViewModel.IsEditingIDEConfig))
-				SetVisualState(ActualWidth);
-		}
-
-		private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
-		{
-			if (e.NewSize.Width == e.PreviousSize.Width)
-				return;
-
-			SetVisualState(e.NewSize.Width);
-		}
-
-		private void SetVisualState(double width)
-		{
-			var defaultPathWidth = 300;
-			var pickIDEWidth = PickIDEExe.ActualWidth == 0 ? 64 : PickIDEExe.ActualWidth;
-			var minWidth = defaultPathWidth + pickIDEWidth;
-			var state = minWidth > width / 1.6 ? "CompactState" : "DefaultState";
-			VisualStateManager.GoToState(this, state, false);
 		}
 	}
 }
