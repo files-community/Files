75c8448c4e7f733ac73b25777c6b7d5a88ea2a7a
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-03-12 23:41:08 +0900

    Code Quality: Replaced DP generator with WCT (#16922)

--- src/Files.App.Controls/ThemedIcon/ThemedIconLayer/ThemedIconLayer.Properties.cs
+++ src/Files.App.Controls/ThemedIcon/ThemedIconLayer/ThemedIconLayer.Properties.cs
@@ -1,38 +1,49 @@
 ﻿// Copyright (c) Files Community
 // Licensed under the MIT License.
 
+using CommunityToolkit.WinUI;
 using Microsoft.UI.Xaml.Media;
 
 namespace Files.App.Controls
 {
-	[DependencyProperty<ThemedIconLayerType>("LayerType", nameof(OnLayerTypePropertyChanged), DefaultValue = "ThemedIconLayerType.Base")]
-	[DependencyProperty<string>("PathData", nameof(OnLayerPathDataPropertyChanged), DefaultValue = "string.Empty")]
-	[DependencyProperty<double>("LayerSize", nameof(OnLayerSizePropertyChanged), DefaultValue = "(double)16")]
-	[DependencyProperty<Brush>("LayerColor", nameof(OnColorPropertyChanged))]
-	[DependencyProperty<ThemedIconColorType>("IconColorType", nameof(OnIconColorTypePropertyChanged), DefaultValue = "ThemedIconColorType.Normal")]
 	public partial class ThemedIconLayer
 	{
-		protected virtual void OnLayerTypePropertyChanged(ThemedIconLayerType oldValue, ThemedIconLayerType newValue)
+		[GeneratedDependencyProperty(DefaultValue = ThemedIconLayerType.Base)]
+		public partial ThemedIconLayerType LayerType { get; set; }
+
+		[GeneratedDependencyProperty(DefaultValue = "")]
+		public partial string PathData { get; set; }
+
+		[GeneratedDependencyProperty(DefaultValue = 16.0d)]
+		public partial double LayerSize { get; set; }
+
+		[GeneratedDependencyProperty]
+		public partial Brush LayerColor { get; set; }
+
+		[GeneratedDependencyProperty(DefaultValue = ThemedIconColorType.Normal)]
+		public partial ThemedIconColorType IconColorType { get; set; }
+
+		partial void OnLayerTypeChanged(ThemedIconLayerType newValue)
 		{
 			LayerTypeChanged(newValue);
 		}
 
-		protected virtual void OnLayerPathDataPropertyChanged(string oldValue, string newValue)
+		partial void OnPathDataChanged(string newValue)
 		{
 			LayerPathDataChanged(newValue);
 		}
 
-		protected virtual void OnLayerSizePropertyChanged(double oldValue, double newValue)
+		partial void OnLayerSizeChanged(double newValue)
 		{
 			LayerSizePropertyChanged(newValue);
 		}
 
-		protected virtual void OnColorPropertyChanged(Brush oldValue, Brush newValue)
+		partial void OnLayerColorChanged(Brush newValue)
 		{
 			IconColorTypeChanged();
 		}
 
-		protected virtual void OnIconColorTypePropertyChanged(ThemedIconColorType oldValue, ThemedIconColorType newValue)
+		partial void OnIconColorTypeChanged(ThemedIconColorType newValue)
 		{
 			IconColorTypeChanged();
 		}
