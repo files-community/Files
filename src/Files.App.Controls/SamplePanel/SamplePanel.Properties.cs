383d82cc33ecfd37981ffd15957dfc9f20eacc4b
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-03-17 00:28:57 +0900

    Code Quality: Implemented Omnibar (#16910)
    Signed-off-by: 0x5BFA <62196528+0x5bfa@users.noreply.github.com>
    Co-authored-by: Martin Anderson <7389110+mdtauk@users.noreply.github.com>
    Co-authored-by: Yair <39923744+yaira2@users.noreply.github.com>

---
+++ src/Files.App.Controls/SamplePanel/SamplePanel.Properties.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using CommunityToolkit.WinUI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace Files.App.Controls
+{
+	public sealed partial class SamplePanel
+	{
+		[GeneratedDependencyProperty]
+		public partial string? Header { get; set; }
+
+		[GeneratedDependencyProperty]
+		public partial UIElement? MainContent { get; set; }
+
+		[GeneratedDependencyProperty]
+		public partial UIElement? SideContent { get; set; }
+
+		partial void OnSideContentChanged(UIElement? newValue)
+		{
+			UpdateVisualStates();
+		}
+	}
+}
