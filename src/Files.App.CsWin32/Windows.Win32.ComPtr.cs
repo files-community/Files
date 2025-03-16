79b7621d22daed2a5cc48a386063665f0d18c848
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-03-17 00:41:35 +0900

    Code Quality: Use ComPtr for all D3D11 pointers (#16908)

--- src/Files.App.CsWin32/Windows.Win32.ComPtr.cs
+++ src/Files.App.CsWin32/Windows.Win32.ComPtr.cs
@@ -27,6 +27,14 @@ namespace Windows.Win32
 				((IUnknown*)ptr)->AddRef();
 		}
 
+		public void Attach(T* other)
+		{
+			if (_ptr is not null)
+				((IUnknown*)_ptr)->Release();
+
+			_ptr = other;
+		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public readonly T* Get()
 		{
