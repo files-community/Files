1a1b651b5b4c898158f281eb55e104d75a8f838e
Yair <39923744+yaira2@users.noreply.github.com>
2025-03-17 11:35:46 -0400

    Feature: Drag to clone (#16920)
    Signed-off-by: Yair <39923744+yaira2@users.noreply.github.com>
    Co-authored-by: Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>

---
+++ src/Files.App/ViewModels/Dialogs/CloneRepoDialogViewModel.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using System.IO;
+using System.Windows.Input;
+
+namespace Files.App.ViewModels.Dialogs
+{
+	public sealed partial class CloneRepoDialogViewModel : ObservableObject
+	{
+		public bool CanCloneRepo
+		{
+			get
+			{
+				var repoInfo = GitHelpers.GetRepoInfo(RepoUrl);
+				if (!string.IsNullOrEmpty(repoInfo.RepoUrl))
+				{
+					RepoName = repoInfo.RepoName;
+					return true;
+				}
+
+				return false;
+			}
+		}
+
+		private string repoUrl = string.Empty;
+		public string RepoUrl
+		{
+			get => repoUrl;
+			set
+			{
+				if (SetProperty(ref repoUrl, value))
+					OnPropertyChanged(nameof(CanCloneRepo));
+			}
+		}
+
+		private string repoName = string.Empty;
+		public string RepoName
+		{
+			get => repoName;
+			set => SetProperty(ref repoName, value);
+
+		}
+
+		private string workingDirectory = string.Empty;
+		public string WorkingDirectory
+		{
+			get => workingDirectory;
+			set => SetProperty(ref workingDirectory, value);
+
+		}
+
+		public ICommand CloneRepoCommand { get; private set; }
+
+		public CloneRepoDialogViewModel(string repoUrl, string directory)
+		{
+			var repoInfo = GitHelpers.GetRepoInfo(repoUrl);
+
+			if (!string.IsNullOrEmpty(repoInfo.RepoName))
+			{
+				RepoUrl = repoInfo.RepoUrl;
+				RepoName = repoInfo.RepoName;
+			}
+			else
+			{
+				RepoUrl = string.Empty;
+				RepoName = string.Empty;
+			}
+
+			WorkingDirectory = directory;
+
+			CloneRepoCommand = new AsyncRelayCommand(DoCloneRepo);
+		}
+
+		private async Task DoCloneRepo()
+		{
+			var targetDirectory = Path.Combine(workingDirectory, RepoName);
+			await GitHelpers.CloneRepoAsync(RepoUrl, repoName, targetDirectory);
+		}
+	}
+}
