3a057b8341648d017da927c83638426fe76ee117
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-03-22 02:11:58 +0900

    Code Quality: Self-defined IStorageProviderQuotaUI instead of using StorageProviderStatusUI.FromAbi (#16965)

---
+++ src/Files.App.CsWin32/HRESULT.Extensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+
+namespace Windows.Win32
+{
+	public static class HRESULTExtensions
+	{
+		/// <summary>
+		/// Throws an exception if the <see cref="HRESULT"/> indicates a failure in debug mode. Otherwise, it returns the original <see cref="HRESULT"/>.
+		/// </summary>
+		/// <param name="hr">Represents the result of an operation, indicating success or failure.</param>
+		/// <returns>Returns the original <see cref="HRESULT"/> value regardless of the operation's success.</returns>
+		public static HRESULT ThrowIfFailedOnDebug(this HRESULT hr)
+		{
+#if DEBUG
+			if (hr.Failed)
+				Marshal.ThrowExceptionForHR(hr.Value);
+#endif
+
+			return hr;
+		}
+	}
+}
