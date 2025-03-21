3946bd1eaec3266533c4e440e3a8b207cca7205f
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-03-21 12:48:34 +0000

    Change WCT from 7.x to new

--- src/Files.App/ViewModels/Settings/AboutViewModel.cs
+++ src/Files.App/ViewModels/Settings/AboutViewModel.cs
@@ -72,7 +72,7 @@ namespace Files.App.ViewModels.Settings
 				new ("https://github.com/microsoft/WindowsAppSDK", "WindowsAppSDK"),
 				new ("https://github.com/microsoft/microsoft-ui-xaml", "WinUI 3"),
 				new ("https://github.com/microsoft/Win2D", "Win2D"),
-				new ("https://github.com/CommunityToolkit/WindowsCommunityToolkit", "Windows Community Toolkit 7.x"),
+				new ("https://github.com/CommunityToolkit/Windows", "Windows Community Toolkit"),
 				new ("https://github.com/mono/taglib-sharp", "TagLibSharp"),
 				new ("https://github.com/Tulpep/Active-Directory-Object-Picker", "ActiveDirectoryObjectPicker"),
 				new ("https://github.com/PowerShell/MMI", "MMI"),
