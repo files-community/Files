c4708546aebcb35b2917d3000e0f458613313baa
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-03-23 10:12:05 +0900

    Code Quality: Implemented Breadcrumb (#16946)

---
+++ src/Files.App.Controls/BreadcrumbBar/BreadcrumbBarItem.Events.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+namespace Files.App.Controls
+{
+	public partial class BreadcrumbBarItem
+	{
+		private void ItemContentButton_Click(object sender, RoutedEventArgs e)
+		{
+			OnItemClicked();
+		}
+
+		private void ItemChevronButton_Click(object sender, RoutedEventArgs e)
+		{
+			FlyoutBase.ShowAttachedFlyout(_itemChevronButton);
+		}
+
+		private void ChevronDropDownMenuFlyout_Opening(object? sender, object e)
+		{
+			if (_ownerRef is null ||
+				_ownerRef.TryGetTarget(out var breadcrumbBar) is false ||
+				sender is not MenuFlyout flyout)
+				return;
+
+			breadcrumbBar.RaiseItemDropDownFlyoutOpening(this, flyout);
+		}
+
+		private void ChevronDropDownMenuFlyout_Opened(object? sender, object e)
+		{
+			VisualStateManager.GoToState(this, "ChevronNormalOn", true);
+		}
+
+		private void ChevronDropDownMenuFlyout_Closed(object? sender, object e)
+		{
+			if (_ownerRef is null ||
+				_ownerRef.TryGetTarget(out var breadcrumbBar) is false ||
+				sender is not MenuFlyout flyout)
+				return;
+
+			breadcrumbBar.RaiseItemDropDownFlyoutClosed(this, flyout);
+
+			VisualStateManager.GoToState(this, "ChevronNormalOff", true);
+			VisualStateManager.GoToState(this, "PointerNormal", true);
+		}
+	}
+}
