af76f0bceb551b8e47f0cdcc6d342c62db6d59a8
Yair <39923744+yaira2@users.noreply.github.com>
2025-04-06 11:03:17 -0400

    Code Quality: Added MenuFlyoutHeaderItem control

---
+++ src/Files.App/UserControls/Menus/MenuFlyoutHeaderItem.xaml.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace Files.App.UserControls
+{
+	public sealed partial class MenuFlyoutHeaderItem : MenuFlyoutItem
+	{
+		public MenuFlyoutHeaderItem()
+		{
+			InitializeComponent();
+		}
+	}
+}
