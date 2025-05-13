5aa072c636e2f59b282c853c1642e0fb3332b7aa
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-05-14 08:46:04 +0900

    Code Quality: Added IsAotCompatible to some class library projects (#17123)

--- src/Files.Shared/Helpers/PathHelpers.cs
+++ src/Files.Shared/Helpers/PathHelpers.cs
@@ -17,76 +17,27 @@ namespace Files.Shared.Helpers
 			return folder.Contains('/') ? Path.Combine(folder, name).Replace('\\', '/') : Path.Combine(folder, name);
 		}
 
-		public static string FormatName(string path)
-		{
-			string fileName;
-			string rootPath = Path.GetPathRoot(path) ?? string.Empty;
-
-			if (rootPath == path && 
-					( path.StartsWith(@"\\") ||
-					  (new DriveInfo(path).DriveType == System.IO.DriveType.Network)
-					)
-			   )
-			{
-				// Network Share path
-				fileName = path.Substring(path.LastIndexOf(@"\", StringComparison.Ordinal) + 1);
-			}
-			else if (rootPath == path)
-			{
-				// Drive path
-				fileName = path;
-			}
-			else
-			{
-				// Standard file name
-				fileName = Path.GetFileName(path);
-			}
-
-			// Check for link file name
-			if (FileExtensionHelpers.IsShortcutOrUrlFile(fileName))
-				fileName = fileName.Remove(fileName.Length - 4);
-
-			return fileName;
-		}
-
-		/// <summary>
-		/// Determines whether the <paramref name="path"/> points to any special system folders.
-		/// </summary>
-		/// <remarks>
-		///	The term "Special folder" refers to any folders that may be natively supported by a certain platform (e.g. Libraries).
-		/// </remarks>
-		/// <param name="path">The path to a folder to check.</param>
-		/// <returns>If the path points to a special folder, returns true; otherwise false.</returns>
-		public static bool IsSpecialFolder(string path)
-		{
-			foreach (Environment.SpecialFolder specialFolder in Enum.GetValues(typeof(Environment.SpecialFolder)))
-			{
-				var specialFolderPath = Environment.GetFolderPath(specialFolder);
-				if (string.Equals(specialFolderPath, path, StringComparison.OrdinalIgnoreCase))
-					return true;
-			}
-
-			return false;
-		}
-
 		public static bool TryGetFullPath(string commandName, out string fullPath)
 		{
 			fullPath = string.Empty;
 			try
 			{
-				var p = new Process();
-				p.StartInfo = new ProcessStartInfo
+				var p = new Process
 				{
-					UseShellExecute = false,
-					CreateNoWindow = true,
-					FileName = "where.exe",
-					Arguments = commandName,
-					RedirectStandardOutput = true
+					StartInfo = new ProcessStartInfo
+					{
+						UseShellExecute = false,
+						CreateNoWindow = true,
+						FileName = "where.exe",
+						Arguments = commandName,
+						RedirectStandardOutput = true
+					}
 				};
+
 				p.Start();
+
 				var output = p.StandardOutput.ReadToEnd();
 				p.WaitForExit(1000);
-
 
 				if (p.ExitCode != 0)
 					return false;
@@ -97,16 +48,17 @@ namespace Files.Shared.Helpers
 					if (FileExtensionHelpers.IsExecutableFile(line))
 					{
 						fullPath = line;
+
 						return true;
 					}
 				}
+
 				return false;
 			}
 			catch (Exception)
 			{
 				return false;
 			}
-
 		}
 	}
 }
