a32ea2758e49c0dbf0f255914994fa24639a7263
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-05-14 21:08:42 +0900

    Init

---
+++ tests/Files.App.UnitTests/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
+
+namespace App1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(0, 0);
+		}
+
+		[UITestMethod]
+		public void TestMethod2()
+		{
+			var grid = new Grid();
+			Assert.AreEqual(0, grid.MinWidth);
+		}
+	}
+}
