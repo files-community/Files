674ad665e8dfb3f2318dbf93154c964ab707c06c
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-05-27 23:20:38 +0900

    [WIP] Added UnfocusedStateBehaviors

---
+++ src/Files.App.Controls/Omnibar/OmnibarModeUnfocusedStateBehaviors.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+namespace Files.App.Controls
+{
+	public enum OmnibarModeUnfocusedStateBehaviors
+	{
+		ReturnToDefaultMode,
+		RetainCurrentMode,
+		RetainCurrentModeWhenEmpty,
+	}
+}
