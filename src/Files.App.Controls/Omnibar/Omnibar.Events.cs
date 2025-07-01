548119ed2fd837b25f1b33943afccf4a4a698148
Yair <39923744+yaira2@users.noreply.github.com>
2025-07-01 17:33:19 -0400

    Fixed clicking tab in command mode

--- src/Files.App.Controls/Omnibar/Omnibar.Events.cs
+++ src/Files.App.Controls/Omnibar/Omnibar.Events.cs
@@ -106,14 +106,6 @@ namespace Files.App.Controls
 					previouslyFocusedElement?.Focus(FocusState.Programmatic);
 				}
 			}
-			else if (e.Key == VirtualKey.Tab && !InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Shift).HasFlag(CoreVirtualKeyStates.Down))
-			{
-				// Focus on inactive content when pressing Tab instead of moving to the next control in the tab order
-				e.Handled = true;
-				IsFocused = false;
-				await Task.Delay(15);
-				CurrentSelectedMode?.ContentOnInactive?.Focus(FocusState.Keyboard);
-			}
 			else
 			{
 				_textChangeReason = OmnibarTextChangeReason.UserInput;
