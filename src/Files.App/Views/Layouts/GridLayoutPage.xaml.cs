f8b3725a13ec57aeef303e02dd18eea13a3e9118
Filippo Ferrario <102259289+ferrariofilippo@users.noreply.github.com>
2025-07-29 17:17:48 +0200

    Fix: Fixed issue where delete key wouldn't work after removing item from selection (#17348)

--- src/Files.App/Views/Layouts/GridLayoutPage.xaml.cs
+++ src/Files.App/Views/Layouts/GridLayoutPage.xaml.cs
@@ -670,10 +670,18 @@ namespace Files.App.Views.Layouts
 
 		private void ItemSelected_Unchecked(object sender, RoutedEventArgs e)
 		{
-			if (sender is CheckBox checkBox &&
-				checkBox.DataContext is ListedItem item &&
-				FileList.SelectedItems.Contains(item))
+			if (sender is not CheckBox checkBox)
+				return;
+
+			if (checkBox.DataContext is ListedItem item && FileList.SelectedItems.Contains(item))
 				FileList.SelectedItems.Remove(item);
+
+			// Workaround for #17298
+			checkBox.IsTabStop = false;
+			checkBox.IsEnabled = false;
+			checkBox.IsEnabled = true;
+			checkBox.IsTabStop = true;
+			FileList.Focus(FocusState.Programmatic);
 		}
 
 		private new void FileList_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
