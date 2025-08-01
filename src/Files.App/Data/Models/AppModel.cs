ee6995f7b31d6fc06ae50f45a879f1beac4b45b0
Yair <39923744+yaira2@users.noreply.github.com>
2025-07-31 22:40:55 -0400

    Update AppModel.cs

--- src/Files.App/Data/Models/AppModel.cs
+++ src/Files.App/Data/Models/AppModel.cs
@@ -42,10 +42,8 @@ namespace Files.App.Data.Models
 				{
 					if (value >= 0 && value < MainPageViewModel.AppInstances.Count)
 					{
-						if (MainWindow.Instance.Content is Frame rootFrame &&	rootFrame.Content is MainPage mainView)
-						{
+						if (MainWindow.Instance.Content is Frame rootFrame && rootFrame.Content is MainPage mainView)
 							mainView.ViewModel.SelectedTabItem = MainPageViewModel.AppInstances[value];
-						}
 					}
 				}
 				catch (COMException)
