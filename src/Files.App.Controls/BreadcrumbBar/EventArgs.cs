fcd9f4201d3f3fd59d55d65d9033f10221695d76
DHANUSH RAJA <155062318+DHANUSHRAJA22@users.noreply.github.com>
2025-08-26 19:11:45 +0530

    Add BreadcrumbBarItemMiddleClickedEventArgs classUpdate EventArgs.cs
    Signed-off-by: DHANUSH RAJA <155062318+DHANUSHRAJA22@users.noreply.github.com>

--- src/Files.App.Controls/BreadcrumbBar/EventArgs.cs
+++ src/Files.App.Controls/BreadcrumbBar/EventArgs.cs
@@ -1,9 +1,8 @@
-﻿// Copyright (c) Files Community
+// Copyright (c) Files Community
 // Licensed under the MIT License.
-
 namespace Files.App.Controls
 {
 	public record class BreadcrumbBarItemClickedEventArgs(BreadcrumbBarItem Item, int Index, bool IsRootItem = false);
-
+	public record class BreadcrumbBarItemMiddleClickedEventArgs(BreadcrumbBarItem Item, int Index, bool IsRootItem = false);
 	public record class BreadcrumbBarItemDropDownFlyoutEventArgs(MenuFlyout Flyout, BreadcrumbBarItem? Item = null, int Index = -1, bool IsRootItem = false);
 }
