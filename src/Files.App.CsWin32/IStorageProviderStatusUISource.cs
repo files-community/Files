1532364f31893005d386018ac6fbb8ceb663065d
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-09-08 20:00:05 +0900

    Init

--- src/Files.App.CsWin32/IStorageProviderStatusUISource.cs
+++ src/Files.App.CsWin32/IStorageProviderStatusUISource.cs
@@ -1,23 +1,16 @@
 ﻿// Copyright (c) Files Community
 // Licensed under the MIT License.
 
+using Files.Shared.Attributes;
 using System;
-using System.Runtime.CompilerServices;
 using Windows.Win32.Foundation;
 
 namespace Windows.Win32.System.WinRT
 {
 	public unsafe partial struct IStorageProviderStatusUISource : IComIID
 	{
-#pragma warning disable CS0649 // Field 'field' is never assigned to, and will always have its default value 'value'
-		private void** lpVtbl;
-#pragma warning restore CS0649 // Field 'field' is never assigned to, and will always have its default value 'value'
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public HRESULT GetStatusUI(IStorageProviderStatusUI** result)
-		{
-			return (HRESULT)((delegate* unmanaged[MemberFunction]<IStorageProviderStatusUISource*, IStorageProviderStatusUI**, int>)lpVtbl[6])((IStorageProviderStatusUISource*)Unsafe.AsPointer(ref this), result);
-		}
+		[GeneratedVTableFunction(Index = 6)]
+		public partial HRESULT GetStatusUI(IStorageProviderStatusUI** result);
 
 		[GuidRVAGen.Guid("A306C249-3D66-5E70-9007-E43DF96051FF")]
 		public static partial ref readonly Guid Guid { get; }
---
+++ src/Files.Core.SourceGenerator/Data/VTableFunctionInfo.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+namespace Files.Core.SourceGenerator.Data
+{
+	internal record VTableFunctionInfo(
+		string FullyQualifiedParentTypeName,
+		string ParentTypeNamespace,
+		string ParentTypeName,
+		string Name,
+		string ReturnTypeName,
+		int Index,
+		EquatableArray<ParameterTypeNamePair> Parameters);
+}
