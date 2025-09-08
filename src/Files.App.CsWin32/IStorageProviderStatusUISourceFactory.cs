fa0953a7dee09caeca780402cfe1924a373c46e4
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-09-08 20:00:05 +0900

    Init

--- src/Files.App.CsWin32/IStorageProviderStatusUISourceFactory.cs
+++ src/Files.App.CsWin32/IStorageProviderStatusUISourceFactory.cs
@@ -1,23 +1,16 @@
 ﻿// Copyright (c) Files Community
 // Licensed under the MIT License.
 
+using Files.Shared.Attributes;
 using System;
-using System.Runtime.CompilerServices;
 using Windows.Win32.Foundation;
 
 namespace Windows.Win32.System.WinRT
 {
 	public unsafe partial struct IStorageProviderStatusUISourceFactory : IComIID
 	{
-#pragma warning disable CS0649 // Field 'field' is never assigned to, and will always have its default value 'value'
-		private void** lpVtbl;
-#pragma warning restore CS0649 // Field 'field' is never assigned to, and will always have its default value 'value'
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public HRESULT GetStatusUISource(nint syncRootId, IStorageProviderStatusUISource** result)
-		{
-			return (HRESULT)((delegate* unmanaged[MemberFunction]<IStorageProviderStatusUISourceFactory*, nint, IStorageProviderStatusUISource**, int>)lpVtbl[6])((IStorageProviderStatusUISourceFactory*)Unsafe.AsPointer(ref this), syncRootId, result);
-		}
+		[GeneratedVTableFunction(Index = 6)]
+		public partial HRESULT GetStatusUISource(nint syncRootId, IStorageProviderStatusUISource** result);
 
 		[GuidRVAGen.Guid("12E46B74-4E5A-58D1-A62F-0376E8EE7DD8")]
 		public static partial ref readonly Guid Guid { get; }
---
+++ src/Files.Core.SourceGenerator/Data/ParameterTypeNamePair.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+namespace Files.Core.SourceGenerator.Data
+{
+	internal record ParameterTypeNamePair(string FullyQualifiedTypeName, string ValueName);
+}
