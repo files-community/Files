cb5d01ea7aec7e6a94e1f08038b4f52c617f757d
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-09-18 04:09:16 +0900

    Code Quality: Generate a COM vtable call from [GeneratedVTableFunction] (#17570)

--- src/Files.App.CsWin32/IStorageProviderQuotaUI.cs
+++ src/Files.App.CsWin32/IStorageProviderQuotaUI.cs
@@ -1,29 +1,19 @@
 ﻿// Copyright (c) Files Community
 // Licensed under the MIT License.
 
+using Files.Shared.Attributes;
 using System;
-using System.Runtime.CompilerServices;
 using Windows.Win32.Foundation;
 
 namespace Windows.Win32.System.WinRT
 {
 	public unsafe partial struct IStorageProviderQuotaUI : IComIID
 	{
-#pragma warning disable CS0649 // Field 'field' is never assigned to, and will always have its default value 'value'
-		private void** lpVtbl;
-#pragma warning restore CS0649 // Field 'field' is never assigned to, and will always have its default value 'value'
+		[GeneratedVTableFunction(Index = 6)]
+		public partial HRESULT GetQuotaTotalInBytes(ulong* value);
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public HRESULT GetQuotaTotalInBytes(ulong* value)
-		{
-			return (HRESULT)((delegate* unmanaged[MemberFunction]<IStorageProviderQuotaUI*, ulong*, int>)(lpVtbl[6]))((IStorageProviderQuotaUI*)Unsafe.AsPointer(ref this), value);
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public HRESULT GetQuotaUsedInBytes(ulong* value)
-		{
-			return (HRESULT)((delegate* unmanaged[MemberFunction]<IStorageProviderQuotaUI*, ulong*, int>)(lpVtbl[8]))((IStorageProviderQuotaUI*)Unsafe.AsPointer(ref this), value);
-		}
+		[GeneratedVTableFunction(Index = 8)]
+		public partial HRESULT GetQuotaUsedInBytes(ulong* value);
 
 		[GuidRVAGen.Guid("BA6295C3-312E-544F-9FD5-1F81B21F3649")]
 		public static partial ref readonly Guid Guid { get; }
---
+++ src/Files.Shared/Attributes/GeneratedVTableFunctionAttribute.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using System;
+
+namespace Files.Shared.Attributes;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+public sealed class GeneratedVTableFunctionAttribute : Attribute
+{
+	public required int Index { get; init; }
+}
