50a82ded4ac048154472d90cc6c1e47ef120dc6c
Saravanan Ganapathi <workbysaran@gmail.com>
2025-09-26 19:51:51 +0530

    Fix: Fixed issue with opening items from search results in Columns View (#17657)

--- src/Files.App/Views/Layouts/DetailsLayoutPage.xaml.cs
+++ src/Files.App/Views/Layouts/DetailsLayoutPage.xaml.cs
@@ -458,8 +458,8 @@ namespace Files.App.Views.Layouts
 
 				if (ctrlPressed && !shiftPressed)
 				{
-					var folders = ParentShellPageInstance?.SlimContentPage.SelectedItems?.Where(file => file.PrimaryItemAttribute == StorageItemTypes.Folder);
-					if (folders is not null)
+					var folders = SelectedItems?.Where(file => file.PrimaryItemAttribute == StorageItemTypes.Folder);
+					if (folders?.Any() ?? false)
 					{
 						foreach (ListedItem folder in folders)
 							await NavigationHelpers.OpenPathInNewTab(folder.ItemPath);
@@ -467,9 +467,19 @@ namespace Files.App.Views.Layouts
 				}
 				else if (ctrlPressed && shiftPressed)
 				{
-					var selectedFolder = SelectedItems?.FirstOrDefault(item => item.PrimaryItemAttribute == StorageItemTypes.Folder);
-					if (selectedFolder is not null)
-						NavigationHelpers.OpenInSecondaryPane(ParentShellPageInstance, selectedFolder);
+					var selectedFolders = SelectedItems?.Where(item => item.PrimaryItemAttribute == StorageItemTypes.Folder);
+					if (selectedFolders?.Count() == 1)
+					{
+						NavigationHelpers.OpenInSecondaryPane(ParentShellPageInstance, selectedFolders.First());
+					}
+				}
+				else if (!ctrlPressed && !shiftPressed && !UserSettingsService.FoldersSettingsService.OpenItemsWithOneClick)
+				{
+					if (SelectedItems?.Any() ?? false)
+					{
+						foreach (var selectedItem in SelectedItems)
+							await OpenItem(selectedItem);
+					}
 				}
 			}
 			else if (e.Key == VirtualKey.Enter && e.KeyStatus.IsMenuKeyDown)
@@ -607,12 +617,34 @@ namespace Files.App.Views.Layouts
 			}
 		}
 
+		private async Task OpenItem(ListedItem item)
+		{
+			if (!Commands.OpenItem.IsExecutable)
+			{
+				// Fallback if the command is not executable. It occurs only when search is performed from the columns view.
+				var itemType = item.PrimaryItemAttribute == StorageItemTypes.Folder ? FilesystemItemType.Directory : FilesystemItemType.File;
+				await NavigationHelpers.OpenPath(item.ItemPath, ParentShellPageInstance, itemType);
+			}
+			else
+			{
+				await Commands.OpenItem.ExecuteAsync();
+			}
+		}
+
 		private async void FileList_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
 		{
 			// Skip opening selected items if the double tap doesn't capture an item
-			if ((e.OriginalSource as FrameworkElement)?.DataContext is ListedItem item && !UserSettingsService.FoldersSettingsService.OpenItemsWithOneClick)
-				await Commands.OpenItem.ExecuteAsync();
-			else if ((e.OriginalSource as FrameworkElement)?.DataContext is not ListedItem && UserSettingsService.FoldersSettingsService.DoubleClickToGoUp)
+			var originalElement = e.OriginalSource as FrameworkElement;
+			var dataContext = originalElement?.DataContext;
+
+			// Try to get the item from DataContext or from sender (ListView)
+			ListedItem? item = dataContext as ListedItem;
+			if (item == null && sender is ListView listView && listView.SelectedItem is ListedItem selectedItem)
+				item = selectedItem;
+
+			if (item != null && !UserSettingsService.FoldersSettingsService.OpenItemsWithOneClick)
+				await OpenItem(item);
+			else if (item == null && UserSettingsService.FoldersSettingsService.DoubleClickToGoUp)
 				await Commands.NavigateUp.ExecuteAsync();
 
 			ResetRenameDoubleClick();
