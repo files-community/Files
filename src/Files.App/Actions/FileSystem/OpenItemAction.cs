25c59f1bd37e0a90115799154bab7cbf996a63d8
Saravanan Ganapathi <workbysaran@gmail.com>
2025-11-04 02:56:21 +0530

    Fix: Fixed an issue where incorrect actions were displayed in the Recycle Bin context menu (#17813)

--- src/Files.App/Actions/FileSystem/OpenItemAction.cs
+++ src/Files.App/Actions/FileSystem/OpenItemAction.cs
@@ -27,7 +27,8 @@ namespace Files.App.Actions
 		public bool IsExecutable =>
 			context.HasSelection &&
 			!(context.ShellPage is ColumnShellPage &&
-			context.SelectedItem?.PrimaryItemAttribute == StorageItemTypes.Folder);
+			context.SelectedItem?.PrimaryItemAttribute == StorageItemTypes.Folder) &&
+			context.PageType != ContentPageTypes.RecycleBin;
 
 		public OpenItemAction()
 		{
@@ -67,6 +68,7 @@ namespace Files.App.Actions
 
 		public bool IsExecutable =>
 			context.HasSelection &&
+			context.PageType != ContentPageTypes.RecycleBin &&
 			context.SelectedItems.All(i =>
 				(i.PrimaryItemAttribute == StorageItemTypes.File && !i.IsShortcut && !i.IsExecutable) ||
 				(i.PrimaryItemAttribute == StorageItemTypes.Folder && i.IsArchive));
