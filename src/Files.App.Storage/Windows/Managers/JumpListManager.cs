4d669b984ec7e9a1df53e0210dd62b95ac85acc7
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2025-11-14 23:19:55 +0900

    Use proper folder icons

--- src/Files.App.Storage/Windows/Managers/JumpListManager.cs
+++ src/Files.App.Storage/Windows/Managers/JumpListManager.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Files Community
 // Licensed under the MIT License.
 
-using System;
+using OwlCore.Storage;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -28,9 +28,9 @@ namespace Files.App.Storage
 		private static readonly Lazy<JumpListManager?> _default = new(Create, LazyThreadSafetyMode.ExecutionAndPublication);
 		public static JumpListManager? Default => _default.Value;
 
-		private readonly static string _aumid = $"{Package.Current.Id.FamilyName}!App";
+		private string _filesAUMID = null!;
 
-		private const string LocalizedRecentCategoryName = "@{C:\\Windows\\SystemResources\\Windows.UI.ShellCommon\\Windows.UI.ShellCommon.pri? ms-resource://Windows.UI.ShellCommon/JumpViewUI/JumpViewCategoryType_Recent}";
+		private string _localizedRecentCategoryName = null!;
 
 		private FileSystemWatcher? _explorerJumpListWatcher;
 		private FileSystemWatcher? _filesJumpListWatcher;
@@ -39,6 +39,8 @@ namespace Files.App.Storage
 		private IAutomaticDestinationList* _filesADL;
 		private ICustomDestinationList* _filesCDL;
 		private IInternalCustomDestinationList* _filesICDL;
+
+		private JumpListManager() { }
 
 		public HRESULT PullJumpListFromExplorer(int maxCount = 40)
 		{
@@ -49,7 +51,7 @@ namespace Files.App.Storage
 
 			try
 			{
-				hr = SyncExplorerJumpListWithFiles(maxCount);
+				hr = SyncFilesJumpListWithExplorer(maxCount);
 				if (FAILED(hr)) return hr;
 			}
 			finally
@@ -70,7 +72,7 @@ namespace Files.App.Storage
 
 			try
 			{
-				hr = SyncFilesJumpListWithExplorer(maxCount);
+				hr = SyncExplorerJumpListWithFiles(maxCount);
 				if (FAILED(hr)) return hr;
 			}
 			finally
@@ -131,7 +133,11 @@ namespace Files.App.Storage
 
 			void* pv = default;
 
-			var instance = new JumpListManager();
+			var instance = new JumpListManager()
+			{
+				_filesAUMID = $"{Package.Current.Id.FamilyName}!App",
+				_localizedRecentCategoryName = $"@{{{Environment.GetEnvironmentVariable("SystemRoot")}\\SystemResources\\Windows.UI.ShellCommon\\Windows.UI.ShellCommon.pri? ms-resource://Windows.UI.ShellCommon/JumpViewUI/JumpViewCategoryType_Recent}}",
+			};
 
 			hr = PInvoke.CoCreateInstance(CLSID.CLSID_AutomaticDestinationList, null, CLSCTX.CLSCTX_INPROC_SERVER, IID.IID_IAutomaticDestinationList, &pv);
 			if (FAILED(hr)) return null;
@@ -141,67 +147,52 @@ namespace Files.App.Storage
 			hr = PInvoke.CoCreateInstance(CLSID.CLSID_AutomaticDestinationList, null, CLSCTX.CLSCTX_INPROC_SERVER, IID.IID_IAutomaticDestinationList, &pv);
 			if (FAILED(hr)) return null;
 			instance._filesADL = (IAutomaticDestinationList*)pv;
-			instance._filesADL->Initialize((PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in _aumid.GetPinnableReference())), default, default);
+			instance._filesADL->Initialize((PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in instance._filesAUMID.GetPinnableReference())), default, default);
 
 			hr = PInvoke.CoCreateInstance(CLSID.CLSID_DestinationList, null, CLSCTX.CLSCTX_INPROC_SERVER, IID.IID_ICustomDestinationList, &pv);
 			if (FAILED(hr)) return null;
 			instance._filesCDL = (ICustomDestinationList*)pv;
-			instance._filesCDL->SetAppID((PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in _aumid.GetPinnableReference())));
+			instance._filesCDL->SetAppID((PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in instance._filesAUMID.GetPinnableReference())));
 
 			hr = PInvoke.CoCreateInstance(CLSID.CLSID_DestinationList, null, CLSCTX.CLSCTX_INPROC_SERVER, IID.IID_IInternalCustomDestinationList, &pv);
 			if (FAILED(hr)) return null;
 			instance._filesICDL = (IInternalCustomDestinationList*)pv;
-			instance._filesICDL->SetApplicationID((PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in _aumid.GetPinnableReference())));
+			instance._filesICDL->SetApplicationID((PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in instance._filesAUMID.GetPinnableReference())));
 
 			return instance;
 		}
 
-		private HRESULT SyncExplorerJumpListWithFiles(int maxItemsToSync)
+		private HRESULT SyncFilesJumpListWithExplorer(int maxItemsToSync)
 		{
 			HRESULT hr = default;
 
-			uint count = 0U;
-			hr = _filesICDL->GetCategoryCount(&count);
+			BOOL hasList = default;
+			hr = _filesADL->HasList(&hasList);
 			if (FAILED(hr)) return hr;
 
-			for (uint dwIndex = 0U; dwIndex < count; dwIndex++)
+			if (hasList)
 			{
-				APPDESTCATEGORY category = default;
-
-				try
-				{
-					hr = _filesICDL->GetCategory(dwIndex, GETCATFLAG.DEFAULT, &category);
-					if (FAILED(hr) || category.Type is not APPDESTCATEGORYTYPE.CUSTOM) continue;
-
-					hr = _filesICDL->DeleteCategory(dwIndex, true);
-					if (FAILED(hr)) continue;
-				}
-				finally
-				{
-					// The memory layout at Name can be either PWSTR or int depending on the category type
-					if (category.Anonymous.Name.Value is not null && category.Type is APPDESTCATEGORYTYPE.CUSTOM)
-						PInvoke.CoTaskMemFree(category.Anonymous.Name);
-				}
+				hr = _filesADL->ClearList(true);
+				if (FAILED(hr)) return hr;
 			}
 
-			hr = _filesICDL->ClearRemovedDestinations();
-			if (FAILED(hr)) return hr;
+			hr = _filesCDL->DeleteList((PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in _filesAUMID.GetPinnableReference())));
 
 			using ComPtr<IObjectCollection> poc = default;
 			hr = _explorerADL->GetList(DESTLISTTYPE.RECENT, maxItemsToSync, GETDESTLISTFLAGS.NONE, IID.IID_IObjectCollection, (void**)poc.GetAddressOf());
 			if (FAILED(hr)) return hr;
 
-			uint cRecentItems = 0U;
-			hr = poc.Get()->GetCount(&cRecentItems);
+			uint dwItemsCount = 0U;
+			hr = poc.Get()->GetCount(&dwItemsCount);
 			if (FAILED(hr)) return hr;
 
 			using ComPtr<IObjectCollection> pNewObjectCollection = default;
 			hr = PInvoke.CoCreateInstance(CLSID.CLSID_EnumerableObjectCollection, null, CLSCTX.CLSCTX_INPROC_SERVER, IID.IID_IObjectCollection, (void**)pNewObjectCollection.GetAddressOf());
 
-			for (int index = (int)cRecentItems - 1; index >= 0U; index--)
+			for (uint dwIndex = 0; dwIndex < dwItemsCount; dwIndex++)
 			{
 				using ComPtr<IShellItem> psi = default;
-				hr = poc.Get()->GetAt((uint)index, IID.IID_IShellItem, (void**)psi.GetAddressOf());
+				hr = poc.Get()->GetAt(dwIndex, IID.IID_IShellItem, (void**)psi.GetAddressOf());
 				if (FAILED(hr)) continue;
 
 				IShellLinkW* psl = default;
@@ -215,7 +206,7 @@ namespace Files.App.Storage
 				hr = _explorerADL->GetPinIndex((IUnknown*)psi.Get(), &pinIndex);
 				if (FAILED(hr)) continue; // If not pinned, HRESULT is E_NOT_SET
 
-				hr = _filesADL->PinItem((IUnknown*)psl, -1);
+				hr = _filesADL->PinItem((IUnknown*)psl, pinIndex);
 				if (FAILED(hr)) continue;
 			}
 
@@ -225,7 +216,7 @@ namespace Files.App.Storage
 
 			PWSTR pOutBuffer = (PWSTR)NativeMemory.Alloc(256 * sizeof(char));
 			hr = PInvoke.SHLoadIndirectString(
-				(PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in LocalizedRecentCategoryName.GetPinnableReference())),
+				(PCWSTR)Unsafe.AsPointer(ref Unsafe.AsRef(in _localizedRecentCategoryName.GetPinnableReference())),
 				pOutBuffer, 256U);
 			if (FAILED(hr)) return hr;
 
@@ -245,21 +236,24 @@ namespace Files.App.Storage
 			return hr;
 		}
 
-		private HRESULT SyncFilesJumpListWithExplorer(int maxItemsToSync)
+		private HRESULT SyncExplorerJumpListWithFiles(int maxItemsToSync)
 		{
 			HRESULT hr = default;
 
 			BOOL hasList = default;
 			hr = _explorerADL->HasList(&hasList);
-			if (FAILED(hr) || !hasList) return hr;
-
-			hr = _explorerADL->ClearList(true);
 			if (FAILED(hr)) return hr;
+
+			if (hasList)
+			{
+				hr = _explorerADL->ClearList(true);
+				if (FAILED(hr)) return hr;
+			}
 
 			uint count = 0U;
 			_filesICDL->GetCategoryCount(&count);
 
-			uint destinationsIndex = 0U;
+			uint dwDestinationsIndex = 0U;
 			for (uint dwIndex = 0U; dwIndex < count; dwIndex++)
 			{
 				APPDESTCATEGORY category = default;
@@ -269,12 +263,13 @@ namespace Files.App.Storage
 					hr = _filesICDL->GetCategory(dwIndex, GETCATFLAG.DEFAULT, &category);
 					if (FAILED(hr) ||
 						category.Type is not APPDESTCATEGORYTYPE.CUSTOM ||
-						!LocalizedRecentCategoryName.Equals(new(category.Anonymous.Name), StringComparison.OrdinalIgnoreCase))
+						!_localizedRecentCategoryName.Equals(new(category.Anonymous.Name), StringComparison.OrdinalIgnoreCase))
 						continue;
 
-					destinationsIndex = dwIndex;
+					dwDestinationsIndex = dwIndex;
 				}
 				finally
+
 				{
 					// The memory layout at Name can be either PWSTR or int depending on the category type
 					if (category.Anonymous.Name.Value is not null && category.Type is APPDESTCATEGORYTYPE.CUSTOM)
@@ -283,7 +278,7 @@ namespace Files.App.Storage
 			}
 
 			using ComPtr<IObjectCollection> pDestinationsObjectCollection = default;
-			hr = _filesICDL->EnumerateCategoryDestinations(destinationsIndex, IID.IID_IObjectCollection, (void**)pDestinationsObjectCollection.GetAddressOf());
+			hr = _filesICDL->EnumerateCategoryDestinations(dwDestinationsIndex, IID.IID_IObjectCollection, (void**)pDestinationsObjectCollection.GetAddressOf());
 
 			uint dwItems = 0U;
 			hr = pDestinationsObjectCollection.Get()->GetCount(&dwItems);
@@ -326,15 +321,27 @@ namespace Files.App.Storage
 			hr = PInvoke.SHGetIDListFromObject((IUnknown*)psi, pidl.GetAddressOf());
 			if (FAILED(hr)) return hr;
 
-			hr = psl.Get()->SetIDList(pidl.Get());
+			using ComHeapPtr<char> pParseablePath = default;
+			hr = psi->GetDisplayName(SIGDN.SIGDN_DESKTOPABSOLUTEPARSING, (PWSTR*)pParseablePath.GetAddressOf());
 			if (FAILED(hr)) return hr;
 
-			hr = psl.Get()->SetArguments("");
+			hr = psl.Get()->SetArguments(new(pParseablePath.Get()));
 			if (FAILED(hr)) return hr;
 
 			using ComHeapPtr<char> pDisplayName = default;
 			hr = psi->GetDisplayName(SIGDN.SIGDN_PARENTRELATIVEFORUI, (PWSTR*)pDisplayName.GetAddressOf());
 			if (FAILED(hr)) return hr;
+
+			PWSTR pIconLocation = (PWSTR)PInvoke.CoTaskMemAlloc(PInvoke.MAX_PATH);
+			int index = 0;
+			hr = GetFolderIconLocation(psi, &pIconLocation, PInvoke.MAX_PATH, &index);
+			if (FAILED(hr)) return hr;
+
+			var iconLocation = new string(pIconLocation);
+			hr = psl.Get()->SetIconLocation(pIconLocation, index);
+			if (FAILED(hr)) return hr;
+
+			PInvoke.CoTaskMemFree(pIconLocation);
 
 			using ComPtr<IPropertyStore> pps = default;
 			hr = psl.Get()->QueryInterface(IID.IID_IPropertyStore, (void**)pps.GetAddressOf());
@@ -356,6 +363,21 @@ namespace Files.App.Storage
 			if (FAILED(hr)) return hr;
 
 			hr = psl.Get()->QueryInterface(IID.IID_IShellLinkW, (void**)ppsl);
+			if (FAILED(hr)) return hr;
+
+			return hr;
+		}
+
+		private HRESULT GetFolderIconLocation(IShellItem* psi, PWSTR* pIconFilePath, uint cch, int* pIndex)
+		{
+			HRESULT hr = default;
+
+			using ComPtr<IExtractIconW> pei = default;
+			hr = psi->BindToHandler(null, BHID.BHID_SFUIObject, IID.IID_IExtractIconW, (void**)pei.GetAddressOf());
+			if (FAILED(hr)) return hr;
+
+			uint flags;
+			hr = pei.Get()->GetIconLocation(PInvoke.GIL_FORSHELL, *pIconFilePath, cch, pIndex, &flags);
 			if (FAILED(hr)) return hr;
 
 			return hr;
