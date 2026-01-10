418dd8f1015088b6d8ea994efd20ac970f6d0a9c
Lamparter <71598437+Lamparter@users.noreply.github.com>
2026-01-10 21:28:50 +0000

    Add default values to `WebMessage` properties

--- src/Files.App/Data/Messages/WebMessage.cs
+++ src/Files.App/Data/Messages/WebMessage.cs
@@ -9,12 +9,12 @@
 		/// The classification type of the web message (to differentiate from different event listeners).
 		/// </summary>
 		[JsonPropertyName("type")]
-		public string Type { get; set; }
+		public string Type { get; set; } = string.Empty;
 
 		/// <summary>
 		/// The key (or payload) associated with the web message.
 		/// </summary>
 		[JsonPropertyName("key")]
-		public string Key { get; set; }
+		public string Key { get; set; } = string.Empty;
 	}
 }
