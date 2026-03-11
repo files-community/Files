539cb7f9d8e953adac474031921921ea83561ad7
yair100 <39923744+yair100@users.noreply.github.com>
2026-03-11 10:47:53 -0400

    Feature: Added action to toggle sidebar (#18239)

---
+++ src/Files.App/Actions/Show/ToggleSidebarAction.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Files Community
+// Licensed under the MIT License.
+
+using Files.App.Controls;
+using Files.App.ViewModels.UserControls;
+
+namespace Files.App.Actions
+{
+	[GeneratedRichCommand]
+	internal sealed partial class ToggleSidebarAction : ObservableObject, IToggleAction
+	{
+		private IAppearanceSettingsService AppearanceSettingsService { get; } = Ioc.Default.GetRequiredService<IAppearanceSettingsService>();
+
+		public string Label
+			=> Strings.ToggleSidebar.GetLocalizedResource();
+
+		public string Description
+			=> Strings.ToggleSidebarDescription.GetLocalizedResource();
+
+		public HotKey HotKey
+			=> new(Keys.B, KeyModifiers.Ctrl);
+
+		public bool IsOn
+			=> AppearanceSettingsService.IsSidebarOpen;
+
+		public ToggleSidebarAction()
+		{
+			AppearanceSettingsService.PropertyChanged += AppearanceSettingsService_PropertyChanged;
+		}
+
+		public Task ExecuteAsync(object? parameter = null)
+		{
+			var sidebarViewModel = Ioc.Default.GetRequiredService<SidebarViewModel>();
+			sidebarViewModel.SidebarDisplayMode = IsOn
+				? SidebarDisplayMode.Compact
+				: SidebarDisplayMode.Expanded;
+
+			return Task.CompletedTask;
+		}
+
+		private void AppearanceSettingsService_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName is nameof(AppearanceSettingsService.IsSidebarOpen))
+				OnPropertyChanged(nameof(IsOn));
+		}
+	}
+}
