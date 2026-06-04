d0cb35f1a70543b77e57a6302c017557aa8d6b89
Yair <39923744+yair100@users.noreply.github.com>
2026-06-04 00:15:14 -0400

    Improved styling

--- src/Files.App/UserControls/StatusBar.xaml.cs
+++ src/Files.App/UserControls/StatusBar.xaml.cs
@@ -60,6 +60,11 @@ namespace Files.App.UserControls
 			BranchesFlyout.Hide();
 		}
 
+		private void ZipEncodingList_ItemClick(object sender, ItemClickEventArgs e)
+		{
+			ZipEncodingFlyout.Hide();
+		}
+
 		private void BranchesFlyout_Closing(object _, object e)
 		{
 			if (StatusBarViewModel is null)
