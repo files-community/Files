202daf359be3bf9885df30fddefd4ad290e08eaa
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2026-06-18 16:17:44 +0900

    Update ToolbarSections.cs

--- src/Files.App/Data/Items/ToolbarSections.cs
+++ src/Files.App/Data/Items/ToolbarSections.cs
@@ -26,6 +26,7 @@ namespace Files.App.Data.Items
 				[AlwaysVisibleContextId] =
 				[
 					new(commandGroup: nameof(CommandGroups.NewItem), showLabel: true),
+					new(commandCode: ToolbarItemDescriptor.SeparatorCommandCode, showIcon: false),
 					new(commandCode: nameof(CommandCodes.CutItem)),
 					new(commandCode: nameof(CommandCodes.CopyItem)),
 					new(commandCode: nameof(CommandCodes.PasteItem)),
