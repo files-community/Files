f4d91f6b4e2acabdefa2cd38dbb3e5f2a0c33812
yair100 <39923744+yair100@users.noreply.github.com>
2026-06-25 15:25:54 -0400

    Update MainPage.xaml.cs

--- src/Files.App/Views/MainPage.xaml.cs
+++ src/Files.App/Views/MainPage.xaml.cs
@@ -56,6 +56,7 @@ namespace Files.App.Views
 
 			ViewModel.PropertyChanged += ViewModel_PropertyChanged;
 			UserSettingsService.OnSettingChangedEvent += UserSettingsService_OnSettingChangedEvent;
+			ContentPageContext.PropertyChanged += ContentPageContext_PropertyChanged;
 
 			_updateDateDisplayTimer = DispatcherQueue.CreateTimer();
 			_updateDateDisplayTimer.Interval = TimeSpan.FromSeconds(1);
@@ -186,6 +187,12 @@ namespace Files.App.Views
 			UpdateStatusBarProperties();
 			UpdateNavToolbarProperties();
 			LoadPaneChanged();
+		}
+
+		private void ContentPageContext_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+		{
+			if (e.PropertyName is nameof(IContentPageContext.PageType))
+				LoadPaneChanged();
 		}
 
 		private void UpdateStatusBarProperties()
