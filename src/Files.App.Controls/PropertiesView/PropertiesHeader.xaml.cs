2430978812f92cedcca514a578013e5365b06f57
Martin Anderson <mdta@live.co.uk>
2026-08-20 14:03:26 +0100

    Corrected Namespaces following changes made by @0x5bfa

--- src/Files.App.Controls/PropertiesView/PropertiesHeader.xaml.cs
+++ src/Files.App.Controls/PropertiesView/PropertiesHeader.xaml.cs
@@ -16,7 +16,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace Files.App.Controls.Dialogs
+namespace Files.App.Controls
 {
 	public sealed partial class PropertiesHeader : UserControl
 	{
