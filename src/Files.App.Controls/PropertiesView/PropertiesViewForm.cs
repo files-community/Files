ec5259938089f083f7b34b91eac269512e16f074
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2026-08-21 12:13:05 +0900

    Implement PropertiesViewForm

---
+++ src/Files.App.Controls/PropertiesView/PropertiesViewForm.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Files Community
+// SPDX-License-Identifier: MPL-2.0
+
+namespace Files.App.Controls
+{
+	public partial class PropertiesViewForm : ItemsControl
+	{
+		public PropertiesViewForm()
+		{
+			DefaultStyleKey = typeof(PropertiesViewForm);
+		}
+	}
+}
--- tests/Files.App.UITests/Dialogs/Properties/Views/PropertiesGeneralPage.xaml.cs
+++ tests/Files.App.UITests/Dialogs/Properties/Views/PropertiesGeneralPage.xaml.cs
@@ -1,31 +1,15 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+// Copyright (c) Files Community
+// SPDX-License-Identifier: MPL-2.0
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Xaml.Controls;
 
 namespace Files.App.UITests.Dialogs.Properties.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class PropertiesGeneralPage : Page
-    {
-        public PropertiesGeneralPage()
-        {
-            InitializeComponent();
-        }
-    }
+	public sealed partial class PropertiesGeneralPage : Page
+	{
+		public PropertiesGeneralPage()
+		{
+			InitializeComponent();
+		}
+	}
 }
