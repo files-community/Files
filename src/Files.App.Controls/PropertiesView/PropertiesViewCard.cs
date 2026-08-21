a72bfd60092f6e213ccb350c32464bce50557281
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2026-08-22 05:26:14 +0900

    Add PropertiesViewCard

---
+++ src/Files.App.Controls/PropertiesView/PropertiesViewCard.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Files Community
+// SPDX-License-Identifier: MPL-2.0
+
+using CommunityToolkit.WinUI;
+
+namespace Files.App.Controls;
+
+public partial class PropertiesViewCard : ButtonBase
+{
+	[GeneratedDependencyProperty]
+	public partial object? Header { get; set; }
+
+	[GeneratedDependencyProperty]
+	public partial FrameworkElement? HeaderIcon { get; set; }
+
+	[GeneratedDependencyProperty]
+	public partial FrameworkElement? IsClickEnabled { get; set; }
+}
